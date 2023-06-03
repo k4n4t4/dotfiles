06e6072b44031b9a87c9d664d5cc62b8a55a7877
nk262 <NimuK@protonmail.com>
2023-06-03 19:57:58 +0900

    fish prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,64 +1,104 @@
 
 function fish_prompt
   set last_status $status
-  set user_pwd  (prompt_pwd)
-  set user_date (date "+%H:%M:%S")
   
-  if [ $USER = "root" ];
-    set user_color "\033[31m"
-    set user_symbol (printf "\uE0B1")
+  if [ $last_status = 0 ]
+    set user_status_color (printf "\033[38;5;226m")
   else
-    set user_color "\033[34m"
-    set user_symbol (printf "\uE0B1")
+    set user_status_color (printf "\033[38;5;160m")
+  end
+  
+  set reset_color (printf "\033[m")
+  set reset_fg_color (printf "\033[39m")
+  set reset_bg_color (printf "\033[49m")
+  
+  set user_pwd (prompt_pwd)
+  if [ -w (pwd) ]
+    set user_pwd_color (printf "\033[38;5;214m")
+  else
+    set user_pwd_color (printf "\033[38;5;196m")
+  end
+  
+  set user_date (date "+%H:%M:%S")
+  set user_date_color (printf "\033[38;5;129m")
+  
+  if [ "$USER" = "root" ]
+    set user_symbol "#"
+    set user_color (printf "\033[38;5;196m")
+  else
+    set user_symbol "\$"
+    set user_color (printf "\033[38;5;63m")
   end
   
   if set repo_type (_repo_type)
-    set repo_branch (_repo_branch_name $repo_type)
-    if test -n (_is_repo_dirty $repo_type)
-      set dirty "!"
+    if [ "$repo_type" = "git" ]
+      set repo_type_color (printf "\033[38;5;232m")
     else
-      set dirty ""
+      set repo_type_color (printf "\033[38;5;255m")
+    end
+    
+    set repo_branch (_repo_branch_name $repo_type)
+    if [ "$repo_branch" = "master" ] || [ "$repo_branch" = "main" ]
+      set repo_branch_color (printf "\033[38;5;164m")
+    else
+      set repo_branch_color (printf "\033[38;5;75m")
+    end
+    
+    if [ -n (_is_repo_dirty $repo_type) ]
+      set repo_dirty "!"
+      set repo_dirty_color (printf "\033[38;5;196m")
+    else
+      set repo_dirty ""
+      set repo_dirty_color ""
     end
   end
   
-  
-  printf "%s" (printf "\033[48;5;240m\033[97m") " " $user_date (printf " \033[m")
-  printf "\033[38;5;240m\033[48;5;236m\uE0B0 "
-  printf "%s" (printf "\033[48;5;236m\033[97m") $user_pwd (printf " \033[m")
-  if [ "$repo_type" != "" ];
-    printf "\033[38;5;236m\033[48;5;240m\uE0B0 "
-
-    printf "\033[97m"
-    printf $repo_type
-    printf "%s" (printf "\033[30m(\033[97m") $repo_branch (printf "\033[30m)")
-    if [ "$dirty" != "" ];
-      printf "%s" (printf "\033[91m") $dirty
-    end
-    
-    printf " "
-    
-    printf "\033[m\033[38;5;240m\uE0B4\033[m"
-  else
-    printf "\033[m\033[38;5;236m\uE0B4\033[m"
-  end
-  
-  
-  printf "\n"
-  
-  printf "%s" \
-    (printf $user_color) $USER "@" (prompt_hostname) \
-    (printf "\033[m")
+  printf "\033[48;5;236m"
   
   printf " "
   
-  if [ $last_status -ne 0 ];
-    printf "\033[91m"
+  printf "%s" $user_date_color $user_date
+  printf " "
+  
+  printf "\033[48;5;238m\033[38;5;236m"
+  printf ""
+  
+  printf " "
+  
+  printf "%s" $user_color $USER "@" (prompt_hostname)
+  printf " "
+  
+  printf "\033[48;5;236m\033[38;5;238m"
+  printf ""
+  printf " "
+  
+  printf "%s" $user_pwd_color $user_pwd
+  printf " "
+  
+  if _is_git_repo
+    
+    printf "\033[48;5;238m\033[38;5;236m"
+    printf ""
+    printf " "
+    
+    printf "%s" $repo_type_color $repo_type
+    printf "%s" $reset_fg_color "(" $repo_branch_color $repo_branch $reset_fg_color ")"
+    printf "%s" $repo_dirty_color $repo_dirty
+    
+    printf " "
+    printf "%s" $reset_color
+    printf "\033[38;5;238m"
+    printf ""
+    
   else
-    printf "\033[93m"
+    printf "%s" $reset_color
+    printf "\033[38;5;236m"
+    printf ""
   end
   
-  printf "%s" $user_symbol (printf "\033[m")
+  printf "\n"
   
+  printf "%s" $reset_color $user_status_color $user_symbol $reset_color
   printf " "
 
 end
