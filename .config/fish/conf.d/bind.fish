3dcaba5462f5282677aad36340efccd5c5cb5546
nk262 <NimuK@protonmail.com>
2023-06-09 18:49:50 +0900

    fish bind

--- .config/fish/conf.d/bind.fish
+++ .config/fish/conf.d/bind.fish
@@ -1,4 +1,6 @@
 bind \er 'commandline -i  \\\\\\n'
 bind \eg 'commandline -r "cd "'
 bind \e/ 'cdgitroot && commandline -f repaint'
+bind \ev 'fish_clipboard_paste'
+bind \ec 'beginning-of-line' 'kill-word'
 
