9c065819fa5d0a0a5e2a3a02f31e50f4fc0665bc
nk262 <NimuK@protonmail.com>
2023-06-10 20:03:09 +0900

    fish prompt

--- .config/fish/functions/not_use_nerd_fish_prompt.fish
+++ .config/fish/functions/not_use_nerd_fish_prompt.fish
@@ -1,4 +1,8 @@
 
+#variable
+set _power_supply_path "/sys/class/power_supply"
+set _battery_path (command find $_power_supply_path | command grep BAT)
+
 function fish_prompt
   set last_status $status
   
@@ -17,6 +21,43 @@ function fish_prompt
     set user_pwd_color (printf "\033[38;5;214m")
   else
     set user_pwd_color (printf "\033[38;5;196m")
+  end
+  
+  set user_battery_capacity       (cat "$_battery_path/capacity")
+  set user_battery_capacity_level (cat "$_battery_path/capacity_level")
+  set user_battery_status         (cat "$_battery_path/status")
+  
+  switch "$user_battery_capacity_level"
+      case "Full"
+        set user_battery_capacity_color (set_color "blue")
+      case "High"
+        set user_battery_capacity_color (set_color "green")
+      case "Normal"
+        set user_battery_capacity_color (set_color "white")
+      case "Low"
+        set user_battery_capacity_color (set_color "yellow")
+      case "Critical"
+        set user_battery_capacity_color (set_color "red")
+      case '*'
+        set user_battery_capacity_color (set_color "purple")
+  end
+  
+  switch "$user_battery_status"
+      case "Full"
+        set user_battery_status_symbol "~"
+        set user_battery_status_color (set_color "blue")
+      case "Charging"
+        set user_battery_status_symbol "+"
+        set user_battery_status_color (set_color "green")
+      case "Discharging"
+        set user_battery_status_symbol "-"
+        set user_battery_status_color (set_color "yellow")
+      case "Not charging"
+        set user_battery_status_symbol "="
+        set user_battery_status_color (set_color "white")
+      case '*'
+        set user_battery_status_symbol "?"
+        set user_battery_status_color (set_color "purple")
   end
   
   set user_date (date "+%H:%M:%S")
@@ -53,45 +94,39 @@ function fish_prompt
     end
   end
   
-  printf "\033[48;5;236m"
-  
-  printf " "
+  printf "\033[48;5;236m "
   
   printf "%s" $user_date_color $user_date
-  printf " "
   
-  printf "\033[48;5;238m\033[38;5;236m"
+  printf " \033[48;5;238m\033[38;5;236m "
   
-  printf " "
+  printf "%s" $user_battery_capacity_color $user_battery_capacity "%" $user_battery_status_color $user_battery_status_symbol
   
-  printf "%s" $user_color $USER "@" (prompt_hostname)
-  printf " "
-  
-  printf "\033[48;5;236m\033[38;5;238m"
-  printf " "
+  printf " \033[48;5;236m\033[38;5;238m "
   
   printf "%s" $user_pwd_color $user_pwd
-  printf " "
   
   if _is_git_repo
-    
-    printf "\033[48;5;238m\033[38;5;236m"
-    printf " "
+    printf " \033[48;5;238m\033[38;5;236m "
     
     printf "%s" $repo_type_color $repo_type
     printf "%s" $reset_fg_color "(" $repo_branch_color $repo_branch $reset_fg_color ")"
     printf "%s" $repo_dirty_color $repo_dirty
     
     printf " "
+    
     printf "%s" $reset_color
     printf "\033[38;5;238m"
-    
   else
+    printf " "
     printf "%s" $reset_color
     printf "\033[38;5;236m"
   end
   
   printf "\n"
+  
+  printf "%s" $user_color $USER "@" (prompt_hostname)
+  printf " "
   
   printf "%s" $reset_color $user_status_color $user_symbol $reset_color
   printf " "
