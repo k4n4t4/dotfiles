51b1bd0847dfb0f0c7eb844d65b8da7389d470ef
nk262 <NimuK@protonmail.com>
2023-06-13 17:59:56 +0900

    fish color

--- .config/fish/conf.d/color.fish
+++ .config/fish/conf.d/color.fish
@@ -1,23 +1,23 @@
 set fish_color_normal         brwhite
 set fish_color_autosuggestion brblack
-set fish_color_command        brgreen
-set fish_color_error          brred
+set fish_color_command        green
+set fish_color_error          red
 set fish_color_comment        white
 set fish_color_cancel         red
-set fish_color_cwd            blue
-set fish_color_cwd_root       red
+set fish_color_escape         cyan
+set fish_color_operator       purple
+set fish_color_param          blue
+set fish_color_quote          yellow
+set fish_color_redirection    cyan
 set fish_color_end            brwhite
-set fish_color_escape         brcyan
 set fish_color_match          brcyan --underline
-set fish_color_operator       brpurple
-set fish_color_param          brblue
-set fish_color_quote          bryellow
-set fish_color_redirection    brcyan
 set fish_color_search_match   --background=brblack
 set fish_color_selection      --background=brblack
 set fish_color_user           brblue
 set fish_color_host           brgreen
 set fish_color_host_remote    bryellow
+set fish_color_cwd            blue
+set fish_color_cwd_root       red
 
 set fish_pager_color_selected_background -r
 set fish_pager_color_completion          brwhite
