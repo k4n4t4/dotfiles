d9983efb852d54e753394c9089ec5fea3533c189
nk262 <NimuK@protonmail.com>
2023-06-19 16:56:17 +0900

    dot

--- .config/fish/conf.d/event_functions.fish
+++ .config/fish/conf.d/event_functions.fish
@@ -1,6 +1,6 @@
 
 function fish_exit --on-event fish_exit
-  printf "\033[90m\uE0B2\033[100m\033[97m exit \033[m\033[90m\uE0B0\033[m\n"
+  printf "\033[90m\033[100m\033[97m exit \033[m\033[90m\033[m\n"
 end
 
 function fish_preexec --on-event fish_preexec
--- .config/fish/functions/fish_command_not_found.fish
+++ .config/fish/functions/fish_command_not_found.fish
@@ -1,5 +1,5 @@
 function fish_command_not_found
   printf "\033[41m\033[37m "
   printf "Did not find command: \033[97m$argv[1]"
-  printf " \033[m\033[31m\uE0B0\033[m\n"
+  printf " \033[m\033[31m\033[m\n"
 end
