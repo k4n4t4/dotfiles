1ad179974a675337debfac40f0ab027ce0401ba9
nk262 <NimuK@protonmail.com>
2023-06-26 22:35:47 +0900

    fish config path

---
+++ files/.config/fish/paths.fish
@@ -0,0 +1,12 @@
+fish_add_path \
+  "~/bin" \
+  "~/.cargo/bin" \
+  "~/.local/bin" \
+  "/snap/bin" \
+  "/usr/sandbox/" \
+  "/usr/local/bin" \
+  "/usr/share/games" \
+  "/usr/local/sbin" \
+  "/usr/sbin" \
+  "/sbin" \
+  "/home/linuxbrew/.linuxbrew/bin"
