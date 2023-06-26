3c2059164467deadb220a3500d69bf35341b2e08
nk262 <NimuK@protonmail.com>
2023-06-26 22:27:01 +0900

    fish config bind

---
+++ files/.config/fish/functions/fish_user_key_bindings.fish
@@ -0,0 +1,11 @@
+function fish_user_key_bindings
+  bind \er 'commandline -i  \\\\\\n'
+  bind \eg 'commandline -r "cd "'
+  bind \e/ 'cdgitroot && commandline -f repaint'
+  bind \ev 'fish_clipboard_paste'
+  bind \ec 'beginning-of-line' 'kill-word'
+  bind \ek 'commandline ""'
+  bind \t 'commandline -f complete'
+  bind \n 'fish_force_preexec && commandline -f execute'
+  bind \r 'fish_force_preexec && commandline -f execute'
+end
