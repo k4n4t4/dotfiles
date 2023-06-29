8fd7de181f77726c67f8bf8ab35f23ab95309591
nk262 <NimuK@protonmail.com>
2023-06-29 17:28:53 +0900

    fish postexec

--- files/.config/fish/conf.d/event_functions.fish
+++ files/.config/fish/conf.d/event_functions.fish
@@ -8,6 +8,9 @@ function fish_preexec --on-event fish_preexec
 end
 
 function fish_postexec --on-event fish_postexec
+  if test "$argv[1]" = "clear"
+    return
+  end
   printf "\033[m"
   yes = | head -n $COLUMNS | tr -d "\n"
   echo
