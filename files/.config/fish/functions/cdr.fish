f6da3c2eb6ae81f6cc877041d57e7e2c6cfffe42
nk262 <NimuK@protonmail.com>
2023-07-01 18:55:27 +0900

    fish

--- files/.config/fish/functions/cdr.fish
+++ files/.config/fish/functions/cdr.fish
@@ -1,5 +1,12 @@
 
 function cdr
-  echo "cdr"
+  if type -q fzf
+    set find_dir (z -l | awk '{print $2}' | fzf --prompt "z > " --query "$argv")
+  else if type -q peco
+    set find_dir (z -l | awk '{print $2}' | peco --prompt "z >" --query "$argv")
+  else
+    echo "Please install peco or fzf."
+    return 1
+  end
 end
 
