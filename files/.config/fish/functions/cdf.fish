59dc36f4874853c4e7974202a610479ffa7bea31
nk262 <NimuK@protonmail.com>
2023-07-01 18:39:04 +0900

    fd

--- files/.config/fish/functions/cdf.fish
+++ files/.config/fish/functions/cdf.fish
@@ -2,18 +2,18 @@
 function cdf
   if type -q fd
     if type -q fzf
-      set find_dir (fd -H -t d | fzf --prompt "ghq > " --query "$argv")
+      set find_dir (fd -H -t d | fzf --prompt "fd > " --query "$argv")
     else if type -q peco
-      set find_dir (fd -H -t d | peco --prompt "ghq >" --query "$argv")
+      set find_dir (fd -H -t d | peco --prompt "fd >" --query "$argv")
     else
       echo "Please install peco or fzf."
       return 1
     end
   else
     if type -q peco
-      set find_dir (find -type d | peco --prompt "ghq >" --query "$argv")
+      set find_dir (find -type d | peco --prompt "find >" --query "$argv")
     else if type -q fzf
-      set find_dir (find -type d | fzf --prompt "ghq > " --query "$argv")
+      set find_dir (find -type d | fzf --prompt "find > " --query "$argv")
     else
       echo "Please install peco or fzf."
       return 1
