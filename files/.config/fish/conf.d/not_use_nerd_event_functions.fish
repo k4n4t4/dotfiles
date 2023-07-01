bc2f293ad3010a060cf36fd61eb38611ea65a6f5
n_k <NimuK@protonmail.com>
2023-07-01 06:44:19 +0000

    fish

--- files/.config/fish/conf.d/not_use_nerd_event_functions.fish
+++ files/.config/fish/conf.d/not_use_nerd_event_functions.fish
@@ -8,10 +8,11 @@ function fish_preexec --on-event fish_preexec
 end
 
 function fish_postexec --on-event fish_postexec
-  if test "$argv[1]" = "clear"
-    return
-  end
-  printf "\033[m"
-  yes = | head -n $COLUMNS | tr -d "\n"
-  echo
+  # if test "$argv[1]" = "clear"
+  #   return
+  # end
+  # printf "\033[m"
+  # yes = | head -n $COLUMNS | tr -d "\n"
+  # echo
+  :
 end
