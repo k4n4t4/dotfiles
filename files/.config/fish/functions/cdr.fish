060644ddb7045b14566b5ac87c0741ff215b20d8
nk262 <NimuK@protonmail.com>
2023-07-01 18:56:56 +0900

    fish

--- files/.config/fish/functions/cdr.fish
+++ files/.config/fish/functions/cdr.fish
@@ -1,12 +1,13 @@
 
 function cdr
   if type -q fzf
-    set find_dir (z -l | awk '{print $2}' | fzf --prompt "z > " --query "$argv")
+    set z_dir (z -l | awk '{print $2}' | fzf --prompt "z > " --query "$argv")
   else if type -q peco
-    set find_dir (z -l | awk '{print $2}' | peco --prompt "z >" --query "$argv")
+    set z_dir (z -l | awk '{print $2}' | peco --prompt "z >" --query "$argv")
   else
     echo "Please install peco or fzf."
     return 1
   end
+  cd $z_dir
 end
 
