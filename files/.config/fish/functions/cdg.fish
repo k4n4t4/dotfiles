832a5ef9560529a4235e98f2fc709153fcc752b9
nk262 <NimuK@protonmail.com>
2023-07-01 18:20:27 +0900

    alias

--- files/.config/fish/functions/cdg.fish
+++ files/.config/fish/functions/cdg.fish
@@ -4,10 +4,10 @@ function cdg
     echo "Please install ghq."
     return 1
   end
-  if type -q peco
-    set git_dir (ghq list --full-path | peco --prompt "ghq >" --query "$argv")
-  else if type -q fzf
+  if type -q fzf
     set git_dir (ghq list --full-path | fzf --prompt "ghq > " --query "$argv")
+  else if type -q peco
+    set git_dir (ghq list --full-path | peco --prompt "ghq >" --query "$argv")
   else
     echo "Please install peco or fzf."
     return 1
