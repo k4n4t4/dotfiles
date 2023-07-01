c52d1e10903dd10b7eca80dcce7346b6612d2849
nk262 <NimuK@protonmail.com>
2023-07-01 18:07:30 +0900

    cd

--- files/.config/fish/functions/cdf.fish
+++ files/.config/fish/functions/cdf.fish
@@ -1,19 +1,19 @@
 
 function cdf
   if type -q fd
-    if type -q peco
-      set find_dir (ghq list --full-path | peco --prompt "ghq >" --query "$argv")
-    else if type -q fzf
-      set find_dir (ghq list --full-path | fzf --prompt "ghq > " --query "$argv")
+    if type -q fzf
+      set find_dir (fd -H -t d | fzf --prompt "ghq > " --query "$argv")
+    else if type -q peco
+      set find_dir (fd -H -t d | peco --prompt "ghq >" --query "$argv")
     else
       echo "Please install peco or fzf."
       return 1
     end
   else
     if type -q peco
-      set find_dir (ghq list --full-path | peco --prompt "ghq >" --query "$argv")
+      set find_dir (find -type d | peco --prompt "ghq >" --query "$argv")
     else if type -q fzf
-      set find_dir (ghq list --full-path | fzf --prompt "ghq > " --query "$argv")
+      set find_dir (find -type d | fzf --prompt "ghq > " --query "$argv")
     else
       echo "Please install peco or fzf."
       return 1
--- files/.config/fish/functions/cdr.fish
+++ files/.config/fish/functions/cdr.fish
@@ -1,5 +1,5 @@
 
 function cdr
-  :
+  echo "cdr"
 end
 
