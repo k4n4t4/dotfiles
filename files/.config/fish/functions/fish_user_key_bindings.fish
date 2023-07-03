c0cb25bfaba3df8c5c919cfe6efc2359a9f93091
nk262 <NimuK@protonmail.com>
2023-07-03 18:13:23 +0900

    fish bind

--- files/.config/fish/functions/fish_user_key_bindings.fish
+++ files/.config/fish/functions/fish_user_key_bindings.fish
@@ -8,4 +8,5 @@ function fish_user_key_bindings
   bind \t 'commandline -f complete'
   bind \n 'fish_force_preexec && commandline -f execute'
   bind \r 'fish_force_preexec && commandline -f execute'
+  bind \eR 'commandline -f repaint'
 end
