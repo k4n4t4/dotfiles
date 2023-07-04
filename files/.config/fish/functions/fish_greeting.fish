f7bfa3ddec82dd2e91c35005bb7b0762ed165f32
nk262 <NimuK@protonmail.com>
2023-07-04 23:48:44 +0900

    fish greeting

--- files/.config/fish/functions/fish_greeting.fish
+++ files/.config/fish/functions/fish_greeting.fish
@@ -1,6 +1,7 @@
 function fish_greeting
+  printf "\n  "
   for i in (seq 0 7)
-    printf "\033[4%sm  \033[10%sm  " $i $i
+    printf "\033[4%sm  \033[m  \033[10%sm  \033[m  " $i $i
   end
-  printf "\033[m\n"
+  printf "\033[m\n\n"
 end
