cf381e59a5b36e193e9d3ca891495a420fe57a87
n_k <NimuK@protonmail.com>
2023-07-05 23:18:22 +0000

    dotfiles fix

--- link_list.sh
+++ link_list.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 set -eu
 
-info "===== bin (total: $(cat "lists/bin" | wc -l)) ====="
-read_link_list "lists/bin"
-
 for path in lists/*; do
   file="$(basename "$path")"
   if cmd_exist "$file" || [ "$file" = "bin" ]; then
