a7472e11230c03eeadddbb419d6268bafd876233
nk262 <NimuK@protonmail.com>
2023-07-12 20:02:48 +0900

    fish

--- files/.config/fish/functions/fish_user_key_bindings.fish
+++ files/.config/fish/functions/fish_user_key_bindings.fish
@@ -5,7 +5,7 @@ function fish_user_key_bindings
   bind \ev 'fish_clipboard_paste'
   bind \ec 'beginning-of-line' 'kill-word'
   bind \ek 'commandline ""'
-  bind \t 'commandline -f complete'
+  bind \t 'commandline -f complete && fish_force_preexec'
   bind \n 'fish_force_preexec && commandline -f execute'
   bind \r 'fish_force_preexec && commandline -f execute'
   bind \eR 'commandline -f repaint'
