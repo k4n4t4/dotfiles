cd9fb7e32cbc66dd5f53175e91034f7860eb6ece
nk262 <NimuK@protonmail.com>
2023-07-18 18:34:57 +0900

    search

--- files/.config/fish/functions/search.fish
+++ files/.config/fish/functions/search.fish
@@ -1,3 +1,3 @@
 function search
-  firefox "https://duckduckgo.com/?kae=t&kp=-2&kak=-1&kax=-1&kaq=-1&kap=-1&kao=-1&kau=-1&k1=-1&kt=p&atb=v377-1&q=$argv"
+  firefox "https://duckduckgo.com/?kae=t&kp=-2&kak=-1&kax=-1&kaq=-1&kap=-1&kao=-1&kau=-1&k1=-1&kt=p&atb=v377-1&q=$argv" &
 end
