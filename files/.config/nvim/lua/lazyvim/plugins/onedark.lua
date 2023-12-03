3490f6795ed283554dbed3ff397423a3f398277a
n_k <nimu>
2023-12-04 07:09:42 +0900

    nvim theme

--- files/.config/nvim/lua/lazyvim/plugins/onedark.lua
+++ files/.config/nvim/lua/lazyvim/plugins/onedark.lua
@@ -3,6 +3,13 @@ return {
     "navarasu/onedark.nvim",
     priority = 1000,
     config = function()
+      require("onedark").setup({
+          transparent = true,
+          styles = {
+              sidebars = "transparent",
+              floats = "transparent",
+          },
+      })
       vim.cmd("colorscheme onedark")
     end
   }
