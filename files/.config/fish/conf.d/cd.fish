4223578609e8d43fff0de1c6b5a708e81c64bf23
n_k <nimu>
2023-12-26 23:43:52 +0900

    fish sudo

--- files/.config/fish/conf.d/cd.fish
+++ files/.config/fish/conf.d/cd.fish
@@ -2,7 +2,13 @@
 functions --copy cd __cd
 
 function cd
-  set -f old_pwd (pwd)
+  set -f old_pwd $PWD
   __cd $argv
-  echo "$old_pwd -> "(pwd)
+  if [ "$old_pwd" != "$PWD" ]
+    printf "\033[48;5;94m\033[38;5;214m\033[1m "
+    printf "CD"
+    printf " \033[38;5;94m\033[48;5;58m\033[38;5;214m\033[1m "
+    printf "$old_pwd 󰁕 $PWD"
+    printf " \033[m\033[38;5;58m\033[m\n"
+  end
 end
--- files/.config/fish/functions/sudo.fish
+++ files/.config/fish/functions/sudo.fish
@@ -5,5 +5,10 @@ function sudo -d "sudo wrapper that handles aliases"
     set argv fish -c "$new_args"
   end
 
-  command sudo -p "Password: " $argv
+  printf "\033[48;5;18m\033[38;5;33m\033[1m "
+  printf "SUDO"
+  printf " \033[38;5;18m\033[48;5;17m\033[38;5;33m\033[1m "
+  printf "Password"
+  printf " \033[m\033[38;5;17m\033[m "
+  command sudo -p "" $argv
 end
