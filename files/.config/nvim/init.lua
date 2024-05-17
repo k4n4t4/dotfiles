f1797f063fb27b40c76056e94a54ca4a4f584655
n_k <nimu>
2024-05-17 23:28:04 +0900

    update

--- files/.config/nvim/init.lua
+++ files/.config/nvim/init.lua
@@ -1,14 +1,4 @@
 
-vim.opt.number = true
-vim.opt.relativenumber = true
-
-vim.opt.clipboard = 'unnamedplus'
-
-vim.opt.expandtab = true
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-
-
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+require("settings/commons")
+require("settings/keymaps")
 
---
+++ files/.config/nvim/lua/settings/commons.lua
@@ -0,0 +1,14 @@
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.clipboard = 'unnamedplus'
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+
+
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
