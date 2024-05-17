8e32a6ce1fdf6f14a52a249e52f2047a8ce0b813
n_k <nimu>
2024-05-18 00:00:21 +0900

    update

--- files/.config/nvim/lua/settings/commons.lua
+++ files/.config/nvim/lua/settings/commons.lua
@@ -1,12 +1,36 @@
+vim.cmd 'autocmd!'
+
+vim.scriptencoding = 'utf-8'
+vim.opt.encoding = 'utf-8'
+vim.opt.fileencoding = 'utf-8'
 
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+vim.ambiwidth = 'double'
+
 vim.opt.wrap = true
 
+vim.opt.mouse = 'a'
+vim.opt.title = true
 vim.opt.clipboard = 'unnamedplus'
 
 vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 
+vim.opt.list = true
+vim.opt.listchars = {tab = '>-', trail = '*', nbsp = '+'}
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+
+vim.opt.visualbell = true
+vim.opt.hlsearch = true
+
+vim.opt.showmatch = true
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.hidden = true
+
+vim.opt.showtabline = 2
