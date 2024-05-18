fd33c99f64490e0ad8046793fba2015b46af4403
n_k <nimu>
2024-05-18 12:53:33 +0900

    update

--- files/.config/nvim/lua/settings/commons.lua
+++ files/.config/nvim/lua/settings/commons.lua
@@ -13,14 +13,14 @@ vim.opt.wrap = true
 
 vim.opt.mouse = 'a'
 vim.opt.title = true
-vim.opt.clipboard = 'unnamedplus'
+vim.opt.clipboard:append 'unnamedplus'
 
 vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 
 vim.opt.list = true
-vim.opt.listchars = {tab = '>-', trail = '*', nbsp = '+'}
+vim.opt.listchars = {tab = '>-', trail = '-', nbsp = '+'}
 vim.opt.smartindent = true
 vim.opt.autoindent = true
 
@@ -34,3 +34,6 @@ vim.opt.backup = false
 vim.opt.hidden = true
 
 vim.opt.showtabline = 2
+
+vim.opt.backspace = 'indent,eol,start'
+
