101353c1657fa7559e9838cfe5123409be3f2860
n_k <nimu>
2024-05-18 23:32:46 +0900

    update

--- files/.config/nvim/lua/settings/commons.lua
+++ files/.config/nvim/lua/settings/commons.lua
@@ -2,20 +2,16 @@ vim.scriptencoding = 'utf-8'
 vim.opt.encoding = 'utf-8'
 vim.opt.fileencoding = 'utf-8'
 
-vim.opt.confirm = true
-
 vim.opt.number = true
 vim.opt.relativenumber = false
 vim.opt.signcolumn = 'yes'
-
+vim.opt.list = true
+vim.opt.listchars = {tab = '>-', trail = '-', nbsp = '+'}
 vim.opt.cursorline = true
 vim.opt.cursorcolumn = false
-
-
-vim.opt.splitright = true
+vim.opt.visualbell = true
 
 vim.ambiwidth = 'single'
-
 vim.opt.wrap = true
 
 vim.opt.mouse = 'a'
@@ -25,32 +21,28 @@ vim.opt.clipboard:append 'unnamedplus'
 vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
-
-vim.opt.list = true
-vim.opt.listchars = {tab = '>-', trail = '-', nbsp = '+'}
 vim.opt.smartindent = true
 vim.opt.autoindent = true
 
-vim.opt.visualbell = true
 vim.opt.hlsearch = true
 vim.opt.incsearch = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.showcmd = true
-
 vim.opt.showmatch = true
 
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.hidden = true
 
-vim.opt.showtabline = 2
-
 vim.opt.backspace = [[indent,eol,start]]
-
+vim.opt.splitright = true
+vim.opt.confirm = true
 vim.opt.wildmenu = true
+
 vim.opt.cmdheight = 1
 vim.opt.laststatus = 2
+vim.opt.showtabline = 2
 
 vim.opt.winblend = 20
 vim.opt.pumblend = 20
