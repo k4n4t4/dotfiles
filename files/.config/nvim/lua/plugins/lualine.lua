f84a430fab6b65727452e3284d04bfd058ce54a2
n_k <nimu>
2024-05-19 19:36:55 +0900

    update

--- files/.config/nvim/lua/plugins/lualine.lua
+++ files/.config/nvim/lua/plugins/lualine.lua
@@ -1,14 +1,14 @@
 return {
-  'nvim-lualine/lualine.nvim',
+  "nvim-lualine/lualine.nvim",
   dependencies = {
-    'nvim-tree/nvim-web-devicons',
+    "nvim-tree/nvim-web-devicons",
   },
   opts = {
     options = {
       icons_enabled = true,
-      theme = 'auto',
-      component_separators = { left = '', right = ''},
-      section_separators = { left = '', right = ''},
+      theme = "auto",
+      component_separators = { left = "", right = ""},
+      section_separators = { left = "", right = ""},
       disabled_filetypes = {
         statusline = {},
         winbar = {},
@@ -23,18 +23,18 @@ return {
       }
     },
     sections = {
-      lualine_a = {'mode'},
-      lualine_b = {'branch', 'diff', 'diagnostics'},
-      lualine_c = {'filename'},
-      lualine_x = {'encoding', 'fileformat', 'filetype'},
-      lualine_y = {'progress'},
-      lualine_z = {'location'}
+      lualine_a = {"mode"},
+      lualine_b = {"branch", "diff", "diagnostics"},
+      lualine_c = {"filename"},
+      lualine_x = {"encoding", "fileformat", "filetype"},
+      lualine_y = {"progress"},
+      lualine_z = {"location"}
     },
     inactive_sections = {
       lualine_a = {},
       lualine_b = {},
-      lualine_c = {'filename'},
-      lualine_x = {'location'},
+      lualine_c = {"filename"},
+      lualine_x = {"location"},
       lualine_y = {},
       lualine_z = {}
     },
