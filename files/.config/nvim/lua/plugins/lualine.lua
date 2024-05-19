526d8ed2aaa990082f40de58f518e485a32bd37d
n_k <nimu>
2024-05-19 20:02:43 +0900

    update

--- files/.config/nvim/lua/plugins/lualine.lua
+++ files/.config/nvim/lua/plugins/lualine.lua
@@ -6,7 +6,7 @@ return {
   opts = {
     options = {
       icons_enabled = true,
-      theme = "auto",
+      theme = "codedark",
       component_separators = { left = "", right = ""},
       section_separators = { left = "", right = ""},
       disabled_filetypes = {
---
+++ files/.config/nvim/lua/plugins/others.lua
@@ -0,0 +1,3 @@
+return {
+  { "joshdick/onedark.vim" },
+}
