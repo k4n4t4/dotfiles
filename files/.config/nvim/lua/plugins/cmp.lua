7454dc0ab2c46c86fa6ad10a69ad3777850d63b0
n_k <nimu>
2024-05-24 21:00:15 +0900

    update

---
+++ files/.config/nvim/lua/plugins/cmp.lua
@@ -0,0 +1,57 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    config = function()
+      local lspkind = require 'lspkind'
+      local cmp = require("cmp")
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body)
+          end,
+        },
+        sources = {
+          { name = "nvim_lsp" },
+          { name = 'vsnip' },
+          { name = "buffer" },
+          { name = "path" },
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-p>"] = cmp.mapping.select_prev_item(),
+          ["<C-n>"] = cmp.mapping.select_next_item(),
+          ['<C-l>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+        }),
+        experimental = {
+          ghost_text = false,
+        },
+        formatting = {
+          format = lspkind.cmp_format({
+            mode = 'symbol',
+            maxwidth = 50,
+            ellipsis_char = '...',
+          })
+        }
+      })
+      cmp.setup.cmdline('/', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = 'buffer' }
+        }
+      })
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "path" },
+        },
+      })
+    end,
+  },
+  { "hrsh7th/cmp-nvim-lsp" },
+  { "hrsh7th/cmp-buffer" },
+  { "hrsh7th/cmp-path"  },
+  { "hrsh7th/vim-vsnip" },
+  { "hrsh7th/cmp-vsnip" },
+  { "onsails/lspkind.nvim" },
+}
---
+++ files/.config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,41 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = true,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      local on_attach = function(client, bufnr)
+        local set = vim.keymap.set
+        set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
+        set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
+        set("n", "<C-m>", "<cmd>lua vim.lsp.buf.signature_help()<CR>")
+        set("n", "gy", "<cmd>lua vim.lsp.buf.type_definition()<CR>")
+        set("n", "rn", "<cmd>lua vim.lsp.buf.rename()<CR>")
+        set("n", "ma", "<cmd>lua vim.lsp.buf.code_action()<CR>")
+        set("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>")
+        set("n", "<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>")
+        set("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>")
+        set("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>")
+      end
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      require("mason-lspconfig").setup()
+      require("mason-lspconfig").setup_handlers {
+        function(server_name)
+          require("lspconfig")[server_name].setup {
+            on_attach = on_attach,
+            capabilities = capabilities,
+          }
+        end,
+      }
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup {}
+    end,
+  },
+}
