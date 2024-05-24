19331010ccfa0880482d5ff02bbbc16626a9837c
n_k <nimu>
2024-05-24 20:14:19 +0900

    update

--- files/.config/nvim/lua/plugins/noice.lua
+++ files/.config/nvim/lua/plugins/noice.lua
@@ -1,9 +1,18 @@
 return {
-  "folke/noice.nvim",
-  event = "VeryLazy",
-  opts = {},
-  dependencies = {
-    "MunifTanjim/nui.nvim",
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {},
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
+  },
+  {
     "rcarriga/nvim-notify",
+    event = "VeryLazy",
+    opts = {
+      background_colour = "#000000",
+    },
   },
 }
