f78bc681803c6b167121a0466e974bf6fff9f935
n_k <nimu>
2024-07-05 20:21:29 +0900

    starship

--- files/.config/fish/config.fish
+++ files/.config/fish/config.fish
@@ -9,3 +9,14 @@ source $fish_dir/aliases.fish
 if [ -f $fish_dir/local_config.fish ]
   source $fish_dir/local_config.fish
 end
+
+if type -q starship
+  function starship_transient_prompt_func
+    starship module character
+  end
+  function starship_transient_rprompt_func
+    starship module time
+  end
+  starship init fish | source
+  enable_transience
+end
