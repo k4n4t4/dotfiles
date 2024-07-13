8fc82f1832a307528c718a106aef7413564c0cc5
n_k <nimu>
2024-07-14 00:00:42 +0900

    nvim config

--- files/.config/nvim/lua/settings/autocmd.lua
+++ files/.config/nvim/lua/settings/autocmd.lua
@@ -1,10 +1,8 @@
 local autocmd = vim.api.nvim_create_autocmd
 
---[
-autocmd({"BufReadPost"}, {
-  pattern = {"*"},
+autocmd("BufReadPost", {
+  pattern = "*",
   callback = function()
     vim.api.nvim_exec('silent! normal! g`"zv', false)
   end,
 })
---]]
--- files/.config/nvim/lua/settings/keymaps.lua
+++ files/.config/nvim/lua/settings/keymaps.lua
@@ -1,9 +1,11 @@
+local keymap = vim.keymap
+
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-vim.keymap.set('n', '<UP>',    '<NOP>')
-vim.keymap.set('n', '<DOWN>',  '<NOP>')
-vim.keymap.set('n', '<LEFT>',  '<NOP>')
-vim.keymap.set('n', '<RIGHT>', '<NOP>')
+keymap.set('n', '<UP>',    '<NOP>')
+keymap.set('n', '<DOWN>',  '<NOP>')
+keymap.set('n', '<LEFT>',  '<NOP>')
+keymap.set('n', '<RIGHT>', '<NOP>')
 
-vim.keymap.set('n', '<ESC><ESC>', '<CMD>noh<CR>')
+keymap.set('n', '<ESC><ESC>', '<CMD>noh<CR>')
