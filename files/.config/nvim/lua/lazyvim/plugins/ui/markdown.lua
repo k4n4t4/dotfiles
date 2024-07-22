ea9c7656ca941daff4b113f9e0f3fd0a06a5bbb3
n_k <nimu>
2024-07-22 20:37:33 +0900

    update

---
+++ files/.config/nvim/lua/lazyvim/plugins/ui/markdown.lua
@@ -0,0 +1,34 @@
+return {
+  {
+    "MeanderingProgrammer/markdown.nvim",
+    name = "render-markdown",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons"
+    },
+    ft = "markdown",
+    config = function()
+      require("render-markdown").setup({})
+    end,
+  },
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*",
+    ft = "markdown",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    opts = {
+      workspaces = {
+        {
+          name = "personal",
+          path = "~/vaults/personal",
+        },
+        {
+          name = "work",
+          path = "~/vaults/work",
+        },
+      },
+    },
+  },
+}
