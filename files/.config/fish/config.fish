5dabb33acdc8a7f65c33822c24d012092d1909d6
n_k <nimu>
2024-08-05 00:16:30 +0900

    update

--- files/.config/fish/config.fish
+++ files/.config/fish/config.fish
@@ -5,8 +5,8 @@ source $fish_dir/variables.fish
 source $fish_dir/colors.fish
 source $fish_dir/aliases.fish
 
-if test -f $fish_dir/local_config.fish
-  source $fish_dir/local_config.fish
+if test -f ~/.fishrc
+  source ~/.fishrc
 end
 
 if type -q starship
