f87bb3df26df06219f0d649ccf5908666be7255c
n_k <nimu>
2024-08-05 13:48:34 +0900

    update

---
+++ files/.config/rofi/system.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+if [ $# -eq 0 ]; then
+  echo "Lock"
+  echo "Quit"
+  echo "Restart"
+  echo "Poweroff"
+  echo "Reboot"
+else
+  case "$1" in
+    ( "Lock" )
+      notify-send "lock"
+      ;;
+    ( "Quit" )
+      notify-send "quit"
+      bspc quit
+      ;;
+    ( "Restart" )
+      notify-send "restart"
+      bspc wm -r
+      ;;
+    ( "Poweroff" )
+      notify-send "poweroff"
+      systemctl poweroff
+      ;;
+    ( "Reboot" )
+      notify-send "reboot"
+      systemctl reboot
+      ;;
+  esac
+fi
