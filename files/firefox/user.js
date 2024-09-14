b052bc13546c2fc932a25d4d9480512d326f7fee
n_k <nimu>
2024-09-14 14:54:08 +0000

    update

---
+++ files/firefox/user.js
@@ -0,0 +1,7 @@
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("browser.aboutConfig.showWarning", false);
+
+user_pref("browser.startup.homepage", "about:home");
+user_pref("browser.download.dir", "~/tmp");
+user_pref("browser.theme.dark-private-windows", true);
+user_pref("browser.tabs.hoverPreview.enabled", true);
