b8a9cc97ec8e47809590349d1de8f0f8f443691e
n_k <nimu>
2024-09-21 09:23:27 +0900

    update

--- files/firefox/user.js
+++ files/firefox/user.js
@@ -1,6 +1,6 @@
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("browser.aboutConfig.showWarning", false);
-
+user_pref("browser.warnOnQuitShortcut", false);
 user_pref("browser.startup.homepage", "about:home");
 user_pref("browser.download.dir", "~/tmp");
 user_pref("browser.theme.dark-private-windows", true);
