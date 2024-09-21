f41f661abe60e59739ee815ed1108dac4762705e
n_k <nimu>
2024-09-22 00:52:56 +0900

    update

--- files/.config/nvim/lua/lazyvim/plugins/ui/statuscol.lua
+++ files/.config/nvim/lua/lazyvim/plugins/ui/statuscol.lua
@@ -1,6 +1,6 @@
 return {
   "luukvbaal/statuscol.nvim",
-  enabled = true,
+  enabled = false,
   config = function()
     local builtin = require "statuscol.builtin"
     require("statuscol").setup {
