01cfc0d83e5083903729ec01d18195cb409d21b8
n_k <nimu>
2024-09-24 01:56:43 +0900

    update

--- files/.config/nvim/lua/settings/foldtext.lua
+++ files/.config/nvim/lua/settings/foldtext.lua
@@ -6,42 +6,55 @@ vim.opt.foldtext = "v:lua.FoldText()"
 -- vim.opt.foldtext = ""
 
 
+local function foldtext()
+  local bnr = vim.api.nvim_get_current_buf()
+  local ns_id = vim.api.nvim_create_namespace("foldtext")
+
+  local start_line = vim.fn.line('w0')
+  local end_line = vim.fn.line('w$')
+
+  vim.api.nvim_buf_clear_namespace(bnr, ns_id, start_line - 1, end_line)
+
+  local line_nr = start_line
+  while line_nr <= end_line do
+    local fold_start = vim.fn.foldclosed(line_nr)
+    if fold_start ~= -1 then
+      local fold_end = vim.fn.foldclosedend(line_nr)
+      local line_text = vim.fn.getline(line_nr)
+
+      local virt_texts = {}
+      for i = 0, #line_text-1 do
+        local inspect = vim.inspect_pos(bnr, line_nr - 1, i, {})
+        local hl
+
+        local treesitter_length = #inspect.treesitter
+
+        if treesitter_length > 0 then
+          hl = hl or inspect.treesitter[treesitter_length].hl_group
+        end
+        table.insert(virt_texts, {"@", hl or 'None'})
+      end
+
+      local opts = {
+        id = fold_start,
+        virt_text = virt_texts,
+        virt_text_pos = 'overlay',
+      }
+      vim.api.nvim_buf_set_extmark(
+        bnr, ns_id, fold_start - 1, 0, opts
+      )
+
+
+      line_nr = fold_end
+    end
+    line_nr = line_nr + 1
+  end
+end
+
 vim.api.nvim_create_autocmd({
   'CursorMoved',
 }, {
   callback = function()
-
-    local bnr = vim.fn.bufnr('%')
-    local ns_id = vim.api.nvim_create_namespace("fold")
-
-    vim.api.nvim_buf_clear_namespace(bnr, ns_id, vim.fn.line('w0') - 1, vim.fn.line('w$'))
-
-    for i = vim.fn.line('w0'), vim.fn.line('w$') do
-      if vim.fn.foldlevel(i) > 0 then
-        local fold_closed = vim.fn.foldclosed(i)
-        if fold_closed ~= -1 then
-
-          local text = {}
-
-          for j = 1, #vim.fn.getline(fold_closed) do
-              local syn_id = vim.fn.synID(fold_closed, j, 1)
-              local name = vim.fn.synIDattr(syn_id, 'name')
-              local hl = vim.fn.synIDattr(vim.fn.synIDtrans(vim.fn.synID(fold_closed, j, 1)), 'name')
-              table.insert(text , {'@', hl or 'Comment'})
-          end
-
-          local opts = {
-            id = fold_closed,
-            -- virt_text = {{vim.fn.getline(fold_closed), "type"}},
-            virt_text = text,
-            virt_text_pos = 'overlay',
-          }
-
-          local mark_id = vim.api.nvim_buf_set_extmark(
-            bnr, ns_id, fold_closed - 1, 0, opts
-          )
-        end
-      end
-    end
+    foldtext()
   end
 })
