23af48194d971f45183bdebb754c6237ea9374de
n_k <nimu>
2024-09-24 06:06:23 +0900

    update

---
+++ files/.config/nvim/lua/lazyvim/plugins/highlights.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "fladson/vim-kitty"
+  },
+  {
+    "MeanderingProgrammer/markdown.nvim",
+    name = "render-markdown",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons"
+    },
+    ft = "markdown",
+  },
+}
