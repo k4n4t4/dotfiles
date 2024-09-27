0a49ac87c634c1f66de54e460a951d88c60cfbcd
n_k <nimu>
2024-09-28 00:08:18 +0900

    update

--- files/.config/nvim/lua/settings/keymaps.lua
+++ files/.config/nvim/lua/settings/keymaps.lua
@@ -3,6 +3,11 @@ local keymap = vim.keymap
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+-- aaaa
+
+-- aaaa
+
+-- aaaa
 
 keymap.set("n", "<ESC><ESC>", "<CMD>noh<CR>")
 keymap.set("t", "<ESC>", "<C-\\><C-N>")
@@ -13,5 +18,5 @@ keymap.set("n", "<LEADER>C", "<CMD>terminal<CR>")
 keymap.set("n", "<LEADER>m", "<CMD>messages<CR>")
 
 
-keymap.set("n", "<LEADER>s", "*N")
-keymap.set("x", "<LEADER>s", "<CMD>call feedkeys(\"*N\")<CR>")
+keymap.set("n", "<LEADER>s", "viwo<CMD>let @/=getregion(getpos('v'), getpos('.'))[0]<CR><CMD>set hls<CR><ESC>")
+keymap.set("x", "<LEADER>s", "<CMD>let @/=getregion(getpos('v'), getpos('.'), {'type': mode()})[0]<CR><CMD>set hls<CR><ESC>")
