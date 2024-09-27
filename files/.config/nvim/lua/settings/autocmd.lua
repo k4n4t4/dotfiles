b3f450eba13fc2ec4d6e64968230879f2322d77f
n_k <nimu>
2024-09-27 21:02:27 +0900

    update

--- files/.config/nvim/lua/settings/autocmd.lua
+++ files/.config/nvim/lua/settings/autocmd.lua
@@ -19,27 +19,39 @@ autocmd("BufWinEnter", {
 if vim.opt.number and vim.opt.relativenumber then
   autocmd("InsertEnter", {
     callback = function()
-      if vim.wo.number then
-        vim.wo.relativenumber = false
+      if vim.opt_local.number then
+        vim.opt_local.relativenumber = false
       end
     end
   })
   autocmd("InsertLeave", {
     callback = function()
-      if vim.wo.number then
-        vim.wo.relativenumber = true
+      if vim.opt_local.number then
+        vim.opt_local.relativenumber = true
       end
     end
   })
 end
 
+if vim.opt.cmdheight == 0 then
+  autocmd("CmdlineEnter", {
+    callback = function()
+      vim.opt_local.cmdheight = 1
+    end
+  })
+  autocmd("CmdlineLeave", {
+    callback = function()
+      vim.opt_local.cmdheight = 0
+    end
+  })
+end
 
 autocmd("TermOpen", {
   callback = function()
-    vim.wo.number = false
-    vim.wo.relativenumber = false
-    vim.wo.foldcolumn = '0'
-    vim.wo.signcolumn = "no"
+    vim.opt_local.number = false
+    vim.opt_local.relativenumber = false
+    vim.opt_local.foldcolumn = '0'
+    vim.opt_local.signcolumn = "no"
     vim.cmd [[startinsert]]
   end
 })
--- files/.config/nvim/lua/settings/keymaps.lua
+++ files/.config/nvim/lua/settings/keymaps.lua
@@ -7,5 +7,7 @@ vim.g.maplocalleader = " "
 keymap.set("n", "<ESC><ESC>", "<CMD>noh<CR>")
 keymap.set("t", "<ESC>", "<C-\\><C-N>")
 
-keymap.set("n", "<LEADER>C", "<CMD>terminal<CR>")
 keymap.set("n", "<LEADER>c", "<CMD>belowright new<CR><CMD>terminal<CR>")
+keymap.set("n", "<LEADER>C", "<CMD>terminal<CR>")
+
+keymap.set("n", "<LEADER>m", "<CMD>messages<CR>")
