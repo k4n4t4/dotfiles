b50262ebe5b277567382e21e7e815f79c93c141f
n_k <nimu>
2024-09-28 12:14:07 +0900

    update

--- files/.config/nvim/lua/lazyvim/plugins/nvimtree.lua
+++ files/.config/nvim/lua/lazyvim/plugins/nvimtree.lua
@@ -3,25 +3,35 @@ return {
   dependencies = {
     "nvim-tree/nvim-web-devicons",
   },
-  opts = {
-    sort_by = "case_sensitive",
-    renderer = {
-      group_empty = true,
-    },
-    filters = {
-      dotfiles = false,
-    },
-    diagnostics = {
-      enable = true,
-      show_on_dirs = false,
-      icons = {
-        hint = "",
-        info = "",
-        warning = "",
-        error = "",
+  event = "BufEnter",
+  config = function()
+    require("nvim-tree").setup {
+      sort_by = "case_sensitive",
+      renderer = {
+        group_empty = true,
       },
-    },
-  },
+      filters = {
+        dotfiles = false,
+      },
+      diagnostics = {
+        enable = true,
+        show_on_dirs = false,
+        icons = {
+          hint = "?",
+          info = "i",
+          warning = "*",
+          error = "!",
+        },
+      },
+    }
+    vim.api.nvim_create_autocmd("BufEnter", {
+      callback = function()
+        if vim.fn.isdirectory("%") == 1 then
+          vim.cmd [[NvimTreeFocus]]
+        end
+      end
+    })
+  end,
   keys = {
     { mode = "n", "<leader>t", "<cmd>NvimTreeToggle<CR>", desc = "NvimTreeToggle" },
   }
