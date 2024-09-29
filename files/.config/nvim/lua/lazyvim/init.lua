25828e54f707f5dabb60ed80811717d42f042202
n_k <nimu>
2024-09-30 01:18:31 +0900

    update

--- files/.config/nvim/lua/lazyvim/init.lua
+++ files/.config/nvim/lua/lazyvim/init.lua
@@ -17,7 +17,6 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup {
   spec = {
     { import = "lazyvim.plugins" },
-    { import = "lazyvim.plugins.lsp" },
   },
   default = {
     lazy = true,
