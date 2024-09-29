ddd6aa6e9f09d6743a86697863acc2e0cdf9ceb7
n_k <nimu>
2024-09-30 01:09:52 +0900

    update

--- files/.config/nvim/lua/lazyvim/plugins/lsp/cmp.lua
+++ files/.config/nvim/lua/lazyvim/plugins/lsp/cmp.lua
@@ -11,22 +11,28 @@ return {
       { "hrsh7th/vim-vsnip" },
       { "hrsh7th/cmp-vsnip" },
       { "onsails/lspkind.nvim" },
+      { "L3MON4D3/LuaSnip" },
+      { "saadparwaiz1/cmp_luasnip" },
+      { "dcampos/nvim-snippy" },
+      { "dcampos/cmp-snippy" },
     },
     event = {
       "InsertEnter",
       "CmdlineEnter",
     },
     config = function()
-      local lspkind = require "lspkind"
       local cmp = require "cmp"
-      cmp.setup({
+      cmp.setup {
         window = {
           completion = {
-            border = 'none',
+            border = 'single',
           },
           documentation = {
             border = 'double',
           },
+        },
+        experimental = {
+          ghost_text = false,
         },
         snippet = {
           expand = function(args)
@@ -42,37 +48,26 @@ return {
         mapping = cmp.mapping.preset.insert({
           ["<C-p>"] = cmp.mapping.select_prev_item(),
           ["<C-n>"] = cmp.mapping.select_next_item(),
-          ["<S-TAB>"] = cmp.mapping.select_prev_item(),
-          ["<TAB>"] = cmp.mapping.select_next_item(),
           ["<C-l>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
           ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-          ["<CR>" ] = cmp.mapping.confirm({ select = false }),
+          ["<S-TAB>"] = cmp.mapping.select_prev_item(),
+          ["<TAB>"] = cmp.mapping.select_next_item(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
         }),
-        experimental = {
-          ghost_text = false,
-        },
-        formatting = {
-          format = lspkind.cmp_format({
-            mode = "symbol",
-            maxwidth = 50,
-            ellipsis_char = "...",
-          })
-        }
-      })
+      }
       cmp.setup.cmdline({ '/', '?' }, {
         mapping = cmp.mapping.preset.cmdline(),
-        sources = {
-          { name = 'buffer' }
+        sources = cmp.config.sources {
+          { name = 'buffer' },
         }
       })
       cmp.setup.cmdline(':', {
         mapping = cmp.mapping.preset.cmdline(),
-        sources = cmp.config.sources({
-          { name = 'path' }
-        }, {
-          { name = 'cmdline' }
-        }),
+        sources = cmp.config.sources {
+          { name = 'path' },
+          { name = 'cmdline' },
+        },
         matching = { disallow_symbol_nonprefix_matching = false }
       })
     end,
--- files/.config/nvim/lua/lazyvim/plugins/lsp/mason.lua
+++ files/.config/nvim/lua/lazyvim/plugins/lsp/mason.lua
@@ -12,20 +12,6 @@ return {
       "neovim/nvim-lspconfig",
     },
     config = function()
-      local on_attach = function(client, bufnr)
-        local set = vim.keymap.set
-        set("n", "<LEADER>lgd", "<CMD>lua vim.lsp.buf.definition()<CR>")
-        set("n", "<LEADER>lK", "<CMD>lua vim.lsp.buf.hover()<CR>")
-        set("n", "<LEADER>l<C-m>", "<CMD>lua vim.lsp.buf.signature_help()<CR>")
-        set("n", "<LEADER>lgy", "<CMD>lua vim.lsp.buf.type_definition()<CR>")
-        set("n", "<LEADER>lrn", "<CMD>lua vim.lsp.buf.rename()<CR>")
-        set("n", "<LEADER>lma", "<CMD>lua vim.lsp.buf.code_action()<CR>")
-        set("n", "<LEADER>lgr", "<CMD>lua vim.lsp.buf.references()<CR>")
-        set("n", "<LEADER>le", "<CMD>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>")
-        set("n", "<LEADER>l[d", "<CMD>lua vim.lsp.diagnostic.goto_prev()<CR>")
-        set("n", "<LEADER>l]d", "<CMD>lua vim.lsp.diagnostic.goto_next()<CR>")
-      end
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
       require("mason-lspconfig").setup {
         ensure_installed = {
           "lua_ls",
@@ -35,8 +21,21 @@ return {
       require("mason-lspconfig").setup_handlers {
         function(server_name)
           require("lspconfig")[server_name].setup {
-            on_attach = on_attach,
-            capabilities = capabilities,
+            on_attach = function()
+              local set = vim.keymap.set
+
+              set("n", "<LEADER>lgd", "<CMD>lua vim.lsp.buf.definition()<CR>")
+              set("n", "<LEADER>lK", "<CMD>lua vim.lsp.buf.hover()<CR>")
+              set("n", "<LEADER>l<C-m>", "<CMD>lua vim.lsp.buf.signature_help()<CR>")
+              set("n", "<LEADER>lgy", "<CMD>lua vim.lsp.buf.type_definition()<CR>")
+              set("n", "<LEADER>lrn", "<CMD>lua vim.lsp.buf.rename()<CR>")
+              set("n", "<LEADER>lma", "<CMD>lua vim.lsp.buf.code_action()<CR>")
+              set("n", "<LEADER>lgr", "<CMD>lua vim.lsp.buf.references()<CR>")
+              set("n", "<LEADER>le", "<CMD>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>")
+              set("n", "<LEADER>l[d", "<CMD>lua vim.lsp.diagnostic.goto_prev()<CR>")
+              set("n", "<LEADER>l]d", "<CMD>lua vim.lsp.diagnostic.goto_next()<CR>")
+            end,
+            capabilities = require("cmp_nvim_lsp").default_capabilities(),
           }
         end,
       }
