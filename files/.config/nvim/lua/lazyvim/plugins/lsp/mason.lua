a3fd42f226db4522ea774a50ccad3e160e8f91fb
n_k <nimu>
2024-09-29 21:38:29 +0900

    update

--- files/.config/nvim/lua/lazyvim/plugins/lsp/mason.lua
+++ files/.config/nvim/lua/lazyvim/plugins/lsp/mason.lua
@@ -2,35 +2,28 @@ return {
   {
     "williamboman/mason.nvim",
     event = "VeryLazy",
-    cmd = {
-      "Mason",
-      "MasonInstall",
-      "MasonUninstall",
-      "MasonUninstallAll",
-      "MasonLog",
-      "MasonUpdate",
-    },
     config = true,
   },
   {
     "williamboman/mason-lspconfig.nvim",
     event = "VeryLazy",
     dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
       "neovim/nvim-lspconfig",
     },
     config = function()
       local on_attach = function(client, bufnr)
         local set = vim.keymap.set
-        set("n", "<leader>lgd", "<cmd>lua vim.lsp.buf.definition()<CR>")
-        set("n", "<leader>lK", "<cmd>lua vim.lsp.buf.hover()<CR>")
-        set("n", "<leader>l<C-m>", "<cmd>lua vim.lsp.buf.signature_help()<CR>")
-        set("n", "<leader>lgy", "<cmd>lua vim.lsp.buf.type_definition()<CR>")
-        set("n", "<leader>lrn", "<cmd>lua vim.lsp.buf.rename()<CR>")
-        set("n", "<leader>lma", "<cmd>lua vim.lsp.buf.code_action()<CR>")
-        set("n", "<leader>lgr", "<cmd>lua vim.lsp.buf.references()<CR>")
-        set("n", "<leader>le", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>")
-        set("n", "<leader>l[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>")
-        set("n", "<leader>l]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>")
+        set("n", "<LEADER>lgd", "<CMD>lua vim.lsp.buf.definition()<CR>")
+        set("n", "<LEADER>lK", "<CMD>lua vim.lsp.buf.hover()<CR>")
+        set("n", "<LEADER>l<C-m>", "<CMD>lua vim.lsp.buf.signature_help()<CR>")
+        set("n", "<LEADER>lgy", "<CMD>lua vim.lsp.buf.type_definition()<CR>")
+        set("n", "<LEADER>lrn", "<CMD>lua vim.lsp.buf.rename()<CR>")
+        set("n", "<LEADER>lma", "<CMD>lua vim.lsp.buf.code_action()<CR>")
+        set("n", "<LEADER>lgr", "<CMD>lua vim.lsp.buf.references()<CR>")
+        set("n", "<LEADER>le", "<CMD>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>")
+        set("n", "<LEADER>l[d", "<CMD>lua vim.lsp.diagnostic.goto_prev()<CR>")
+        set("n", "<LEADER>l]d", "<CMD>lua vim.lsp.diagnostic.goto_next()<CR>")
       end
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
       require("mason-lspconfig").setup {
