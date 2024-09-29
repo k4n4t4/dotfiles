972b613f668284a3cc551adf859923ff491aac57
n_k <nimu>
2024-09-29 11:44:08 +0900

    update

--- files/.config/nvim/lua/lazyvim/plugins/highlights.lua
+++ files/.config/nvim/lua/lazyvim/plugins/highlights.lua
@@ -1,5 +1,22 @@
 return {
   {
+    "navarasu/onedark.nvim",
+    enabled = true,
+    opts = {
+      style = "darker",
+    },
+  },
+  {
+    "catppuccin/nvim",
+    enabled = true,
+    name = "catppuccin",
+  },
+  {
+    "folke/tokyonight.nvim",
+    enabled = true,
+  },
+
+  {
     "fladson/vim-kitty",
     ft = "kitty",
   },
@@ -11,5 +28,12 @@ return {
       "nvim-tree/nvim-web-devicons"
     },
     ft = "markdown",
+  },
+
+  {
+    "norcalli/nvim-colorizer.lua",
+    enabled = true,
+    event = "VeryLazy",
+    config = true,
   },
 }
--- files/.config/nvim/lua/lazyvim/plugins/others.lua
+++ files/.config/nvim/lua/lazyvim/plugins/others.lua
@@ -1,12 +1,6 @@
 return {
   { "dstein64/vim-startuptime" },
   {
-    "norcalli/nvim-colorizer.lua",
-    enabled = true,
-    event = "VeryLazy",
-    config = true,
-  },
-  {
     "windwp/nvim-autopairs",
     enabled = true,
     event = "InsertEnter",
