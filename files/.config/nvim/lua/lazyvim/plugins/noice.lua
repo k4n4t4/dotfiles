4a542662c81f71cf44bcebad87da82dcd9e48fe7
n_k <nimu>
2024-10-04 00:51:13 +0900

    update

---
+++ files/.config/nvim/lua/lazyvim/plugins/noice.lua
@@ -0,0 +1,88 @@
+return {
+  "folke/noice.nvim",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "rcarriga/nvim-notify",
+  },
+  event = "VeryLazy",
+  opts = {
+    cmdline = {
+      enabled = true,
+      view = 'cmdline',
+    },
+    messages = {
+      enabled = true,
+      view = 'mini',
+      view_error = 'mini',
+      view_warn = 'mini',
+      view_history = 'messages',
+      view_search = 'virtualtext',
+    },
+    popupmenu = {
+      enabled = true,
+      backend = "nui",
+    },
+    redirect = {
+      view = 'popup',
+    },
+    commands = {
+      history = {
+        view = 'split',
+      },
+      last = {
+        view = 'popup',
+      },
+      errors = {
+        view = 'popup',
+      },
+      all = {
+        view = 'split',
+      },
+    },
+    notify = {
+      enabled = true,
+      view = 'mini',
+    },
+    lsp = {
+      progress = {
+        enabled = true,
+        format_done = "lsp_progress_done",
+        throttle = 1000 / 30,
+        view = "mini",
+      },
+      override = {
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        ["vim.lsp.util.stylize_markdown"] = true,
+        ["cmp.entry.get_documentation"] = true,
+      },
+      hover = {
+        enabled = true,
+        silent = false,
+        view = nil,
+        opts = {},
+      },
+      signature = {
+        enabled = true,
+        auto_open = {
+          enabled = true,
+          trigger = true,
+          luasnip = true,
+          throttle = 50,
+        },
+        view = nil,
+      },
+      message = {
+        enabled = true,
+        view = "notify",
+      },
+    },
+    presets = {
+      bottom_search = true,
+      command_palette = true,
+      long_message_to_split = true,
+      inc_rename = true,
+      lsp_doc_border = true,
+    },
+    throttle = 1000 / 30,
+  },
+}
