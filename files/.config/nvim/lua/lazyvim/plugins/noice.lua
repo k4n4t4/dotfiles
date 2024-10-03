9bba01e676fe92bdb67b3d36a469d6369f4ce3ee
n_k <nimu>
2024-10-04 00:57:14 +0900

    update

--- files/.config/nvim/lua/lazyvim/plugins/noice.lua
+++ files/.config/nvim/lua/lazyvim/plugins/noice.lua
@@ -9,6 +9,15 @@ return {
     cmdline = {
       enabled = true,
       view = 'cmdline',
+      format = {
+        cmdline = { pattern = "^:", icon = "", lang = "vim" },
+        search_down = { kind = "search", pattern = "^/", icon = "", lang = "regex" },
+        search_up = { kind = "search", pattern = "^%?", icon = "", lang = "regex" },
+        filter = { pattern = "^:%s*!", icon = "$", lang = "bash" },
+        lua = { pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" }, icon = "", lang = "lua" },
+        help = { pattern = "^:%s*he?l?p?%s+", icon = "" },
+        input = { view = "cmdline_input", icon = "󰥻" },
+      },
     },
     messages = {
       enabled = true,
