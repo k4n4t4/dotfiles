3f3a0095de6d8507df461b54f09b622c4e29a105
n_k <nimu>
2024-10-03 22:43:28 +0900

    update

--- files/.config/nvim/lua/settings/keymaps.lua
+++ files/.config/nvim/lua/settings/keymaps.lua
@@ -8,7 +8,7 @@ vim.g.maplocalleader = " "
 set("n", "<ESC><ESC>", "<CMD>noh<CR>")
 set("t", "<ESC>", "<C-\\><C-N>")
 
-set("n", "<LEADER>c", "<CMD>belowright new<CR><CMD>terminal<CR>")
+set("n", "<LEADER>c", "<CMD>belowright 10split<CR><CMD>terminal<CR>")
 set("n", "<LEADER>C", "<CMD>terminal<CR>")
 
 set("n", "<LEADER>h", "viwo<CMD>let @/=getregion(getpos('v'), getpos('.'))[0]<CR><CMD>set hls<CR><ESC>")
