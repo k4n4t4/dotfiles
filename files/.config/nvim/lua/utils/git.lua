27bcce4495221116a273ee062eb1aed3e832df4d
n_k <nimu>
2024-10-06 02:52:40 +0900

    update

--- files/.config/nvim/lua/utils/git.lua
+++ files/.config/nvim/lua/utils/git.lua
@@ -1,8 +1,46 @@
 local M = {}
 
+M.data = {}
+
 ---@param bufnr number
-function M.get(bufnr)
-  return {}
+---@param callback? fun(data: any)
+function M.update(bufnr, callback)
+  M.data[bufnr] = M.data[bufnr] or {}
+
+  local system_promises = {
+    {
+      -- todo:  cd dir
+      cmd = { "git", "rev-parse", "--abbrev-ref", "@" },
+      opts = { stderr = false, timeout = 5000 },
+      on_exit = function(obj)
+        if obj.code == 0 then
+          M.data[bufnr].branch = obj.stdout
+        end
+      end,
+    },
+  }
+
+  local count = #system_promises
+  for _, system in pairs(system_promises) do
+    vim.system(system.cmd, system.opts, function(obj)
+      system.on_exit(obj)
+      count = count - 1
+      if count == 0 then
+        if callback then
+          callback(M.data[bufnr])
+        end
+      end
+    end)
+  end
+
 end
+
+function M.get(bufnr)
+  return M.data[bufnr]
+end
+
+M.update(0, function(data)
+  print(data.branch)
+end)
 
 return M
