b35befb55478bea566e0fbc25b4b40f6333a74d0
n_k <nimu>
2024-10-06 21:00:53 +0900

    update

--- files/greetd/login.sh
+++ files/greetd/login.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-PATH="$HOME/bin:$PATH"
-PATH="$HOME/.local/bin:$PATH"
-PATH="$HOME/.cargo/bin:$PATH"
-PATH="$HOME/go/bin:$PATH"
-
 if [ -e /home/linuxbrew/.linuxbrew/bin/brew ]; then
   eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 fi
+
+PATH="$HOME/.local/bin:$PATH"
+PATH="$HOME/.cargo/bin:$PATH"
+PATH="$HOME/go/bin:$PATH"
+PATH="$HOME/bin:$PATH"
 
 exec Hyprland > /dev/null
