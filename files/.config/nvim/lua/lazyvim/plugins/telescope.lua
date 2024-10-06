3059dcb04037d9bd7cc8a9c61e607d832a60eed2
n_k <nimu>
2024-10-06 21:37:32 +0900

    update

--- files/.config/nvim/lua/lazyvim/plugins/telescope.lua
+++ files/.config/nvim/lua/lazyvim/plugins/telescope.lua
@@ -12,6 +12,7 @@ return {
   config = function()
     local telescope = require "telescope"
     local actions = require "telescope.actions"
+    local project_actions = require "telescope._extensions.project.actions"
     telescope.setup {
       defaults = {
         borderchars = { '─', '│', '─', '│', '┌', '┐', '┘', '└' },
@@ -37,7 +38,50 @@ return {
             end
           end,
           show_columns = 'both',
-        }
+        },
+        project = {
+          base_dirs = {
+            '~/dotfiles',
+          },
+          hidden_files = true,
+          theme = "dropdown",
+          order_by = "asc",
+          search_by = "title",
+          sync_with_nvim_tree = true,
+          on_project_selected = function(prompt_bufnr)
+            project_actions.change_working_directory(prompt_bufnr, false)
+            require('neo-tree.command').execute {
+              action = "focus",
+            }
+          end,
+          mappings = {
+            n = {
+              ['d'] = project_actions.delete_project,
+              ['r'] = project_actions.rename_project,
+              ['c'] = project_actions.add_project,
+              ['C'] = project_actions.add_project_cwd,
+              ['f'] = project_actions.find_project_files,
+              ['b'] = project_actions.browse_project_files,
+              ['s'] = project_actions.search_in_project_files,
+              ['R'] = project_actions.recent_project_files,
+              ['w'] = project_actions.change_working_directory,
+              ['o'] = project_actions.next_cd_scope,
+            },
+            i = {
+              ['<c-d>'] = project_actions.delete_project,
+              ['<c-v>'] = project_actions.rename_project,
+              ['<c-a>'] = project_actions.add_project,
+              ['<c-A>'] = project_actions.add_project_cwd,
+              ['<c-f>'] = project_actions.find_project_files,
+              ['<c-b>'] = project_actions.browse_project_files,
+              ['<c-s>'] = project_actions.search_in_project_files,
+              ['<c-r>'] = project_actions.recent_project_files,
+              ['<c-l>'] = project_actions.change_working_directory,
+              ['<c-o>'] = project_actions.next_cd_scope,
+              ['<c-w>'] = project_actions.change_workspace,
+            }
+          }
+        },
       },
     }
     telescope.load_extension "fzf"
