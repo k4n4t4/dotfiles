f43b4327d21ad6e59ad3bbe3c4249efb7567c718
n_k <nimu>
2024-10-06 22:42:00 +0900

    update

--- files/.config/nvim/lua/lazyvim/plugins/alpha.lua
+++ files/.config/nvim/lua/lazyvim/plugins/alpha.lua
@@ -8,17 +8,40 @@ return {
     local dashboard = require("alpha.themes.dashboard")
 
     dashboard.section.header.val = {
-      [[█◣  █             █   █        ]],
-      [[█◥◣ █ ▄▄▄▄▄ █▀▀▀█ █   █ ▀ ▄▄▄▄▄]],
-      [[█ ◥◣█ █▄▄▄█ █   █ ◥◣ ◢◤ █ █ █ █]],
-      [[█  ◥█ █▄▄▄▄ █▄▄▄█  ◥█◤  █ █ █ █]],
+      [[                                   ]],
+      [[█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█]],
+      [[█ █◣  █             █   █         █]],
+      [[█ █◥◣ █ ▄▄▄▄▄ █▀▀▀█ █   █ ▀ ▄▄▄▄▄ █]],
+      [[█ █ ◥◣█ █▄▄▄█ █   █ ◥◣ ◢◤ █ █ █ █ █]],
+      [[█ █  ◥█ █▄▄▄▄ █▄▄▄█  ◥█◤  █ █ █ █ █]],
+      [[█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█]],
+      (function()
+        local space = [[NeoVim                             ]]
+
+        local version = vim.version()
+        local format = {
+          "Version ",
+          version.major,
+          ".",
+          version.minor,
+          ".",
+          version.patch,
+        }
+        local str = table.concat(format, "")
+
+        if #space < #str then
+          return str
+        end
+        return string.sub(space, 1, #space - #str) .. str
+      end)(),
     }
 
     dashboard.section.buttons.val = {
       dashboard.button("n", "  New File", "<CMD>enew<CR>"),
       dashboard.button("r", "  Recent File", "<CMD>Telescope oldfiles<CR>"),
-      dashboard.button("f", "󰥨  Find File", "<CMD>Telescope find_files<CR>"),
-      dashboard.button("g", "󰱼  Find Text", "<CMD>Telescope live_grep<CR>"),
+      dashboard.button("f", "󰈞  Find File", "<CMD>Telescope find_files<CR>"),
+      dashboard.button("g", "󱎸  Find Text", "<CMD>Telescope live_grep<CR>"),
+      dashboard.button("t", "  Projects", "<CMD>Telescope project<CR>"),
       dashboard.button("p", "  Plugins", "<CMD>Lazy<CR>"),
       dashboard.button("u", "  Update Plugins", "<CMD>Lazy sync<CR>"),
       dashboard.button("s", "  Settings", "<CMD>e $MYVIMRC<CR><CMD>silent :cd %:p:h<CR>"),
