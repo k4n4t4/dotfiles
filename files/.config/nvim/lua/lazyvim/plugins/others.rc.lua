43b11a44e7755cb8d16377b7e79137d06e942749
n_k <nimu>
2024-10-08 21:54:19 +0900

    update

--- files/.config/nvim/lua/lazyvim/plugins/others.rc.lua
+++ files/.config/nvim/lua/lazyvim/plugins/others.rc.lua
@@ -77,6 +77,37 @@ return {
     event = 'VeryLazy',
   },
 
+  {
+    "shellRaining/hlchunk.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      require("hlchunk").setup {
+        chunk = {
+          enable = true,
+          style = {
+            { fg = "#22A0A0" },
+            { fg = "#CC2233" },
+          },
+          use_treesitter = true,
+          chars = {
+            horizontal_line = "─",
+            vertical_line = "│",
+            left_top = "┌",
+            left_bottom = "└",
+            right_arrow = ">",
+          },
+          textobject = "",
+          max_file_size = 1024 * 1024,
+          error_sign = true,
+          delay = 0,
+        },
+        indent = {
+          enable = false,
+        }
+      }
+    end
+  },
+
 
   {
     "dstein64/vim-startuptime",
