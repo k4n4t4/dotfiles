dc2b75f81eca7a43861f8ec917d7f638c0b41ad8
n_k <nimu>
2024-10-11 14:36:31 +0900

    update

--- files/.config/nvim/lua/lazyvim/plugins/others.rc.lua
+++ files/.config/nvim/lua/lazyvim/plugins/others.rc.lua
@@ -283,4 +283,21 @@ return {
     end,
     event = 'VeryLazy',
   },
+
+  {
+    "luukvbaal/statuscol.nvim",
+    config = function()
+      local builtin = require("statuscol.builtin")
+      vim.opt.foldcolumn = '1'
+      require("statuscol").setup({
+        segments = {
+          { text = { "%s" } },
+          { text = { builtin.lnumfunc } },
+          { text = { builtin.foldfunc } },
+          { text = { "|" } },
+        }
+      })
+    end,
+    event = 'VeryLazy',
+  },
 }
