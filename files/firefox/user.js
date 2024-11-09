5ac12640cc848113221829ccfc23ce8f5c6c166f
nimu <n_k>
2024-11-09 10:03:44 +0900

    update

--- files/firefox/user.js
+++ files/firefox/user.js
@@ -5,3 +5,5 @@ user_pref("browser.startup.homepage", "about:home");
 user_pref("browser.download.dir", "~/tmp");
 user_pref("browser.theme.dark-private-windows", true);
 user_pref("browser.tabs.hoverPreview.enabled", true);
+user_pref("browser.compactmode.show", true);
+user_pref("extensions.pocket.enabled", false);
