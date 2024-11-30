b559837b5ce4c75c23220a9c1fa93a85830c70bd
nimu <n_k>
2024-11-30 22:55:14 +0900

    update

--- files/.config/ags/widget/Bar/title.tsx
+++ files/.config/ags/widget/Bar/title.tsx
@@ -7,20 +7,14 @@ export default function BarTitle(): JSX.Element {
   const hyprland = Hyprland.get_default()
 
   let tmp_label = ""
-  const label = bind(Variable.derive([
-    bind(hyprland, 'focused_client'),
-    bind(hyprland, 'clients'),
-  ], (active, clients) => {
-    if (clients.length === 0) {
-      tmp_label = ""
-    }
+  const label = bind(hyprland, 'focused_client').as(active => {
     if (active?.title) {
       tmp_label = active.title
       return active.title
     } else {
       return tmp_label
     }
-  }))
+  })
 
   return (
     <box className="bar-title" >
