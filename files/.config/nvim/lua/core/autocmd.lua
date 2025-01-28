7bdaee0866da8332348d093699669155cfaa3dfc
nimu <n_k>
2025-01-28 15:42:10 +0900

    update

--- files/.config/nvim/lua/core/autocmd.lua
+++ files/.config/nvim/lua/core/autocmd.lua
@@ -81,3 +81,17 @@ autocmd("BufEnter", {
     end
   end
 })
+
+
+-- fcitx
+if vim.fn.executable("fcitx5") then
+  autocmd("InsertLeave", {
+    group = group,
+    callback = function()
+      local out = vim.fn.system { "fcitx5-remote" }
+      if out == "2\n" then
+        vim.fn.system { "fcitx5-remote", "-c" }
+      end
+    end
+  })
+end
