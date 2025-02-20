833b839a534fa41c51bc2754de304553808d460e
nimu <n_k>
2025-02-20 20:18:12 +0900

    update

--- files/.config/fish/conf.d/cd.fish
+++ files/.config/fish/conf.d/cd.fish
@@ -1,8 +1,9 @@
+functions --copy cd __cd
+
 function cd
   set -l old_pwd $PWD
 
-  builtin cd $argv
-
+  __cd $argv
   if test "$old_pwd" != "$PWD"
     printf "\033[48;5;94m\033[38;5;214m\033[1m "
     printf "CD"
