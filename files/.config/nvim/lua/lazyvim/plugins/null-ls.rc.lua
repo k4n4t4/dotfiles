cb2ed72fd4595b619e82382dd3de698ab42885b8
nimu <n_k>
2025-04-01 03:36:12 +0900

    update

--- files/.config/nvim/lua/lazyvim/plugins/null-ls.rc.lua
+++ files/.config/nvim/lua/lazyvim/plugins/null-ls.rc.lua
@@ -1,13 +1,11 @@
 return {
   "nvimtools/none-ls.nvim",
-  enabled = false,
+  enabled = true,
 
   config = function()
     local null_ls = require "null-ls"
     null_ls.setup {
       sources = {
-        null_ls.builtins.completion.spell,
-        null_ls.builtins.completion.tags,
         null_ls.builtins.diagnostics.buf,
       },
     }
