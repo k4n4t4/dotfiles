b8e927f3a9e7004be5d3a5d949191573c86b2d0f
kana <kana>
2025-04-20 13:17:20 +0000

    update

--- files/nix-home/flake.nix
+++ files/nix-home/flake.nix
@@ -1,32 +1,53 @@
 {
   description = "Home Configuration";
 
-
   inputs = {
-
     nixpkgs = {
       url = "github:NixOS/nixpkgs/nixos-unstable";
     };
-
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
   };
 
+  outputs = { self, nixpkgs, home-manager, ... } @ inputs: let
+    config = {
+      username = "kanata";
 
-  outputs = { self, nixpkgs, home-manager, ... } @ inputs:
-  {
+      system = "x86_64-linux";
+      # system = "aarch64-linux";
+      # system = "x86_64-darwin";
+      # system = "aarch64-darwin";
+
+      version = "24.11";
+    };
+
+    isLinux = config.system == "x86_64-linux" || config.system == "aarch64-linux";
+    isDarwin = config.system == "x86_64-darwin" || config.system == "aarch64-darwin";
+
+    home = {
+      stateVersion = config.version;
+      username = config.username;
+      homeDirectory =
+        if isLinux then
+          "/home/${config.username}"
+        else if isDarwin then
+          "/Users/${config.username}"
+        else
+          throw "\"${config.system}\" is unsupported system"
+        ;
+    };
+  in {
     homeConfigurations = {
       "desktop" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages."x86_64-linux";
+        pkgs = nixpkgs.legacyPackages.${config.system};
         modules = [
+          { inherit home; }
           ./homes/common.nix
           ./homes/desktop.nix
         ];
       };
     };
   };
-
 }
--- files/nix-home/homes/desktop.nix
+++ files/nix-home/homes/desktop.nix
@@ -1,19 +1,8 @@
 { config, pkgs, ... }:
-let
-  version = "24.05";
-  username = "kanata";
-in
 {
-  programs.home-manager.enable = true;
-
   fonts.fontconfig.enable = true;
 
   home = {
-    stateVersion = "${version}";
-
-    username = "${username}";
-    homeDirectory = "/home/${username}";
-
     packages = with pkgs; [
       fish starship
       neovim luajit luarocks
