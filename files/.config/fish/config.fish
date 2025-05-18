3f7c2f4be8bf656545b391f8af7c0e9916fa65cd
kana <kana>
2025-05-18 14:56:20 +0000

    update

--- files/.config/fish/config.fish
+++ files/.config/fish/config.fish
@@ -1,4 +1,8 @@
-export fish_dir=(dirname (status --current-filename))
+if status --is-interactive
+  export fish_dir=(dirname (status --current-filename))
 
-source $fish_dir/settings.fish
-source $fish_dir/aliases.fish
+  source $fish_dir/settings.fish
+  source $fish_dir/aliases.fish
+else
+  echo 1
+end
