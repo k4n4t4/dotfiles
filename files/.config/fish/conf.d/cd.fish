3b54de09fcd314c1172e37356d8e7d6a334ecafc
kana <kana>
2025-05-19 15:06:11 +0000

    update

--- files/.config/fish/conf.d/cd.fish
+++ files/.config/fish/conf.d/cd.fish
@@ -1,14 +1,16 @@
-functions --copy cd __cd
+if status is-interactive
+  functions --copy cd __cd
 
-function cd
-  set -l old_pwd $PWD
+  function cd
+    set -l old_pwd $PWD
 
-  __cd $argv
-  if test "$old_pwd" != "$PWD"
-    printf "\033[48;5;94m\033[38;5;214m\033[1m "
-    printf "CD"
-    printf " \033[38;5;94m\033[48;5;58m\033[38;5;214m\033[1m "
-    printf "$old_pwd 󰁕 $PWD"
-    printf " \033[m\033[38;5;58m\033[m\n"
+    __cd $argv
+    if test "$old_pwd" != "$PWD"
+      printf "\033[48;5;94m\033[38;5;214m\033[1m "
+      printf "CD"
+      printf " \033[38;5;94m\033[48;5;58m\033[38;5;214m\033[1m "
+      printf "$old_pwd 󰁕 $PWD"
+      printf " \033[m\033[38;5;58m\033[m\n"
+    end
   end
 end
