a16622da1edb9d6a06fc0dae813ca2d71ebd109f
kana <kana>
2025-05-25 06:24:06 +0000

    update

--- scripts/default.sh
+++ scripts/default.sh
@@ -53,13 +53,7 @@ if cmd_exist wlogout; then
 fi
 
 if cmd_exist firefox; then
-  for folder in ~/.mozilla/firefox/*.default-release; do
-    if [ -d "$folder" ]; then
-      dot ".config/firefox/user.js" "$folder/user.js"
-      dot ".config/firefox/userChrome.css" "$folder/chrome/userChrome.css"
-      dot ".config/firefox/userContent.css" "$folder/chrome/userContent.css"
-    fi
-  done
+  source_script firefox
 fi
 
 if cmd_exist git; then
