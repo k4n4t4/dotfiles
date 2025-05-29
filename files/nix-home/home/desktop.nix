718bfe3b6b5ecfe6c8a27c52e664cd9ffb8e9923
kana <kana>
2025-05-29 12:19:53 +0000

    update

--- files/nix-home/home/desktop.nix
+++ files/nix-home/home/desktop.nix
@@ -8,8 +8,17 @@
       dash
       zsh
       fish starship
-      neovim luajit luarocks
       eza fd ripgrep fzf zoxide btop ncdu delta trash-cli tmux
+    ];
+  };
+
+  programs.neovim = {
+    enable = true;
+    extraPackages = with pkgs; [
+      lua-language-server
+      markdownlint-cli
+      luarocks
+      luajit
     ];
   };
 }
