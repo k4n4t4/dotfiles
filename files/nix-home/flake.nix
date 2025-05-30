afe2d6e3c34a8bf1728347c2dcef999647eeab83
kana <kana>
2025-05-31 00:45:08 +0900

    update

--- files/nix-home/flake.nix
+++ files/nix-home/flake.nix
@@ -9,6 +9,9 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    hyprland = {
+      url = "github:hyprwm/Hyprland";
+    };
   };
 
   outputs = { self, nixpkgs, home-manager, ... } @ inputs: let
--- files/nix-home/home/desktop.nix
+++ files/nix-home/home/desktop.nix
@@ -1,4 +1,4 @@
-{ inputs, pkgs, ... }:
+{ inputs, pkgs, lib, config, ... }:
 {
   fonts.fontconfig.enable = true;
 
