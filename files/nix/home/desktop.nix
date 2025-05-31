20797f6259cb127d7a0784d5a052de42fa2ebd60
kana <kana>
2025-05-31 20:26:05 +0900

    update

--- files/nix/home/desktop.nix
+++ files/nix/home/desktop.nix
@@ -4,6 +4,14 @@
     inputs.ags.homeManagerModules.default
   ];
 
+  i18n.inputMethod = {
+    enabled = "fcitx5";
+    fcitx5.addons = with pkgs; [
+      fcitx5-mozc
+      fcitx5-gtk
+    ];
+  };
+
   home = {
     packages = with pkgs; [
       uwsm
@@ -27,6 +35,9 @@
       alsa-utils
       brightnessctl
 
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-emoji
       nerd-fonts.comic-shanns-mono
       nerd-fonts.hurmit
     ];
@@ -38,6 +49,48 @@
       name = "rose-pine-cursor";
       size = 300;
     };
+
+    sessionVariables = {
+      GTK_IM_MODULE = "fcitx";
+      QT_IM_MODULE = "fcitx";
+      XMODIFIERS = "@im=fcitx";
+    };
+    file.".config/fcitx5/profile".text = ''
+      [Groups/0]
+      Name=Default
+      Default Layout=jp
+      DefaultIM=mozc
+
+      [Groups/0/Items/0]
+      Name=keyboard-jp
+      Layout=
+
+      [Groups/0/Items/1]
+      Name=mozc
+      Layout=
+
+      [GroupOrder]
+      0=Default
+    '';
+    file.".config/fcitx5/conf/classicui.conf".text = ''
+      Vertical Candidate List=False
+      WheelForPaging=True
+      Font="Sans 10"
+      MenuFont="Sans 10"
+      TrayFont="Sans Bold 10"
+      TrayOutlineColor=#000000
+      TrayTextColor=#ffffff
+      PreferTextIcon=False
+      ShowLayoutNameInIcon=True
+      UseInputMethodLanguageToDisplayText=True
+      Theme=default-dark
+      DarkTheme=default-dark
+      UseDarkTheme=False
+      UseAccentColor=True
+      PerScreenDPI=False
+      ForceWaylandDPI=0
+      EnableFractionalScale=True
+    '';
   };
 
   programs.neovim = {
