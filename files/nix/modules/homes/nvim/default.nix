4c99eb82e3b56e9605ed5f88bc8e111ffc33d416
kana <kana>
2025-06-05 19:44:16 +0900

    update

--- files/nix/modules/homes/nvim/default.nix
+++ files/nix/modules/homes/nvim/default.nix
@@ -3,16 +3,27 @@
   programs.neovim = {
     enable = true;
     extraPackages = with pkgs; [
+      git
+      wget
+      tree-sitter
+      curl
       unzip
       nodejs
       python3
       gnumake
       gcc
-      luajit
-      luarocks
+      fd
+      ripgrep
+      fzf
+      nodePackages.npm
+      luajitPackages.luarocks
+      lua51Packages.lua
       lua-language-server
       rust-analyzer
+      cargo
       markdownlint-cli
+      libxml2
+      imagemagick
     ];
   };
 }
