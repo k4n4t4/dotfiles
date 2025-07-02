256974f2a06e7bc71fee402b21c9e05a3a2890c5
kana <k>
2025-07-02 07:44:14 +0000

    update

--- files/.config/nvim/lua/core/options.lua
+++ files/.config/nvim/lua/core/options.lua
@@ -3,13 +3,13 @@ vim.scriptencoding   = "utf-8"
 vim.opt.fileencoding = "utf-8"
 vim.opt.fileencodings = {
   "utf-8",
+  "utf-16",
   "ucs2le",
   "ucs-2",
   "iso-2022-jp",
   "euc-jp",
   "sjis",
   "cp932",
-  "utf-16",
 }
 vim.opt.fileformats = { "unix", "dos", "mac" }
 
