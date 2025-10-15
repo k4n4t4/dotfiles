5d744a67e674e8d2c864ac2dc9d02199b367eec3
kana <kana>
2025-10-15 17:29:53 +0900

    update

--- files/.config/ags/utils/index.tsx
+++ files/.config/ags/utils/index.tsx
@@ -1,7 +1,7 @@
 import { createPoll } from "ags/time"
 import GLib from "gi://GLib?version=2.0"
 
-export {default as Clickable} from "./Clickable"
+export * from "./elements"
 
 export function createDateTime(interval: number = 1000) {
   return createPoll<GLib.DateTime>(GLib.DateTime.new_now_local(), interval, () => GLib.DateTime.new_now_local())
