93b34307df6cc9c75ea668a34687bb3bf17fa946
kana <k>
2025-10-24 08:28:05 +0000

    update

--- files/nix/modules/hosts/docker/default.nix
+++ files/nix/modules/hosts/docker/default.nix
@@ -4,6 +4,10 @@
     enable = true;
   };
 
+  environment.systemPackages = with pkgs; [
+    docker-buildx 
+  ];
+
   users.users = lib.mapAttrs (name: value: {
     extraGroups = [ "docker" ];
   }) users;
