afafedb747fcc92c07ecc1b6bb105ab0d666025c
kana <k>
2025-11-02 17:36:46 +0000

    update

--- scripts/default.sh
+++ scripts/default.sh
@@ -1,7 +1,6 @@
 alias dothome="dot --origin-prefix=home"
 alias dotconf="dot --origin-prefix=home/.config --target-prefix=.config"
 
-dot "$WORK_PATH/dm" ".local/bin/dm"
 dothome "bin" ".local/bin" -r
 dothome ".fonts.conf"
 dothome ".profile"
