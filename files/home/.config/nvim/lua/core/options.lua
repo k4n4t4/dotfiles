8bbea527811b9fd2072b70e874c3d874277718d3
kana <k>
2025-11-03 15:20:50 +0000

    update

--- files/home/.config/nvim/lua/core/options.lua
+++ files/home/.config/nvim/lua/core/options.lua
@@ -150,6 +150,9 @@ vim.opt.fillchars = {
 }
 
 vim.diagnostic.config {
+  virtual_text = true,
+  update_in_insert = false,
+  severity_sort = true,
   signs = {
     text = {
       [vim.diagnostic.severity.ERROR] = "!";
