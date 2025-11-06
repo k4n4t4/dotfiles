bb144da7b9d947a4488a941d079dc0ad718a19ad
kana <k>
2025-11-06 15:32:42 +0000

    update

--- files/home/.config/nvim/lua/lazyvim/config/lsp/lua_ls.lua
+++ files/home/.config/nvim/lua/lazyvim/config/lsp/lua_ls.lua
@@ -15,11 +15,16 @@ return {
         path = { "?.lua", "?/init.lua" };
       };
       workspace = {
-        library = is_nvim and vim.list_extend(vim.api.nvim_get_runtime_file("lua", true), {
-          "${3rd}/luv/library",
-          "${3rd}/busted/library",
-          "${3rd}/luassert/library",
-        }) or {};
+        library = is_nvim and (
+          vim.list_extend(
+            vim.api.nvim_get_runtime_file("lua", true),
+            {
+              "${3rd}/luv/library",
+              "${3rd}/busted/library",
+              "${3rd}/luassert/library",
+            }
+          )
+        ) or {};
         checkThirdParty = "Disable";
       };
       diagnostics = {
