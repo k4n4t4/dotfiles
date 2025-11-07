ce3f87c509f9851d56344c571b4b44e0794155ae
kana <k>
2025-11-07 10:50:25 +0000

    update

--- files/home/.config/nvim/lua/lazyvim/config/mason.lua
+++ files/home/.config/nvim/lua/lazyvim/config/mason.lua
@@ -1,11 +1,14 @@
 return function()
   local mason_lspconfig = require("mason-lspconfig")
 
-  vim.lsp.config("lua_ls", require("lazyvim.config.lsp.lua_ls"))
-  vim.lsp.enable("lua_ls")
-
-  vim.lsp.config("emmet_language_server", require("lazyvim.config.lsp.emmet_language_server"))
-  vim.lsp.enable("emmet_language_server")
+  local lsps = {
+    "lua_ls",
+    "emmet_language_server"
+  }
+  for _, value in ipairs(lsps) do
+    vim.lsp.config(value, require("lazyvim.config.lsp." .. value))
+    vim.lsp.enable(value)
+  end
 
   mason_lspconfig.setup {
     ensure_installed = {
