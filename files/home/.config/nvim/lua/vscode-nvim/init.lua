290b32da62db24f56ac1fafcf69d8c3ed426c399
kana <k>
2025-11-12 10:23:30 +0000

    update

--- files/home/.config/nvim/lua/vscode-nvim/init.lua
+++ files/home/.config/nvim/lua/vscode-nvim/init.lua
@@ -1,4 +1,37 @@
+local vscode = require('vscode')
+
 require "core.options"
 require "core.keymaps"
 
 vim.opt.cmdheight = 2
+
+local keymap = vim.keymap
+local set = keymap.set
+
+-- fold
+set('n', 'zc', function()
+  vscode.action("editor.fold")
+end)
+set('n', 'zC', function()
+  vscode.action("editor.foldRecursively")
+end)
+set('n', 'zM', function()
+  vscode.action("editor.foldAll")
+end)
+set('n', 'zo', function()
+  vscode.action("editor.unfold")
+end)
+set('n', 'zO', function()
+  vscode.action("editor.unfoldRecursively")
+end)
+set('n', 'zR', function()
+  vscode.action("editor.unfoldAll")
+end)
+set('n', 'za', function()
+  vscode.action("editor.toggleFold")
+end)
+
+-- tree
+set('n', '<LEADER>e', function()
+  vscode.action("workbench.action.toggleSidebarVisibility")
+end)
