38488a3ebe344194bfcef68d079b238b3dd44b66
kana <k>
2025-11-27 12:56:25 +0000

    update

--- files/home/.config/nvim/lua/core/ui/statuscolumn/init.lua
+++ files/home/.config/nvim/lua/core/ui/statuscolumn/init.lua
@@ -10,7 +10,7 @@ vim.opt.numberwidth = 4
 vim.opt.cursorline = true
 vim.opt.cursorlineopt = "number"
 vim.opt.cursorcolumn = false
-vim.opt.colorcolumn = { 80, 160 }
+-- vim.opt.colorcolumn = { 80, 160 }
 
 vim.opt.foldcolumn = "0"
 vim.opt.statuscolumn = "%{% g:actual_curwin == win_getid() ? v:lua.StatusColumn() : v:lua.StatusColumnInactive() %}"
