99e936985a6a56775c73b470a8f04e9ce7804531
kana <k>
2025-12-24 06:45:34 +0000

    update

--- files/home/.config/nvim/lua/core/options/common.lua
+++ files/home/.config/nvim/lua/core/options/common.lua
@@ -1,3 +1,5 @@
+vim.opt.debug = 'throw'
+
 vim.opt.encoding     = "utf-8"
 vim.scriptencoding   = "utf-8"
 vim.opt.fileencoding = "utf-8"
@@ -17,14 +19,25 @@ vim.opt.fileencodings = {
 vim.opt.fileformat = "unix"
 vim.opt.fileformats = { "unix", "dos", "mac" }
 
-vim.opt.ambiwidth = "single"
+vim.opt.history = 1000
 
+vim.opt.title = true
+vim.opt.mouse = "a"
+vim.opt.clipboard:append "unnamedplus"
 
-vim.opt.debug = 'throw'
-
+vim.opt.shortmess:append 'I'
 
 vim.opt.termguicolors = true
 vim.opt.background = "dark"
+
+
+vim.opt.ambiwidth = "single"
+
+vim.opt.synmaxcol = 1000
+
+vim.opt.belloff = ""
+vim.opt.visualbell = true
+vim.opt.errorbells = false
 
 vim.opt.cmdwinheight = 10
 
@@ -34,87 +47,54 @@ vim.opt.pumblend = 20
 vim.opt.pumheight = 10
 vim.opt.pumwidth = 10
 
-vim.opt.wrap = true
-vim.opt.display = "lastline"
-vim.opt.breakat = " ^I!@*-+;:,./?"
-vim.opt.linebreak = false
-vim.opt.breakindent = false
-vim.opt.showbreak = ""
-
 vim.opt.wildmenu = true
 vim.opt.wildignorecase = true
 vim.opt.wildmode = "list:full"
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
 
 vim.opt.scroll = 10
 vim.opt.scrolloff = 3
 vim.opt.sidescroll = 1
 vim.opt.sidescrolloff = 3
 
-vim.opt.spell = false
-vim.opt.spelllang = { "en", "ja" }
+vim.opt.wrap = true
+vim.opt.display = "lastline"
+vim.opt.breakat = " ^I!@*-+;:,./?"
+vim.opt.linebreak = false
+vim.opt.breakindent = true
+vim.opt.showbreak = ""
 
-vim.opt.belloff = "all"
-vim.opt.visualbell = true
-vim.opt.errorbells = false
-
-vim.opt.shortmess:append 'I'
-
-vim.opt.smarttab = true
-vim.opt.expandtab = true
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.smartindent = true
-vim.opt.autoindent = true
-
-
+-- match
 vim.opt.hlsearch = true
 vim.opt.incsearch = true
 vim.opt.wrapscan = true
+vim.opt.startofline = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
+vim.opt.gdefault = false
 vim.opt.inccommand = "split"
-
-
-vim.opt.directory = vim.fn.stdpath("state") .. "/swap//"
-vim.opt.undodir = vim.fn.stdpath("state") .. "/undo//"
-vim.opt.backupdir = vim.fn.stdpath("state") .. "/backup//"
-
-vim.opt.swapfile = true
-vim.opt.undofile = true
-vim.opt.backup = true
-vim.opt.writebackup = true
-vim.opt.backupext = ".bak"
-
-
-vim.opt.title = true
-vim.opt.mouse = "a"
-vim.opt.clipboard:append "unnamedplus"
+vim.opt.magic = true
 
 vim.opt.autoread = true
-
 vim.opt.hidden = true
-
 vim.opt.confirm = true
-
-vim.opt.splitbelow = true
-vim.opt.splitright = true
 
 vim.opt.virtualedit = "block"
 
-vim.opt.gdefault = false
 
 vim.opt.whichwrap = "b,s,~,<,>,[,]"
-
 vim.opt.backspace = { "indent", "eol", "nostop" }
-
 vim.opt.nrformats = { "bin", "octal", "hex" }
 
-vim.opt.showmatch = true
-vim.opt.matchtime = 1
-vim.opt.matchpairs = { "(:)", "{:}", "[:]", "<:>" }
 
-vim.opt.completeopt = { "menuone", "preview" }
+vim.opt.completeopt = {
+  "menu",
+  "menuone",
+  "preview",
+  "noselect"
+}
 
 vim.opt.viewoptions = {
   "folds",
@@ -122,7 +102,32 @@ vim.opt.viewoptions = {
   "localoptions"
 }
 
-vim.opt.synmaxcol = 1000
+vim.opt.swapfile = true
+vim.opt.undofile = true
+vim.opt.backup = true
+vim.opt.writebackup = true
+vim.opt.backupext = ".bak"
+
+vim.opt.directory = vim.fn.stdpath("state") .. "/swap//"
+vim.opt.undodir = vim.fn.stdpath("state") .. "/undo//"
+vim.opt.backupdir = vim.fn.stdpath("state") .. "/backup//"
+
+
+vim.opt.showmatch = true
+vim.opt.matchtime = 1
+vim.opt.matchpairs = { "(:)", "{:}", "[:]", "<:>" }
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.smarttab = true
+
+
+vim.opt.spell = false
+vim.opt.spelllang = { "en", "cjk" }
 
 
 vim.env.EDITOR = "nvim --server \"$NVIM\" --remote-tab"
