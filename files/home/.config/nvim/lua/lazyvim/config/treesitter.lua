87873e1d69c15e6126e1e96e01a3bdf131b2fa2d
kana <k>
2025-12-25 09:39:39 +0000

    update

--- files/home/.config/nvim/lua/lazyvim/config/treesitter.lua
+++ files/home/.config/nvim/lua/lazyvim/config/treesitter.lua
@@ -1,21 +1,53 @@
 return function()
-  require("nvim-treesitter.configs").setup {
-    sync_install = true;
-    auto_install = true;
-    ignore_install = {};
-    modules = {};
-    ensure_installed = {
-      "lua",
-      "vim",
-      "vimdoc",
-      "query",
-    };
-    highlight = {
-      enable = true;
-      disable = {};
-    };
-    indent = {
-      enable = true;
-    };
-  }
+    require("nvim-treesitter.configs").setup {
+        ignore_install = {};
+        modules = {};
+
+        sync_install = true;
+        auto_install = true;
+        ensure_installed = {
+            "lua",
+            "vim",
+            "vimdoc",
+            "query",
+        };
+        highlight = {
+            enable = true;
+        };
+        indent = {
+            enable = true;
+        };
+        fold = {
+            enable = true;
+        };
+        incremental_selection = {
+            enable = true;
+            keymaps = {
+                init_selection = "<CR>";
+                node_incremental = "<CR>";
+                scope_incremental = false;
+                node_decremental = "<BS>";
+            };
+        };
+        textobjects = {
+            select = {
+                enable = true;
+                lookahead = true;
+                keymaps = {
+                    ["af"] = "@function.outer";
+                    ["if"] = "@function.inner";
+                    ["ac"] = "@class.outer";
+                    ["ic"] = "@class.inner";
+                    ["ab"] = "@block.outer";
+                    ["ib"] = "@block.inner";
+                    ["aa"] = "@parameter.outer";
+                    ["ia"] = "@parameter.inner";
+                };
+            };
+        };
+    }
+
+    vim.opt.foldmethod = "expr"
+    vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+    vim.opt.foldenable = false;
 end
