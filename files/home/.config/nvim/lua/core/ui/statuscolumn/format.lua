d779cfe691140581bd53b5ba71007e3250897c91
kana <k>
2025-12-26 11:29:02 +0000

    update

--- files/home/.config/nvim/lua/core/ui/statuscolumn/format.lua
+++ files/home/.config/nvim/lua/core/ui/statuscolumn/format.lua
@@ -1,33 +1,32 @@
 local status = require("core.ui.statuscolumn.components")
 
 function StatusColumn()
-    if vim.wo.number then
-        local line_number = status.number()
-        local fold = status.fold()
-        local separator = status.separator()
-        return (
-            "%s" ..
-            "%=" ..
-            line_number ..
-            fold ..
-            separator
-        )
-    else
-        return ""
-    end
-end
+    local winid = vim.g.statusline_winid
+    local curwin = vim.api.nvim_get_current_win()
 
-function StatusColumnInactive()
-    if vim.wo.number then
-        local fold = status.fold()
-        return (
-            "%s" ..
-            "%=" ..
-            "%l" ..
-            fold ..
-            "│"
-        )
+    if winid == curwin then
+        if vim.wo[winid].number then
+            return (
+                "%s" ..
+                "%=" ..
+                status.number() ..
+                status.fold() ..
+                status.separator()
+            )
+        else
+            return ""
+        end
     else
-        return ""
+        if vim.wo[winid].number then
+            return (
+                "%s" ..
+                "%=" ..
+                "%l" ..
+                status.fold() ..
+                "│"
+            )
+        else
+            return ""
+        end
     end
 end
--- files/home/.config/nvim/lua/core/ui/statuscolumn/init.lua
+++ files/home/.config/nvim/lua/core/ui/statuscolumn/init.lua
@@ -3,8 +3,6 @@ require "core.ui.statuscolumn.format"
 
 
 vim.opt.signcolumn = "yes"
-
 vim.opt.cursorline = true
 vim.opt.cursorlineopt = "number"
-
-vim.opt.statuscolumn = "%{% g:actual_curwin == win_getid() ? v:lua.StatusColumn() : v:lua.StatusColumnInactive() %}"
+vim.opt.statuscolumn = "%!v:lua.StatusColumn()"
