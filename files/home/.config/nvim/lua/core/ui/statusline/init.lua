9d78cf748e6fea49bd08df3bbb8943b17d5861b9
kana <k>
2025-12-26 12:57:49 +0000

    update

--- files/home/.config/nvim/lua/core/ui/statusline/init.lua
+++ files/home/.config/nvim/lua/core/ui/statusline/init.lua
@@ -4,6 +4,5 @@ require "core.ui.statusline.format"
 
 vim.opt.showcmdloc = 'statusline'
 vim.opt.cmdheight = 0
--- vim.opt.laststatus = 3
-vim.opt.laststatus = 2 -- DEBUG:
+vim.opt.laststatus = 3
 vim.opt.statusline = "%!v:lua.StatusLine()"
