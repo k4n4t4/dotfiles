47992038c7ba1445ed6ae9be4c4968ff3a55c1e1
kana <kana>
2025-12-31 21:18:14 +0900

    update

--- files/nix/modules/home/fcitx5/default.nix
+++ files/nix/modules/home/fcitx5/default.nix
@@ -1,19 +1,64 @@
 { inputs, pkgs, lib, config, ... }:
 {
-  i18n.inputMethod = {
-    enable = true;
-    type = "fcitx5";
-    fcitx5.addons = with pkgs; [
-      fcitx5-mozc
-      fcitx5-gtk
-    ];
-  };
-
-  home = {
-    sessionVariables = {
-      GTK_IM_MODULE = "fcitx";
-      QT_IM_MODULE = "fcitx";
-      XMODIFIERS = "@im=fcitx";
+    i18n.inputMethod = {
+        enable = true;
+        type = "fcitx5";
+        fcitx5.addons = with pkgs; [
+            fcitx5-mozc
+            fcitx5-gtk
+        ];
     };
-  };
+
+    home = {
+        sessionVariables = {
+            GTK_IM_MODULE = "fcitx";
+            QT_IM_MODULE = "fcitx";
+            XMODIFIERS = "@im=fcitx";
+        };
+
+        file = {
+            ".config/fcitx5/profile" = {
+                text = ''
+                    [Groups/0]
+                    Name=Default
+                    Default Layout=us
+                    DefaultIM=mozc
+
+                    [Groups/0/Items/0]
+                    Name=keyboard-us
+                    Layout=
+
+                    [Groups/0/Items/1]
+                    Name=mozc
+                    Layout=
+
+                    [GroupOrder]
+                    0=Default
+                '';
+                force = true;
+            };
+            ".config/fcitx5/conf/classicui.conf" = {
+                text = ''
+                    Vertical Candidate List=False
+                    WheelForPaging=True
+                    Font="Sans 10"
+                    MenuFont="Sans 10"
+                    TrayFont="Sans Bold 10"
+                    TrayOutlineColor=#000000
+                    TrayTextColor=#ffffff
+                    PreferTextIcon=False
+                    ShowLayoutNameInIcon=True
+                    UseInputMethodLanguageToDisplayText=True
+                    Theme=default-dark
+                    DarkTheme=default-dark
+                    UseDarkTheme=False
+                    UseAccentColor=True
+                    PerScreenDPI=False
+                    ForceWaylandDPI=0
+                    EnableFractionalScale=True
+                '';
+                force = true;
+            };
+        };
+    };
 }
