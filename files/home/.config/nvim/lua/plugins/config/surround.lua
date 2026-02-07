85843f6f503cfa026285a3579328b968dff5e246
kana <k>
2026-02-07 12:14:22 +0000

    update

--- files/home/.config/nvim/lua/plugins/config/surround.lua
+++ files/home/.config/nvim/lua/plugins/config/surround.lua
@@ -1,3 +1,13 @@
 return function()
-    require("nvim-surround").setup {}
+    require("nvim-surround").setup {
+        keymaps = {
+            normal       = "ys";
+            normal_cur   = "yss";
+            normal_line  = "yS";
+            visual       = "S";
+            visual_line  = "S";
+            delete       = "ds";
+            change       = "cs";
+        };
+    }
 end
--- files/home/.config/nvim/lua/plugins/specs/others.lua
+++ files/home/.config/nvim/lua/plugins/specs/others.lua
@@ -279,19 +279,27 @@ return {
 
     {
         "folke/which-key.nvim";
-        opts = {
-            preset = 'modern';
-            delay = function(ctx)
-                return ctx.plugin and 0 or 500
-            end;
-            win = {
-                border = 'single';
+        config = function ()
+            local which_key = require "which-key"
+            which_key.setup {
+                preset = 'modern';
+                delay = 1000;
+                win = {
+                    border = 'single';
+                };
             };
-        };
+
+            which_key.add {
+                { "cs*",  group = "+surround change" },
+                { "ds*",  group = "+surround delete" },
+                { "ys*",  group = "+surround add" },
+                { "S",    group = "+surround visual",  mode = "v" },
+            }
+        end;
         event = 'VeryLazy';
         keys = {
             {
-                mode = 'n',
+                mode = "n",
                 '<LEADER>?',
                 function()
                     require("which-key").show({ global = true })
