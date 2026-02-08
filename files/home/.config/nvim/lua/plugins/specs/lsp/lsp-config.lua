9034935bbde44e8ef815068e2e6b3ddf9c907be7
kana <k>
2026-02-08 07:51:27 +0000

    update

--- files/home/.config/nvim/lua/plugins/specs/lsp/lsp-config.lua
+++ files/home/.config/nvim/lua/plugins/specs/lsp/lsp-config.lua
@@ -1,38 +1,33 @@
 return {
-    {
-        "mason-org/mason-lspconfig.nvim";
-        dependencies = {
-            {
-                "mason-org/mason.nvim";
-                opts = {
-                    ui = {
-                        border = 'double';
-                    };
+    "mason-org/mason-lspconfig.nvim";
+    dependencies = {
+        {
+            "mason-org/mason.nvim";
+            opts = {
+                ui = {
+                    border = 'double';
                 };
-            },
-            "neovim/nvim-lspconfig";
-            "nvimtools/none-ls.nvim";
-            "jayp0521/mason-null-ls.nvim";
-        };
-        config = function()
-            local mason_lspconfig = require("mason-lspconfig")
+            };
+        },
+        "neovim/nvim-lspconfig";
+        "nvimtools/none-ls.nvim";
+        "jayp0521/mason-null-ls.nvim";
+    };
+    config = function()
+        local mason_lspconfig = require("mason-lspconfig")
 
-            mason_lspconfig.setup {
-                ensure_installed = {
-                    "vimls",
-                };
-                automatic_enable = false;
-                handlers = {
-                    function(server_name)
-                        require("lspconfig")[server_name].setup {}
-                    end
-                }
+        mason_lspconfig.setup {
+            ensure_installed = {
+                "vimls",
+            };
+            automatic_enable = false;
+            handlers = {
+                function(server_name)
+                    require("lspconfig")[server_name].setup {}
+                end
             }
+        }
 
-        end;
-        event = {
-            "BufReadPre",
-            "BufNewFile",
-        };
-    },
+    end;
+    event = "VeryLazy";
 }
