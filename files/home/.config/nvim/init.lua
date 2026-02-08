e8bc927e19a53ed91f88c2fac893c23ce866b820
kana <k>
2026-02-08 04:27:41 +0000

    update

--- files/home/.config/nvim/init.lua
+++ files/home/.config/nvim/init.lua
@@ -1,4 +1,7 @@
-if vim.g.vscode then
+local is_nixos = vim.fn.executable("nixos-version") == 1
+local is_vscode = vim.g.vscode
+
+if is_vscode then
     require "vscode-nvim"
 else
     require "core"
