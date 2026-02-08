27b4801b85eb646f0912a4b1d3773583e9a48ac1
kana <k>
2026-02-08 09:56:48 +0000

    update

--- files/home/.config/nvim/lua/core/lsp/init.lua
+++ files/home/.config/nvim/lua/core/lsp/init.lua
@@ -1,8 +1,13 @@
-local lsp = require "utils.lsp"
+-- local lsp = require "utils.lsp"
 
-lsp.set("core.lsp.config", {
-    { {"c", "cpp"}, "clangd" },
-    { {"html", "css"}, "emmet_language_server" },
-    { {"lua"}, "lua_ls" },
-    { {"sh", "bash"}, "bash_ls" },
-})
+-- lsp.set("core.lsp.config", {
+--     { {"c", "cpp"}, "clangd" },
+--     { {"html", "css"}, "emmet_language_server" },
+--     { {"lua"}, "lua_ls" },
+--     { {"sh", "bash"}, "bash_ls" },
+-- })
+
+
+local lua_ls_config = require("core.lsp.config.lua_ls")
+vim.lsp.config("lua_ls", lua_ls_config)
+vim.lsp.enable("lua_ls")
