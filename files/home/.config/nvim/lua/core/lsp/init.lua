4f8ea3501cd87e7d4430972af63d8b4c2806001e
kana <k>
2026-02-08 12:52:30 +0000

    update

--- files/home/.config/nvim/lua/core/lsp/init.lua
+++ files/home/.config/nvim/lua/core/lsp/init.lua
@@ -1,3 +1,9 @@
+local mason_bin = vim.fn.stdpath("data") .. "/mason/bin"
+
+if not vim.env.PATH:find(mason_bin, 1, true) then
+    vim.env.PATH = mason_bin .. ":" .. vim.env.PATH
+end
+
 local lsp = require "utils.lsp"
 
 lsp.set("core.lsp.config", {
