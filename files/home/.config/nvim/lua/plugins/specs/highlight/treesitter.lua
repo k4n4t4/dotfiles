a1d16bf4ca09223dc9bf5d7af504dfe6ff912140
kana74 <kana74>
2026-02-09 22:46:59 +0900

    update

--- files/home/.config/nvim/lua/plugins/specs/highlight/treesitter.lua
+++ files/home/.config/nvim/lua/plugins/specs/highlight/treesitter.lua
@@ -1,77 +1,23 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter";
-        dependencies = {
-            "nvim-treesitter/nvim-treesitter-textobjects";
-        };
         build = ":TSUpdate";
         config = function()
-            require("nvim-treesitter.config").setup {
-                ignore_install = {};
-                modules = {};
-
-                sync_install = true;
-                auto_install = true;
-                ensure_installed = {
-                    "lua",
-                    "vim",
-                    "vimdoc",
-                    "query",
-                };
-                highlight = {
-                    enable = true;
-                };
-                indent = {
-                    enable = true;
-                };
-                fold = {
-                    enable = true;
-                };
-                incremental_selection = {
-                    enable = true;
-                    keymaps = {
-                        init_selection = "<CR>";
-                        node_incremental = "<CR>";
-                        scope_incremental = false;
-                        node_decremental = "<BS>";
-                    };
-                };
-                textobjects = {
-                    select = {
-                        enable = true;
-                        lookahead = true;
-                        keymaps = {
-                            ["af"] = "@function.outer";
-                            ["if"] = "@function.inner";
-                            ["ac"] = "@class.outer";
-                            ["ic"] = "@class.inner";
-                            ["ab"] = "@block.outer";
-                            ["ib"] = "@block.inner";
-                            ["aa"] = "@parameter.outer";
-                            ["ia"] = "@parameter.inner";
-                        };
-                    };
-                };
-            }
+            vim.api.nvim_create_autocmd('FileType', {
+                callback = function()
+                    pcall(vim.treesitter.start)
+                end;
+            })
+            vim.api.nvim_create_autocmd('FileType', {
+                callback = function()
+                    vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+                    vim.wo[0][0].foldmethod = 'expr'
+                    vim.opt.foldlevel = 99
+                    vim.opt.foldlevelstart = 99
+                end;
+            })
         end;
-        event = 'VeryLazy';
-        cmd = {
-            "TSUpdate",
-            "TSInstall",
-            "TSUninstall",
-            "TSToggle",
-            "TSEnable",
-            "TSDisable",
-            "TSBufToggle",
-            "TSBufEnable",
-            "TSBufDisable",
-        };
-    },
-    {
-        "nvim-treesitter/nvim-treesitter-context";
-        event = 'VeryLazy';
-        dependencies = {
-            "nvim-treesitter/nvim-treesitter";
-        };
-    },
+        -- event = 'VeryLazy';
+        lazy = false
+    }
 }
