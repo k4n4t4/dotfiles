7fc1ac45c4286977458426a778d01bfec416cbbe
kana <k>
2026-02-10 12:50:42 +0000

    update

--- files/home/.config/nvim/lua/core/keymaps/common.lua
+++ files/home/.config/nvim/lua/core/keymaps/common.lua
@@ -8,11 +8,11 @@ set('n', '<LEADER>k', "<CMD>belowright 10split<CR><CMD>terminal<CR>", { desc = "
 set('n', '<LEADER>K', "<CMD>terminal<CR>", { desc = "Terminal (full)" })
 
 -- move
-set('n', '<TAB>', "5j",   { desc = "Scroll Down" })
-set('n', '<S-TAB>', "5k", { desc = "Scroll Up" })
-set('x', '<TAB>', "5j",   { desc = "Scroll Down" })
-set('x', '<S-TAB>', "5k", { desc = "Scroll Up" })
+set({'n', 'o', 'x'}, '<TAB>', "5j",   { desc = "Scroll Down" })
+set({'n', 'o', 'x'}, '<S-TAB>', "5k", { desc = "Scroll Up" })
 
 set('n', '<LEADER>w', "<C-W><C-W>", { desc = "Switch Window" })
 
 set('n', '<LEADER>H', "<CMD>noh<CR>", { desc = "No hlsearch" })
+
+set({'n', 'x'}, '<LEADER>a', "ggVoG", { desc = "Select All" })
