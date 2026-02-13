5d39438e4fd6c3800778fdb1b451dc62e856c19c
kana <k>
2026-02-13 13:15:04 +0000

    update

--- files/home/.config/nvim/lua/plugins/specs/operation/copilot.lua
+++ files/home/.config/nvim/lua/plugins/specs/operation/copilot.lua
@@ -67,36 +67,63 @@ return {
                 };
                 prompts = {
                     Explain = {
+                        sticky = {
+                            "#buffer",
+                            "#selection",
+                        };
                         prompt = "/COPILOT_EXPLAIN Please explain the code in " .. language .. ".";
                         mapping = '<leader>ce';
                         description = "Ask for code explanation";
                     };
                     Review = {
+                        sticky = {
+                            "#buffer",
+                            "#selection",
+                        };
                         prompt = '/COPILOT_REVIEW Please review the code in ' .. language .. '.';
                         mapping = '<leader>cv';
                         description = "Ask for code review";
                     };
                     Fix = {
+                        sticky = {
+                            "#buffer",
+                            "#selection",
+                        };
                         prompt = "/COPILOT_FIX There is a problem with this code. Please show the fixed code and explain in " .. language .. ".";
                         mapping = '<leader>cf';
                         description = "Ask for code fix";
                     };
                     Optimize = {
+                        sticky = {
+                            "#buffer",
+                            "#selection",
+                        };
                         prompt = "/COPILOT_REFACTOR Please optimize the selected code for performance and readability, and explain in " .. language .. ".";
                         mapping = '<leader>co';
                         description = "Ask for code optimization";
                     };
                     Docs = {
+                        sticky = {
+                            "#buffer",
+                            "#selection",
+                        };
                         prompt = "/COPILOT_GENERATE Please generate documentation comments for the selected code in " .. language .. ".";
                         mapping = '<leader>cg';
                         description = "Ask for code documentation";
                     };
                     Tests = {
+                        sticky = {
+                            "#buffer",
+                            "#selection",
+                        };
                         prompt = "/COPILOT_TESTS Please write detailed unit tests for the selected code and explain in " .. language .. ".";
                         mapping = '<leader>cd';
                         description = "Ask for test code";
                     };
                     FixDiagnostic = {
+                        sticky = {
+                            "#buffer",
+                        };
                         prompt = 'Please fix the issues according to the code diagnostics and explain the changes in ' .. language .. '.';
                         mapping = '<leader>cF';
                         description = "Ask for code fix";
@@ -105,7 +132,9 @@ return {
                 };
             };
 
-            vim.keymap.set({'n', 'v'}, '<leader>cc',
+            local map = vim.keymap.set
+
+            map({'n', 'v'}, '<leader>cc',
                 function()
                     require("CopilotChat").open {
                         sticky = {
@@ -116,11 +145,31 @@ return {
                     }
                 end,
             { desc = 'Copilot Chat Open' })
-            vim.keymap.set({'n', 'v'}, '<leader>ct', function() require("CopilotChat").toggle() end, { desc = 'Copilot Chat Toggle' })
-            vim.keymap.set({'n', 'v'}, '<leader>cq', function() require("CopilotChat").close() end, { desc = 'Copilot Chat Close' })
-            vim.keymap.set({'n', 'v'}, '<leader>cs', function() require("CopilotChat").stop() end, { desc = 'Copilot Chat Stop' })
-            vim.keymap.set({'n', 'v'}, '<leader>cr', function() require("CopilotChat").reset() end, { desc = 'Copilot Chat Reset' })
-            vim.keymap.set({'n', 'v'}, '<leader>cS', function() require("CopilotChat").save() end, { desc = 'Copilot Chat Save' })
+            map({'n', 'v'}, '<leader>ct',
+                function()
+                    require("CopilotChat").toggle()
+                end,
+            { desc = 'Copilot Chat Toggle' })
+            map({'n', 'v'}, '<leader>cq',
+                function()
+                    require("CopilotChat").close()
+                end,
+            { desc = 'Copilot Chat Close' })
+            map({'n', 'v'}, '<leader>cs',
+                function()
+                    require("CopilotChat").stop()
+                end,
+            { desc = 'Copilot Chat Stop' })
+            map({'n', 'v'}, '<leader>cr',
+                function()
+                    require("CopilotChat").reset()
+                end,
+            { desc = 'Copilot Chat Reset' })
+            map({'n', 'v'}, '<leader>cS',
+                function()
+                    require("CopilotChat").save()
+                end,
+            { desc = 'Copilot Chat Save' })
         end;
     }
 }
