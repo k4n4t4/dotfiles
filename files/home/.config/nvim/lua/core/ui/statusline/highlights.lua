3786293eabc4a805ec68660146af82179fa255af
kana74 <kana74>
2026-02-14 15:32:37 +0900

    update

--- files/home/.config/nvim/lua/core/ui/statusline/highlights.lua
+++ files/home/.config/nvim/lua/core/ui/statusline/highlights.lua
@@ -1,76 +1,29 @@
-local function status_line_highlights()
-    local hls = {
-        StlModeNormal = {
-            fg = "#99EE99";
-        };
-        StlModeInsert = {
-            fg = "#EE9999";
-        };
-        StlModeReplace = {
-            fg = "#EEEE99";
-        };
-        StlModeVisual = {
-            fg = "#9999EE";
-        };
-        StlModeConfirm = {
-            fg = "#999999";
-        };
-        StlModeTerminal = {
-            fg = "#999999";
-        };
-        StlModeOther = {
-            fg = "#EE99EE";
-        };
+local highlight = require("utils.highlight")
 
-        StlMacro = {
-            fg = "#BB77EE";
-        };
+highlight.set("StlModeNormal", { fg = "#99EE99" })
+highlight.set("StlModeInsert", { fg = "#EE9999" })
+highlight.set("StlModeReplace", { fg = "#EEEE99" })
+highlight.set("StlModeVisual", { fg = "#9999EE" })
+highlight.set("StlModeConfirm", { fg = "#999999" })
+highlight.set("StlModeTerminal", { fg = "#999999" })
+highlight.set("StlModeOther", { fg = "#EE99EE" })
 
-        StlFileFlag = {
-            fg = "#DDEE99";
-        };
+highlight.set("StlMacro", { fg = "#BB77EE" })
 
-        StlDiagnosticERROR = {
-            fg = "#EE9999";
-        };
-        StlDiagnosticWARN = {
-            fg = "#EEEE99";
-        };
-        StlDiagnosticINFO = {
-            fg = "#99EEEE";
-        };
-        StlDiagnosticHINT = {
-            fg = "#99EE99";
-        };
+highlight.set("StlFileFlag", { fg = "#DDEE99" })
 
-        StlGitAdd = {
-            fg = "#55CC55";
-        };
-        StlGitRemove = {
-            fg = "#CC5555";
-        };
-        StlGitChange = {
-            fg = "#5555CC";
-        };
-        StlGitBranch = {
-            fg = "#CC9955";
-        };
-    }
-    for name, spec in pairs(hls) do
-        vim.api.nvim_set_hl(0, name, spec)
-    end
-end
+highlight.set("StlDiagnosticERROR", { fg = "#EE9999" })
+highlight.set("StlDiagnosticWARN", { fg = "#EEEE99" })
+highlight.set("StlDiagnosticINFO", { fg = "#99EEEE" })
+highlight.set("StlDiagnosticHINT", { fg = "#99EE99" })
+
+highlight.set("StlGitAdd", { fg = "#55CC55" })
+highlight.set("StlGitRemove", { fg = "#CC5555" })
+highlight.set("StlGitChange", { fg = "#5555CC" })
+highlight.set("StlGitBranch", { fg = "#CC9955" })
 
 
 local group = vim.api.nvim_create_augroup("StatusLine", { clear = true })
-
-vim.api.nvim_create_autocmd({
-    "VimEnter",
-    "ColorScheme",
-}, {
-    group = group;
-    callback = status_line_highlights;
-})
 
 -- Redraw statusline when mode changed. (e.g. 'ix' mode)
 vim.api.nvim_create_autocmd("ModeChanged", {
