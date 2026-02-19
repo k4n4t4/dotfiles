bd661650b1ab7054e02bb6f8341bab361fcfaf0d
kana74 <kana74>
2026-02-19 21:04:41 +0900

    update

--- files/home/.config/nvim/lua/core/keymaps/common.lua
+++ files/home/.config/nvim/lua/core/keymaps/common.lua
@@ -2,17 +2,12 @@ local keymap = vim.keymap
 local set = keymap.set
 
 
--- terminal
-set('t', '<ESC>', "<C-\\><C-N>")
-set('n', '<LEADER>k', "<CMD>belowright 10split<CR><CMD>terminal<CR>", { desc = "Terminal" })
-set('n', '<LEADER>K', "<CMD>terminal<CR>", { desc = "Terminal (full)" })
-
 -- move
-set({ 'n', 'o', 'x' }, '<TAB>', "5j", { desc = "Scroll Down" })
-set({ 'n', 'o', 'x' }, '<S-TAB>', "5k", { desc = "Scroll Up" })
+set({ 'n', 'o', 'x' }, '<tab>', "5j", { desc = "Scroll Down" })
+set({ 'n', 'o', 'x' }, '<S-tab>', "5k", { desc = "Scroll Up" })
 
-set({ 'n', 'x' }, '<LEADER>w', "<C-W><C-W>", { desc = "Switch Window" })
+set({ 'n', 'x' }, '<leader>w', "<C-w><C-w>", { desc = "Switch Window" })
 
-set('n', '<LEADER>H', "<CMD>noh<CR>", { desc = "No hlsearch" })
+set('n', '<leader>H', "<cmd>noh<cr>", { desc = "No hlsearch" })
 
-set({ 'n', 'x' }, '<LEADER>a', "ggVoG", { desc = "Select All" })
+set({ 'n', 'x' }, '<leader>a', "ggVoG", { desc = "Select All" })
---
+++ files/home/.config/nvim/lua/core/ui/terminal/init.lua
@@ -0,0 +1,59 @@
+local state = {
+  floating = {
+    buf = -1,
+    win = -1,
+  }
+}
+
+local function create_floating_window(opts)
+  opts = opts or {}
+  local width = opts.width or math.floor(vim.o.columns * 0.7)
+  local height = opts.height or math.floor(vim.o.lines * 0.7)
+
+  local col = math.floor((vim.o.columns - width) / 2)
+  local row = math.floor((vim.o.lines - height) / 2)
+
+  local buf = nil
+  if vim.api.nvim_buf_is_valid(opts.buf) then
+    buf = opts.buf
+  else
+    buf = vim.api.nvim_create_buf(false, true)
+  end
+
+  local win_config = {
+    relative = "editor",
+    width = width,
+    height = height,
+    col = col,
+    row = row,
+    style = "minimal",
+    border = "rounded",
+  }
+
+  local win = vim.api.nvim_open_win(buf, true, win_config)
+
+  vim.wo[win].winblend = 10
+
+  return { buf = buf, win = win }
+end
+
+local toggle_terminal = function()
+  if not vim.api.nvim_win_is_valid(state.floating.win) then
+    state.floating = create_floating_window({ buf = state.floating.buf })
+    if vim.bo[state.floating.buf].buftype ~= "terminal" then
+      vim.cmd.term()
+    end
+  else
+    vim.api.nvim_win_hide(state.floating.win)
+  end
+end
+
+
+local keymap = vim.keymap
+local set = keymap.set
+
+-- terminal
+set('t', '<esc>', "<C-\\><C-n>")
+set('n', '<leader>kk', "<cmd>belowright 10split<cr><cmd>terminal<cr>", { desc = "Terminal" })
+set({"n", "t"}, "<leader>kf", toggle_terminal, { desc = "Terminal (floating)" })
+set('n', '<leader>K', "<cmd>terminal<cr>", { desc = "Terminal (full)" })
