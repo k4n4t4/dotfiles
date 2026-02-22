01f2743ee66d252a8661b5f6a300e5e63ad35cfd
kana <k>
2026-02-22 13:32:26 +0000

    update

--- files/home/.config/nvim/lua/core/autocmd/fcitx.lua
+++ files/home/.config/nvim/lua/core/autocmd/fcitx.lua
@@ -1,14 +1,20 @@
 local group = vim.api.nvim_create_augroup("fcitx5", { clear = true })
 local autocmd = vim.api.nvim_create_autocmd
 
-if vim.fn.executable("fcitx5") == 1 then
-    autocmd("InsertLeave", {
-        group = group,
-        callback = function()
-            local out = vim.fn.system { "fcitx5-remote" }
-            if out == "2\n" then
-                vim.fn.system { "fcitx5-remote", "-c" }
-            end
-        end,
-    })
-end
+autocmd("VimEnter", {
+    group = group,
+    once = true,
+    callback = function()
+        if vim.fn.executable("fcitx5") == 1 then
+            autocmd("InsertLeave", {
+                group = group,
+                callback = function()
+                    local out = vim.fn.system { "fcitx5-remote" }
+                    if out == "2\n" then
+                        vim.fn.system { "fcitx5-remote", "-c" }
+                    end
+                end,
+            })
+        end
+    end,
+})
