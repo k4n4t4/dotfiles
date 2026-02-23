f9d78061e3fbe5e724e768343843ae5d4dc53ab3
kana74 <kana74>
2026-02-24 02:30:10 +0900

    update

---
+++ files/home/.config/nvim/lua/core/options/indent.lua
@@ -0,0 +1,3 @@
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
---
+++ files/home/.config/nvim/lua/core/options/search.lua
@@ -0,0 +1,6 @@
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.inccommand = "split"
+vim.opt.wildignorecase = true
+vim.opt.wildmode = "list:full"
+vim.opt.pumheight = 10
