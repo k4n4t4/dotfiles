170ded8174922aa616be7fd0e5e5ab1e7b5b771a
kana74 <kana74>
2026-02-25 01:22:30 +0900

    update

--- files/home/.config/nvim/lua/core/keymaps/common.lua
+++ files/home/.config/nvim/lua/core/keymaps/common.lua
@@ -11,3 +11,9 @@ set({ 'n', 'x' }, '<leader>w', "<C-w><C-w>", { desc = "Switch Window" })
 set('n', '<leader>H', "<cmd>noh<cr>", { desc = "No hlsearch" })
 
 set({ 'n', 'x' }, '<leader>a', "ggVoG", { desc = "Select All" })
+
+set('n', '<leader>mx', require"core.ui.tabline.scope".close, { desc = "No hlsearch" })
+set('n', '<leader>mj', require"core.ui.tabline.scope".next, { desc = "No hlsearch" })
+set('n', '<leader>mk', require"core.ui.tabline.scope".prev, { desc = "No hlsearch" })
+set('n', '<leader>mh', vim.cmd.tabprevious, { desc = "Tab Left" })
+set('n', '<leader>ml', vim.cmd.tabnext, { desc = "Tab Right" })
