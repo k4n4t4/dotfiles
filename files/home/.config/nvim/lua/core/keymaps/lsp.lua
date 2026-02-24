7a90c8d25683413d597a6b05688714ea929c85fc
kana74 <kana74>
2026-02-25 00:02:06 +0900

    update

--- files/home/.config/nvim/lua/core/keymaps/lsp.lua
+++ files/home/.config/nvim/lua/core/keymaps/lsp.lua
@@ -6,15 +6,24 @@ vim.api.nvim_create_autocmd("LspAttach", {
     group = group,
     callback = function(event)
         local buf = event.buf
-        set('n', 'K', vim.lsp.buf.format, { buffer = buf, desc = "Format" })
         set('n', '<Leader>lf', vim.lsp.buf.format, { buffer = buf, desc = "Format" })
         set('n', '<Leader>lr', vim.lsp.buf.rename, { buffer = buf, desc = "Rename" })
         set('n', '<Leader>ld', vim.lsp.buf.definition, { buffer = buf, desc = "Definition" })
         set('n', '<Leader>lt', vim.lsp.buf.type_definition, { buffer = buf, desc = "Type Definition" })
-        set('n', '<Leader>lh', vim.lsp.buf.hover, { buffer = buf, desc = "Hover" })
         set('n', '<Leader>lc', vim.lsp.buf.code_action, { buffer = buf, desc = "Code Action" })
         set('n', '<Leader>lg', vim.lsp.buf.references, { buffer = buf, desc = "References" })
-        set('n', '<Leader>ls', vim.lsp.buf.signature_help, { buffer = buf, desc = "Signature Help" })
+        set('n', '<Leader>lh', function()
+            vim.lsp.buf.hover({ border = "none", winblend = 10 })
+        end, { buffer = buf, desc = "Hover" })
+        set('n', '<Leader>ls', function()
+            vim.lsp.buf.signature_help({ border = "none", winblend = 10 })
+        end, { buffer = buf, desc = "Signature Help" })
+        set('n', 'K', function()
+            vim.lsp.buf.hover({ border = "none", winblend = 10 })
+        end, { buffer = buf, desc = "Hover" })
+        set('n', '<C-K>', function()
+            vim.lsp.buf.signature_help({ border = "none", winblend = 10 })
+        end, { buffer = buf, desc = "Signature Help" })
         set('n', '<Leader>le', function() vim.lsp.diagnostic.show_line_diagnostics() end,
             { buffer = buf, desc = "Show Line Diagnostics" })
         set('n', '<Leader>l[', function() vim.lsp.diagnostic.goto_prev() end,
--- files/home/.config/nvim/lua/plugins/specs/lsp/lsp-config.lua
+++ files/home/.config/nvim/lua/plugins/specs/lsp/lsp-config.lua
@@ -1,25 +1,30 @@
 return {
-    "mason-org/mason-lspconfig.nvim",
-    dependencies = {
-        {
-            "mason-org/mason.nvim",
-            opts = {
-                ui = {
-                    border = 'double',
-                },
-            },
-        },
+    {
         "neovim/nvim-lspconfig",
     },
-    event = "VeryLazy",
-    config = function()
-        local mason_lspconfig = require("mason-lspconfig")
-
-        mason_lspconfig.setup {
-            ensure_installed = {
-                "vimls",
+    {
+        "mason-org/mason-lspconfig.nvim",
+        dependencies = {
+            {
+                "mason-org/mason.nvim",
+                opts = {
+                    ui = {
+                        border = 'double',
+                    },
+                },
             },
-            automatic_enable = false,
-        }
-    end,
+            "neovim/nvim-lspconfig",
+        },
+        event = "VeryLazy",
+        config = function()
+            local mason_lspconfig = require("mason-lspconfig")
+
+            mason_lspconfig.setup {
+                ensure_installed = {
+                    "vimls",
+                },
+                automatic_enable = false,
+            }
+        end,
+    },
 }
