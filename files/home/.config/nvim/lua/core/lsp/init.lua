fb50cb1453a50b49e8494abfa21ca34f9fc8598c
kana74 <kana74>
2026-02-24 21:39:37 +0900

    update

--- files/home/.config/nvim/lua/core/lsp/init.lua
+++ files/home/.config/nvim/lua/core/lsp/init.lua
@@ -3,21 +3,6 @@ if not vim.env.PATH:find(mason_bin, 1, true) then
     vim.env.PATH = mason_bin .. ":" .. vim.env.PATH
 end
 
-local win_opts = { border = "shadow", winblend = 10 }
-vim.lsp.config("*", {
-    handlers = {
-        ["textDocument/hover"] = function(err, result, ctx, config)
-            vim.lsp.handlers["textDocument/hover"](
-                err, result, ctx, vim.tbl_extend("force", config or {}, win_opts)
-            )
-        end,
-        ["textDocument/signatureHelp"] = function(err, result, ctx, config)
-            vim.lsp.handlers["textDocument/signatureHelp"](
-                err, result, ctx, vim.tbl_extend("force", config or {}, win_opts)
-            )
-        end,
-    },
-})
 
 local lsp = require "utils.lsp"
 
