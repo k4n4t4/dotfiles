6f7d55200faeeea37ba0880ee728d7afcd52171f
kana74 <kana74>
2026-02-25 08:31:29 +0900

    update

--- files/home/.config/nvim/lua/core/ui/tabline/format.lua
+++ files/home/.config/nvim/lua/core/ui/tabline/format.lua
@@ -4,23 +4,25 @@ local tab_format = require "core.ui.tabline.components.tab"
 return function()
     local s = ""
 
+    local bufline = ""
     local bufs = vim.t.bufs or {}
     for _, bufnr in ipairs(bufs) do
         if vim.api.nvim_buf_is_valid(bufnr) then
-            s = s .. buf_format(bufnr)
+            bufline = bufline .. buf_format(bufnr)
         end
     end
 
-    s = s .. "%#TabLineFill#%="
-
+    local tabline = ""
     local tab_count = vim.fn.tabpagenr('$')
     local current_tabnr = vim.fn.tabpagenr()
     for tabnr = 1, tab_count do
-        s = s .. tab_format {
+        tabline = tabline .. tab_format {
             tabnr = tabnr,
             is_current_tab = tabnr == current_tabnr,
         }
     end
+
+    s = bufline .. "%#TabLineFill#%=" .. tabline
 
     return s
 end
