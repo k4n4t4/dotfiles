2e05e1c74695c4af1d6252b77a28c839735d615b
kana <k>
2026-02-25 21:17:58 +0000

    update

--- files/home/.config/nvim/lua/core/ui/init.lua
+++ files/home/.config/nvim/lua/core/ui/init.lua
@@ -1,14 +1,20 @@
 local plugin = require "utils.plugin"
 plugin.load("nvim-web-devicons", "User", "Ready")
 
-local hl = require "utils.highlight"
 
-hl.link("MacroRecord", "Operator")
-hl.link("FileModified", "WarningMsg")
-hl.link("GitBranch", "Constant")
+vim.api.nvim_create_autocmd("User", {
+    pattern = "Ready",
+    callback = function()
+        local hl = require "utils.highlight"
 
-require "core.ui.statusline"
-require "core.ui.statuscolumn"
-require "core.ui.tabline"
-require "core.ui.foldtext"
-require "core.ui.terminal"
+        hl.link("MacroRecord", "Operator")
+        hl.link("FileModified", "WarningMsg")
+        hl.link("GitBranch", "Constant")
+
+        require "core.ui.statusline"
+        require "core.ui.statuscolumn"
+        require "core.ui.tabline"
+        require "core.ui.foldtext"
+        require "core.ui.terminal"
+    end,
+})
