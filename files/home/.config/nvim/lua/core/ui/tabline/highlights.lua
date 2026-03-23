c81beeaba8b34ad959502d3a81bc8a3ac4134669
kanata <91060664+k4n4t4@users.noreply.github.com>
2026-03-23 02:10:25 +0000

    update

--- files/home/.config/nvim/lua/core/ui/tabline/highlights.lua
+++ files/home/.config/nvim/lua/core/ui/tabline/highlights.lua
@@ -1,15 +1,5 @@
 local hl = require("utils.highlight")
 
-hl.set("TabLine", {
-    fg = hl.ref("TabLine", "fg"),
-    bg = hl.ref("TabLine", "bg"),
-})
-hl.set("TabLineSel", {
-    fg = hl.ref("TabLineSel", "fg"),
-    bg = hl.ref("StatusLine", "bg"),
+hl.patch("TabLineSel", {
     bold = true,
-})
-hl.set("TabLineFill", {
-    fg = hl.ref("TabLineFill", "fg"),
-    bg = hl.ref("TabLineFill", "bg"),
 })
