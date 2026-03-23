6eefe294f471612502fae17ee27af0536c59332a
kanata <91060664+k4n4t4@users.noreply.github.com>
2026-03-23 03:39:18 +0000

    update

--- files/home/.config/wezterm/wezterm.lua
+++ files/home/.config/wezterm/wezterm.lua
@@ -3,11 +3,10 @@ local act = wezterm.action
 
 local config = {}
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+    config = wezterm.config_builder()
 end
-
-
 config.automatically_reload_config = true
+
 
 config.color_scheme = 'Tokyo Night'
 
@@ -17,6 +16,8 @@ config.font = wezterm.font(
     { weight = "Medium", stretch = "Normal", style = "Normal" }
 )
 config.font_size = 13
+config.cell_width = 1.1
+config.line_height = 1.2
 
 -- Window
 config.initial_cols = 120
@@ -27,10 +28,20 @@ config.window_padding = {
     top = 0,
     bottom = 0,
 }
-config.window_decorations = "RESIZE"
-config.hide_tab_bar_if_only_one_tab = true
-config.window_background_opacity = 0.85
+config.window_decorations = 'RESIZE'
+config.window_background_opacity = 0.90
+config.window_frame = {
+    inactive_titlebar_bg = "none",
+    active_titlebar_bg = "none",
+}
 
+-- Tab Bar
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = false
+config.hide_tab_bar_if_only_one_tab = true
+config.show_new_tab_button_in_tab_bar = false
+config.tab_bar_at_bottom = true
+config.tab_max_width = 16
 
 -- Key Bindings
 config.disable_default_key_bindings = true
@@ -38,10 +49,8 @@ config.keys = {
     -- Toggle full screen
     {
         key = 'F11',
-        mods = '',
         action = act.ToggleFullScreen,
     },
-
     -- Increase/Decrease font size
     {
         key = 'UpArrow',
@@ -53,7 +62,6 @@ config.keys = {
         mods = 'CTRL|ALT',
         action = act.DecreaseFontSize,
     },
-
     -- Copy/Paste
     {
         key = 'c',
@@ -65,19 +73,124 @@ config.keys = {
         mods = 'CTRL|ALT',
         action = act.PasteFrom 'Clipboard',
     },
-
     -- Pager
     {
         key = 'p',
         mods = 'CTRL|ALT',
         action = act.ActivateCopyMode,
     },
-
     -- Quit
     {
         key = 'q',
         mods = 'CTRL|ALT',
         action = act.QuitApplication,
+    },
+
+    -- New/Close Tab
+    {
+        key = 'n',
+        mods = 'CTRL|ALT',
+        action = act.SpawnTab 'CurrentPaneDomain',
+    },
+    {
+        key = 'w',
+        mods = 'CTRL|ALT',
+        action = act.CloseCurrentTab { confirm = true },
+    },
+    -- Focus Tab
+    {
+        key = ']',
+        mods = 'CTRL|ALT',
+        action = act.ActivateTabRelative(1),
+    },
+    {
+        key = '[',
+        mods = 'CTRL|ALT',
+        action = act.ActivateTabRelative(-1),
+    },
+    -- Move Tab
+    {
+        key = '}',
+        mods = 'CTRL|ALT|SHIFT',
+        action = act.MoveTabRelative(1),
+    },
+    -- Move Tab
+    {
+        key = '{',
+        mods = 'CTRL|ALT|SHIFT',
+        action = act.MoveTabRelative(-1),
+    },
+
+    -- Split Panes
+    {
+        key = 'v',
+        mods = 'CTRL|ALT',
+        action = act.SplitHorizontal { domain = 'CurrentPaneDomain' },
+    },
+    {
+        key = 's',
+        mods = 'CTRL|ALT',
+        action = act.SplitVertical { domain = 'CurrentPaneDomain' },
+    },
+    -- Close Pane
+    {
+        key = 'w',
+        mods = 'CTRL|ALT',
+        action = act.CloseCurrentPane { confirm = true },
+    },
+    -- Focus Panes
+    {
+        key = 'j',
+        mods = 'CTRL|ALT',
+        action = act.ActivatePaneDirection 'Down',
+    },
+    {
+        key = 'k',
+        mods = 'CTRL|ALT',
+        action = act.ActivatePaneDirection 'Up',
+    },
+    {
+        key = 'h',
+        mods = 'CTRL|ALT',
+        action = act.ActivatePaneDirection 'Left',
+    },
+    {
+        key = 'l',
+        mods = 'CTRL|ALT',
+        action = act.ActivatePaneDirection 'Right',
+    },
+    -- Resize Panes
+    {
+        key = 'j',
+        mods = 'CTRL|ALT|SHIFT',
+        action = act.AdjustPaneSize { 'Down', 1 },
+    },
+    {
+        key = 'k',
+        mods = 'CTRL|ALT|SHIFT',
+        action = act.AdjustPaneSize { 'Up', 1 },
+    },
+    {
+        key = 'h',
+        mods = 'CTRL|ALT|SHIFT',
+        action = act.AdjustPaneSize { 'Left', 1 },
+    },
+    {
+        key = 'l',
+        mods = 'CTRL|ALT|SHIFT',
+        action = act.AdjustPaneSize { 'Right', 1 },
+    },
+    -- Toggle Pane Zoom
+    {
+        key = 'z',
+        mods = 'CTRL|ALT',
+        action = act.TogglePaneZoomState,
+    },
+    -- Rotate Panes
+    {
+        key = 'r',
+        mods = 'CTRL|ALT',
+        action = act.RotatePanes 'Clockwise',
     },
 }
 
