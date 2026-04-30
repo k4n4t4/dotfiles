a82f20f4fe78fa973a4f3b5dcb20fd9fe716e5a5
kanata <91060664+k4n4t4@users.noreply.github.com>
2026-04-30 23:40:47 +0900

    update

--- scripts/default.sh
+++ scripts/default.sh
@@ -1,8 +1,5 @@
-alias dothome="dot --origin-prefix=home"
-alias dotconf="dot --origin-prefix=home/.config --target-prefix=.config"
-
-dot "$REPO_PATH/dm" ".local/bin/dm"
 dothome "bin" ".local/bin" -r
+
 dothome ".profile"
 
 dotconf "fontconfig" -r
