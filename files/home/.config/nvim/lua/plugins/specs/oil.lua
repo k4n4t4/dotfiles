0f25902903a2c71b70e8e87c4259088be03c52b3
kanata <91060664+k4n4t4@users.noreply.github.com>
2026-05-01 00:45:30 +0900

    update

--- files/home/.config/nvim/lua/plugins/specs/oil.lua
+++ files/home/.config/nvim/lua/plugins/specs/oil.lua
@@ -3,11 +3,10 @@ return {
     dependencies = { { "nvim-mini/mini.icons", opts = {} } },
     event = 'User DirEnter',
     config = function()
-        require('oil').setup {
-        }
+        require('oil').setup {}
     end,
     keys = {
-        { mode = 'n', '<Leader>-', '<CMD>Oil<CR>', desc = 'Oil' },
+        { mode = 'n', '<Leader>-', '<CMD>Oil<CR>',         desc = 'Oil' },
         { mode = 'n', '<Leader>=', '<CMD>Oil --float<CR>', desc = 'Oil float ' },
     },
 }
