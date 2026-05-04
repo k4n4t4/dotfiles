61ec43c256b2bb42e08dcbd5ff0357571258e578
kana <91060664+k4n4t4@users.noreply.github.com>
2026-05-04 20:56:32 +0900

    update

--- files/home/.config/nvim/lua/plugins/specs/oil.lua
+++ files/home/.config/nvim/lua/plugins/specs/oil.lua
@@ -1,12 +1,34 @@
 return {
-    'stevearc/oil.nvim',
-    dependencies = { { "nvim-mini/mini.icons", opts = {} } },
-    event = 'User DirEnter',
-    config = function()
-        require('oil').setup {}
-    end,
-    keys = {
-        { mode = 'n', '<Leader>-', '<CMD>Oil<CR>',         desc = 'Oil' },
-        { mode = 'n', '<Leader>=', '<CMD>Oil --float<CR>', desc = 'Oil float ' },
+    {
+        "adelarsq/image_preview.nvim",
+        event = "User Ready",
+        config = function()
+            require("image_preview").setup()
+        end,
+    },
+    {
+        'stevearc/oil.nvim',
+        dependencies = {
+            {
+                "nvim-mini/mini.icons",
+                opts = {},
+            },
+            "adelarsq/image_preview.nvim",
+        },
+        event = 'User DirEnter',
+        config = function()
+            require('oil').setup {
+                show_hidden = true,
+                keymaps = {
+                    ["g."] = "actions.toggle_hidden",
+                    ["<C-p>"] = "actions.preview",
+                    ["gp"] = require("image_preview").PreviewImageOil,
+                },
+            }
+        end,
+        keys = {
+            { mode = 'n', '<Leader>-', '<CMD>Oil<CR>',         desc = 'Oil' },
+            { mode = 'n', '<Leader>=', '<CMD>Oil --float<CR>', desc = 'Oil float ' },
+        },
     },
 }
