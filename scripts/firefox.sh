7ef7f422ab782cf5f9228abea903780f868c232c
kana <91060664+k4n4t4@users.noreply.github.com>
2026-05-05 20:27:33 +0900

    update

--- scripts/firefox.sh
+++ scripts/firefox.sh
@@ -1,9 +1,11 @@
 if cmd_exists firefox; then
-    for folder in ~/.config/mozilla/firefox/*.default-release; do
-        if [ -d "$folder" ]; then
-            dothome ".config/firefox/user.js" "$folder/user.js"
-            dothome ".config/firefox/userChrome.css" "$folder/chrome/userChrome.css"
-            dothome ".config/firefox/userContent.css" "$folder/chrome/userContent.css"
-        fi
-    done
+    firefox_folder=~/.config/mozilla/firefox
+    firefox_profile_name="$(grep -m 1 '^Default=' $firefox_folder/profiles.ini | cut -d= -f2)"
+    firefox_profile="$firefox_folder/$firefox_profile_name"
+
+    if [ -d "$firefox_profile" ]; then
+        dothome ".config/firefox/user.js" "$firefox_profile/user.js"
+        dothome ".config/firefox/userChrome.css" "$firefox_profile/chrome/userChrome.css"
+        dothome ".config/firefox/userContent.css" "$firefox_profile/chrome/userContent.css"
+    fi
 fi
