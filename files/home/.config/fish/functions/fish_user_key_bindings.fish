b2672fa5e5b3c423e1617bc6b809e129401c78cf
kana <kana>
2026-06-10 17:29:27 +0900

    update

--- files/home/.config/fish/functions/fish_user_key_bindings.fish
+++ files/home/.config/fish/functions/fish_user_key_bindings.fish
@@ -12,10 +12,10 @@ function fish_user_key_bindings
     bind --mode insert \cl 'keybind:clear-screen'
     bind --mode insert \t 'keybind:complete'
     bind --mode insert \eR 'keybind:repaint'
-    bind --mode insert \cb 'keybind:file-browser'
+    bind --mode insert \cf 'keybind:file-browser'
     bind --mode insert \cd 'keybind:dir-browser'
+    bind --mode insert \cp 'keybind:paste-path'
     bind --mode insert \ch 'keybind:history-search'
     bind --mode insert \cg 'keybind:git-status'
-    bind --mode insert \cp 'keybind:paste-path'
     bind --mode insert \eC 'keybind:cd-recent'
 end
