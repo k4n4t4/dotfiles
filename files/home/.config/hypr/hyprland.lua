163c22b5e471b303d86cff6906ce0aa008ed3163
kana <91060664+k4n4t4@users.noreply.github.com>
2026-06-18 02:30:07 +0900

    update

--- files/home/.config/hypr/hyprland.lua
+++ files/home/.config/hypr/hyprland.lua
@@ -278,24 +278,27 @@ hl.on("hyprland.start", function()
     -- fcitx5
     hl.exec_cmd("uwsm app -- fcitx5 -d")
 
-    -- waybar
-    hl.exec_cmd("uwsm app -- waybar")
+    -- -- awww
+    -- hl.exec_cmd("uwsm app -- awww-daemon")
+    -- hl.exec_cmd("uwsm app -- awww img ~/pers/imgs/wallpaper.png --transition-type center --transition-duration 1")
 
-    -- awww
-    hl.exec_cmd("uwsm app -- awww-daemon")
-    hl.exec_cmd("uwsm app -- awww img ~/pers/imgs/wallpaper.png --transition-type center --transition-duration 1")
+    -- -- waybar
+    -- hl.exec_cmd("uwsm app -- waybar")
 
-    -- mako
-    hl.exec_cmd("uwsm app -- mako")
+    -- -- mako
+    -- hl.exec_cmd("uwsm app -- mako")
 
-    -- swayosd
-    hl.exec_cmd("uwsm app -- swayosd-server")
+    -- -- swayosd
+    -- hl.exec_cmd("uwsm app -- swayosd-server")
+
+    -- -- wl-clipboard
+    -- hl.exec_cmd("uwsm app -- wl-paste -w cliphist store")
+
+    -- noctalia shell
+    hl.exec_cmd("uwsm app -- qs -c noctalia-shell")
 
     -- polkit
     hl.exec_cmd("uwsm app -- /usr/lib/mate-polkit/polkit-mate-authentication-agent-1")
-
-    -- wl-clipboard
-    hl.exec_cmd("uwsm app -- wl-paste -w cliphist store")
 
     -- hypridle
     hl.exec_cmd("pidof hypridle || uwsm app -- hypridle")
