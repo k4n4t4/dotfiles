9970978894c183817608ba7fb43c04816d9f147f
kana <kana>
2026-06-30 21:14:19 +0900

    update

--- scripts/xdg.sh
+++ scripts/xdg.sh
@@ -1,2 +1,12 @@
 dothome ".config/xdg/user-dirs.dirs" ".config/user-dirs.dirs"
+. "$TARGET_PATH/.config/xdg/user-dirs.dirs"
+mkdir -p -- "$XDG_DESKTOP_DIR"
+mkdir -p -- "$XDG_DOWNLOAD_DIR"
+mkdir -p -- "$XDG_TEMPLATES_DIR"
+mkdir -p -- "$XDG_PUBLICSHARE_DIR"
+mkdir -p -- "$XDG_DOCUMENTS_DIR"
+mkdir -p -- "$XDG_MUSIC_DIR"
+mkdir -p -- "$XDG_PICTURES_DIR"
+mkdir -p -- "$XDG_VIDEOS_DIR"
+mkdir -p -- "$XDG_PROJECTS_DIR"
 xdg-user-dirs-update
