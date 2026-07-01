4682271bf6f5d8c5c8b3b4e9c02aad08a0c3774d
kana <kana>
2026-07-02 08:18:24 +0900

    update

--- scripts/setup/arch/greeter.sh
+++ scripts/setup/arch/greeter.sh
@@ -1,5 +1,5 @@
 sudo pacman --needed --noconfirm -S greetd greetd-tuigreet
 sudo systemctl enable greetd
 
-sudo sed -i '/^\[default_session\]/,/^\[/ s|^command = .*|command = "tuigreet --time --remember --remember-session --user-menu"|' /etc/greetd/config.toml
+sudo sed -i '/^\[default_session\]/,/^\[/ s|^command = .*|command = "tuigreet --time --remember --remember-session --user-menu --cmd '"'"'uwsm start niri.desktop'"'"'"|' /etc/greetd/config.toml
 sudo sed -i '/^\[default_session\]/,/^\[/ s|^user = .*|user = "greeter"|' /etc/greetd/config.toml
