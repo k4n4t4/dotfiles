826e54c99a6765839bf979181c4c1a97c04327a0
kana <kana>
2026-07-02 03:24:08 +0900

    update

--- scripts/setup/arch/vm.sh
+++ scripts/setup/arch/vm.sh
@@ -1,8 +1,8 @@
 sudo pacman -S --noconfirm qemu-full libvirt virt-install virt-manager virt-viewer edk2-ovmf dnsmasq tuned ntfs-3g swtpm
-sudo systemctl enable libvirtd.service
-
 
 sudo sed -i -e 's/^#unix_sock_group = "libvirt"/unix_sock_group = "libvirt"/' -e 's/^#unix_sock_rw_perms = "0770"/unix_sock_rw_perms = "0770"/' /etc/libvirt/libvirtd.conf
+
+sudo systemctl enable libvirtd.service
 
 sudo usermod -a -G libvirt $(whoami)
 newgrp libvirt
