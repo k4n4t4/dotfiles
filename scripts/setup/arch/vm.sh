15abf4c8f9627f7c5142a4213ebf5a0c39403bb4
kana <kana>
2026-07-02 03:23:01 +0900

    update

--- scripts/setup/arch/vm.sh
+++ scripts/setup/arch/vm.sh
@@ -2,7 +2,7 @@ sudo pacman -S --noconfirm qemu-full libvirt virt-install virt-manager virt-view
 sudo systemctl enable libvirtd.service
 
 
-sudo sed -i -e 's/^unix_sock_group = "libvirt"/#&/' -e 's/^unix_sock_rw_perms = "0770"/#&/' /etc/libvirt/libvirtd.conf
+sudo sed -i -e 's/^#unix_sock_group = "libvirt"/unix_sock_group = "libvirt"/' -e 's/^#unix_sock_rw_perms = "0770"/unix_sock_rw_perms = "0770"/' /etc/libvirt/libvirtd.conf
 
 sudo usermod -a -G libvirt $(whoami)
 newgrp libvirt
