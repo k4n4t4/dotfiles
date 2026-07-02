3b456020a3620498f192d7c5b23af460c0a59033
kana <kana>
2026-07-02 18:22:33 +0900

    update

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # dotfiles
-My dotfiles
+
+> My dotfiles
+
+Install niri environment (arch)
+~~~bash
+./dm i setup/arch/all all xdg -f
+~~~
--- scripts/setup/arch/all.sh
+++ scripts/setup/arch/all.sh
@@ -1,8 +1,6 @@
 script_source setup/arch/essential
 script_source setup/arch/yay
 script_source setup/arch/cli-tools
-script_source setup/arch/virus-scanner
 script_source setup/arch/niri
 script_source setup/arch/greeter
 script_source setup/arch/apps
-script_source setup/arch/latex
