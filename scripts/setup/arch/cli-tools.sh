5b003ef9405684c484bc5f10d396b01bc9b12bd1
kana <kana>
2026-07-05 09:47:47 +0900

    update

--- scripts/setup/arch/cli-tools.sh
+++ scripts/setup/arch/cli-tools.sh
@@ -1,2 +1,2 @@
-sudo pacman --needed --noconfirm -S git-delta github-cli fzf fd ripgrep zoxide eza bat tldr btop trash-cli wl-clipboard tree-sitter-cli fish starship neovim nodejs npm fastfetch 7zip
+sudo pacman --needed --noconfirm -S git-delta github-cli fzf fd ripgrep zoxide eza bat tldr navi btop trash-cli wl-clipboard tree-sitter-cli fish starship neovim nodejs npm fastfetch 7zip
 sudo npm install -g @github/copilot
