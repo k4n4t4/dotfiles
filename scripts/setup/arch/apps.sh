f188ccd2e98b8641bf12b59b49a47de61448bc2a
kana <kana>
2026-07-08 23:35:13 +0900

    update

--- scripts/setup/arch/apps.sh
+++ scripts/setup/arch/apps.sh
@@ -1,8 +1,8 @@
-sudo pacman --needed --noconfirm -S kitty vlc
+sudo pacman --needed --noconfirm -S kitty vlc firefox gimp discord obsidian
 
-sudo pacman --needed --noconfirm -S flatpak
-
-flatpak install -y flathub org.mozilla.firefox
-flatpak install -y flathub org.gimp.GIMP
-flatpak install -y flathub com.discordapp.Discord
-flatpak install -y flathub md.obsidian.Obsidian
+# sudo pacman --needed --noconfirm -S flatpak
+#
+# flatpak install -y flathub org.mozilla.firefox
+# flatpak install -y flathub org.gimp.GIMP
+# flatpak install -y flathub com.discordapp.Discord
+# flatpak install -y flathub md.obsidian.Obsidian
