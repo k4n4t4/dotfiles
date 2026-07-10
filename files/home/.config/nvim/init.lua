a66da9ff58de5ef87c76ee9c498ab8a3e11276ec
kana <kana>
2026-07-10 22:11:50 +0900

    update

--- files/home/.config/nvim/init.lua
+++ files/home/.config/nvim/init.lua
@@ -3,62 +3,31 @@ vim.loader.enable()
 
 require "core"
 
+
+--[[ GENERAL SETTINGS ]]--
+
+-- default shell
 vim.opt.shell = "fish"
+
+-- colorscheme
 pcall(vim.cmd.colorscheme, "solarized-osaka")
+
+-- transparent
 require("utils.transparent").enable()
+
+-- line number
 vim.opt.number = true
 vim.opt.relativenumber = false
 require("utils.toggle_relnumber").enable()
+
+-- mouse
 vim.opt.mouse = "a"
-vim.opt.scrolloff = 5
-vim.opt.sidescrolloff = 5
 
 
--- nil: enable
--- 1: disable
-vim.g.skip_defaults_vim         = 1
-vim.g.skip_loading_mswin        = 1
+--[[ BUILTIN PLUGINS ]]--
 
-vim.g.did_install_default_menus = 1
-vim.g.did_install_syntax_menu   = 1
-vim.g.did_indent_on             = 1
-vim.g.did_load_filetypes        = nil
-vim.g.did_load_ftplugin         = nil
+-- disable builtin plugins
+vim.opt.loadplugins = false
 
-vim.g.loaded_gzip               = 1
-vim.g.loaded_tar                = 1
-vim.g.loaded_tarPlugin          = 1
-vim.g.loaded_zip                = 1
-vim.g.loaded_zipPlugin          = 1
-
-vim.g.loaded_netrw              = 1
-vim.g.loaded_netrwPlugin        = 1
-vim.g.loaded_netrwSettings      = 1
-vim.g.loaded_netrwFileHandlers  = 1
-
-vim.g.loaded_nvim_net_plugin    = 1
-
-vim.g.loaded_man                = nil
-
-vim.g.loaded_matchit            = 1
-vim.g.loaded_matchparen         = 1
-
-vim.g.loaded_remote_plugins     = 1
-vim.g.loaded_shada_plugin       = 1
-vim.g.loaded_spellfile_plugin   = 1
-vim.g.loaded_tutor_mode_plugin  = 1
-
-vim.g.loaded_2html_plugin       = 1
-
-vim.g.loaded_vimball            = 1
-vim.g.loaded_vimballPlugin      = 1
-vim.g.loaded_getscript          = 1
-vim.g.loaded_getscriptPlugin    = 1
-vim.g.loaded_rrhelper           = 1
-
--- 1: enable
--- 0: disable
-vim.g.loaded_node_provider      = 0
-vim.g.loaded_python3_provider   = 0
-vim.g.loaded_perl_provider      = 0
-vim.g.loaded_ruby_provider      = 0
+-- enable man plugin
+vim.cmd.runtime("plugin/man.lua")
