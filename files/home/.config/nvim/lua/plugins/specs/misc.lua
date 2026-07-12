072bfbc0baa1a52374f25be6ab8154878562b003
kana <kana>
2026-07-12 14:13:26 +0900

    update

--- files/home/.config/nvim/lua/plugins/specs/misc.lua
+++ files/home/.config/nvim/lua/plugins/specs/misc.lua
@@ -62,49 +62,7 @@ return {
         end
     },
 
-    {
-        "iamcco/markdown-preview.nvim",
-        cmd = { "MarkdownPreview", "MarkdownPreviewToggle", "MarkdownPreviewStop" },
-        build = "cd app && npm install",
-        ft = { "markdown" },
-    },
-
-    {
-        "epwalsh/obsidian.nvim",
-        version = "*",
-        event = "VeryLazy",
-        dependencies = { "nvim-lua/plenary.nvim", },
-        config = function()
-            local function setup_obsidian()
-                local cwd = vim.fn.getcwd()
-                local vault = vim.fn.isdirectory(cwd .. "/.obsidian") == 1 and cwd or nil
-                if vault then
-                    require("obsidian").setup {
-                        workspaces = {
-                            {
-                                name = "main",
-                                path = vault,
-                            },
-                        },
-                        preferred_link_style = "wiki",
-                        ui = { enable = false, },
-                        completion = {
-                            blink = true,
-                            min_chars = 2,
-                        },
-                    }
-                end
-            end
-
-            setup_obsidian()
-
-            vim.api.nvim_create_autocmd("DirChanged", {
-                pattern = "*",
-                callback = setup_obsidian,
-            })
-        end,
-    },
-
+    -- debugging
     {
         "mfussenegger/nvim-dap",
         dependencies = {
--- files/home/.config/nvim/lua/plugins/specs/syntax.lua
+++ files/home/.config/nvim/lua/plugins/specs/syntax.lua
@@ -1,4 +1,5 @@
 return {
+    --[[ TREESITTER ]]--
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
@@ -14,13 +15,7 @@ return {
         end,
     },
 
-    { "fladson/vim-kitty", ft = "kitty" },
-
-    {
-        "mattn/emmet-vim",
-        ft = { "html", "css", "javascript", "typescript", "typescriptreact", "*.tsx", "*.jsx" },
-    },
-
+    --[[ MARKDOWN PLUGINS ]]--
     {
         "MeanderingProgrammer/render-markdown.nvim",
         ft = { "markdown", "Avante" },
@@ -39,6 +34,47 @@ return {
                 },
                 file_types = { "markdown", "Avante" },
             }
+        end,
+    },
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreview", "MarkdownPreviewToggle", "MarkdownPreviewStop" },
+        build = "cd app && npm install",
+        ft = { "markdown" },
+    },
+    {
+        "epwalsh/obsidian.nvim",
+        version = "*",
+        event = "VeryLazy",
+        dependencies = { "nvim-lua/plenary.nvim", },
+        config = function()
+            local function setup_obsidian()
+                local cwd = vim.fn.getcwd()
+                local vault = vim.fn.isdirectory(cwd .. "/.obsidian") == 1 and cwd or nil
+                if vault then
+                    require("obsidian").setup {
+                        workspaces = {
+                            {
+                                name = "main",
+                                path = vault,
+                            },
+                        },
+                        preferred_link_style = "wiki",
+                        ui = { enable = false, },
+                        completion = {
+                            blink = true,
+                            min_chars = 2,
+                        },
+                    }
+                end
+            end
+
+            setup_obsidian()
+
+            vim.api.nvim_create_autocmd("DirChanged", {
+                pattern = "*",
+                callback = setup_obsidian,
+            })
         end,
     },
 }
