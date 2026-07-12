bb7033fe07beb48dc2fa7a9d5563b01f1bc4bed2
kana <kana>
2026-07-12 17:46:06 +0900

    update

--- files/home/.config/nvim/lua/utils/stl/init.lua
+++ files/home/.config/nvim/lua/utils/stl/init.lua
@@ -2,6 +2,7 @@ local M = {}
 
 M.global_name = "Statusline"
 M.group = vim.api.nvim_create_augroup("StatusLine", { clear = true })
+M.callbacks = {}
 
 function M.lsp()
     local utils_lsp = require "utils.lsp"
@@ -125,19 +126,30 @@ function M.mode(opts)
 end
 
 
+---@class StatuslineClick
+---@field name string
+---@field callback fun(minwid: number, clicks: number, button: string, modifiers: string)
+
 ---@class StatuslineNode
 ---@field hl? string highlight group name
+---@field click? StatuslineClick
 ---@field content? string|(string|StatuslineNode)[] output string or child nodes
 
 ---@class StatuslineState
 ---@field hl? string current highlight group name
+---@field click? string current click function name
+---@field is_child? boolean internal flag to detect recursive calls
 
 ---@param tbls (string|StatuslineNode)[] list of strings or statusline nodes
 ---@param stat? StatuslineState status object to keep track of current highlight group
 ---@return string generated statusline format string
 function M.make_str(tbls, stat)
     stat = stat or {}
+
+    if not stat.is_child then M.callbacks = {} end
+
     stat.hl = stat.hl or ""
+    stat.click = stat.click or ""
 
     local str = ""
     for _, tbl in ipairs(tbls) do
@@ -145,22 +157,41 @@ function M.make_str(tbls, stat)
             str = str .. tbl
         elseif type(tbl) == "table" then
             local hl = tbl.hl or stat.hl
+            local click_name = stat.click
+
+            if tbl.click and tbl.click.name and tbl.click.callback then
+                click_name = tbl.click.name
+                M.callbacks[click_name] = tbl.click.callback
+            end
+
             local node_content = tbl.content
             local content = ""
             if type(node_content) == "string" then
                 content = node_content
             elseif type(node_content) == "table" then
-                content = M.make_str(node_content, { hl = hl })
+                content = M.make_str(node_content, { hl = hl, click = click_name, is_child = true })
             end
+
+            local click_str = ""
+            local restore_click = ""
+            if tbl.click and tbl.click.name then
+                click_str = "%@v:lua." .. M.global_name .. ".callbacks." .. tbl.click.name .. "@"
+                restore_click = (stat.click == "") and (
+                    "%X"
+                ) or (
+                    "%@v:lua." .. M.global_name .. ".callbacks." .. stat.click .. "@"
+                )
+            end
+
             if tbl.hl then
                 local restore_hl = (stat.hl == "") and (
                     "%*"
                 ) or (
                     "%#" .. stat.hl .. "#"
                 )
-                str = str .. "%#" .. tbl.hl .. "#" .. content .. restore_hl
+                str = str .. "%#" .. tbl.hl .. "#" .. click_str .. content .. restore_click .. restore_hl
             else
-                str = str .. content
+                str = str .. click_str .. content .. restore_click
             end
         end
     end
