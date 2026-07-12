eca758acd3e9204e910519e24470a4f357186cc7
kana <kana>
2026-07-12 14:22:31 +0900

    update

--- files/home/.config/nvim/lua/plugins/specs/misc.lua
+++ files/home/.config/nvim/lua/plugins/specs/misc.lua
@@ -55,10 +55,6 @@ return {
     -- breadcrumbs
     {
         'Bekaboo/dropbar.nvim',
-        dependencies = {
-            'nvim-telescope/telescope-fzf-native.nvim',
-            build = 'make'
-        },
         event = 'User Ready',
         config = function()
             local dropbar_api = require('dropbar.api')
