32736225dbe7628077cbcb5bec41230eceb8d40f
kana <kana>
2026-07-14 22:31:01 +0900

    update

--- files/home/.config/nvim/lua/plugins/init.lua
+++ files/home/.config/nvim/lua/plugins/init.lua
@@ -1,3 +1,5 @@
+vim.opt.loadplugins = true
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
     local lazyrepo = "https://github.com/folke/lazy.nvim.git"
