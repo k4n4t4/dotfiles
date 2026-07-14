d841e85e8e307283030b6055f8e8dac9f66b04bb
kana <kana>
2026-07-15 01:41:01 +0900

    update

--- files/home/.config/nvim/lua/plugins/specs/misc.lua
+++ files/home/.config/nvim/lua/plugins/specs/misc.lua
@@ -84,8 +84,10 @@ return {
                 options = {
                     icons_enabled = true,
                     theme = "auto",
-                    component_separators = { left = "", right = "" },
-                    section_separators = { left = "", right = "" },
+                    component_separators = { left = '|', right = '|' },
+                    section_separators = { left = '', right = '' },
+
+
                     globalstatus = true,
                 },
                 sections = {
