fafd3a9ee99611f1990d49302fd972d04d4752df
kana <kana>
2026-07-20 19:24:50 +0900

    update

--- files/home/.config/nvim/lua/plugins/specs/misc.lua
+++ files/home/.config/nvim/lua/plugins/specs/misc.lua
@@ -463,4 +463,13 @@ return {
             }
         end,
     },
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        build = "cd app && npm install && git restore .",
+        init = function()
+            vim.g.mkdp_filetypes = { "markdown" }
+        end,
+        ft = { "markdown" },
+    },
 }
