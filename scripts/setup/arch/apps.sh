22dc20a37b95934d7142f041b01915bc759ac4bc
kana <kana>
2026-07-20 20:45:57 +0900

    update

--- scripts/setup/arch/apps.sh
+++ scripts/setup/arch/apps.sh
@@ -1,8 +1,1 @@
 sudo pacman --needed --noconfirm -S zed wezterm mpv firefox thunderbird gimp discord obsidian
-
-# sudo pacman --needed --noconfirm -S flatpak
-#
-# flatpak install -y flathub org.mozilla.firefox
-# flatpak install -y flathub org.gimp.GIMP
-# flatpak install -y flathub com.discordapp.Discord
-# flatpak install -y flathub md.obsidian.Obsidian
