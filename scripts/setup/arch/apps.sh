87f45a66cab149ced7bf7e35e8a810dd6b905305
kana <kana>
2026-07-20 17:05:55 +0900

    update

--- scripts/setup/arch/apps.sh
+++ scripts/setup/arch/apps.sh
@@ -1,4 +1,4 @@
-sudo pacman --needed --noconfirm -S zed kitty mpv firefox thunderbird gimp discord obsidian
+sudo pacman --needed --noconfirm -S zed wezterm mpv firefox thunderbird gimp discord obsidian
 
 # sudo pacman --needed --noconfirm -S flatpak
 #
