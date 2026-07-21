86597b963bc35cae6b6461e065b263b562cb0b7d
kana <kana>
2026-07-21 23:31:55 +0900

    update

--- scripts/setup/arch/apps.sh
+++ scripts/setup/arch/apps.sh
@@ -1,2 +1,6 @@
-sudo pacman --needed --noconfirm -S zed ghostty mpv firefox thunderbird gimp discord obsidian
-git clone https://github.com/sahaj-b/ghostty-cursor-shaders ~/.config/ghostty/shaders
+sudo pacman --needed --noconfirm -S zed kitty mpv firefox thunderbird gimp discord obsidian
+
+# sudo pacman --needed --noconfirm -S wezterm
+
+# sudo pacman --needed --noconfirm -S ghostty
+# git clone https://github.com/sahaj-b/ghostty-cursor-shaders ~/.config/ghostty/shaders
