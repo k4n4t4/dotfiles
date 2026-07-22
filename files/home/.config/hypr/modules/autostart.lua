4b178c36bcc27c7b60bf1092d56c79297114bc8e
kana <kana>
2026-07-23 02:02:13 +0900

    update

--- files/home/.config/hypr/modules/autostart.lua
+++ files/home/.config/hypr/modules/autostart.lua
@@ -1,19 +1,4 @@
-local use_noctalia_shell = true
-
 hl.on("hyprland.start", function()
     hl.exec_cmd("uwsm app -- fcitx5 -d")
-
-    if use_noctalia_shell then
-        hl.exec_cmd("uwsm app -- noctalia")
-        return
-    end
-
-    hl.exec_cmd("uwsm app -- awww-daemon")
-    hl.exec_cmd("uwsm app -- awww img ~/pers/imgs/wallpaper.png --transition-type center --transition-duration 1")
-    hl.exec_cmd("uwsm app -- waybar")
-    hl.exec_cmd("uwsm app -- mako")
-    hl.exec_cmd("uwsm app -- wl-paste -w cliphist store")
-    hl.exec_cmd("uwsm app -- swayosd-server")
-    hl.exec_cmd("uwsm app -- /usr/lib/mate-polkit/polkit-mate-authentication-agent-1")
-    hl.exec_cmd("pidof hypridle || uwsm app -- hypridle")
+    hl.exec_cmd("uwsm app -- noctalia")
 end)
