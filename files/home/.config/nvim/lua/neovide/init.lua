c9b8a1e39650fde6fd367a5b14e901220047a6f9
kana <kana>
2026-07-22 20:18:36 +0900

    update

--- files/home/.config/nvim/lua/neovide/init.lua
+++ files/home/.config/nvim/lua/neovide/init.lua
@@ -2,3 +2,10 @@ vim.opt.guifont = "JetBrainsMono Nerd Font"
 
 vim.g.neovide_scale_factor = 1.1
 
+vim.g.neovide_opacity = 0.8
+vim.g.neovide_normal_opacity = 0.8
+
+vim.g.neovide_position_animation_length = 0.2
+vim.g.neovide_scroll_animation_far_lines = 1
+
+vim.g.neovide_hide_mouse_when_typing = true
