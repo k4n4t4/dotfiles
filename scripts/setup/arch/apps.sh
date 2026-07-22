7a117b2959c16d0922a3cfd41b7f1ed215486320
kana <kana>
2026-07-23 00:32:06 +0900

    update

--- scripts/setup/arch/apps.sh
+++ scripts/setup/arch/apps.sh
@@ -1,5 +1,7 @@
-sudo pacman --needed --noconfirm -S zed kitty neovide mpv firefox thunderbird gimp discord obsidian
+sudo pacman --needed --noconfirm -S zed kitty neovide nautilus mpv firefox thunderbird gimp discord obsidian
+
 xdg-mime default neovide.desktop text/plain
+xdg-mime default org.gnome.Nautilus.desktop inode/directory
 
 # sudo pacman --needed --noconfirm -S wezterm
 
