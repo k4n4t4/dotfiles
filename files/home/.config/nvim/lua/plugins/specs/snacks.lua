fdc53c8e3b8f7b5e4af9c1a50815cd08194a9ded
kana <kana>
2026-07-22 20:46:50 +0900

    update

--- files/home/.config/nvim/lua/plugins/specs/snacks.lua
+++ files/home/.config/nvim/lua/plugins/specs/snacks.lua
@@ -25,7 +25,7 @@ return {
                 doc = {
                     enabled = true,
                     inline = true,
-                    float = true,
+                    float = false,
                 },
             },
             picker = {
