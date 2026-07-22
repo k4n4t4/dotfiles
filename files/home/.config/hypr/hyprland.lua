1d8a562741e684b13eb06a25a702688dac6c57a0
kana <kana>
2026-07-23 01:34:56 +0900

    update

--- files/home/.config/hypr/hyprland.lua
+++ files/home/.config/hypr/hyprland.lua
@@ -4,3 +4,6 @@ require "modules.animations"
 require "modules.autostart"
 require "modules.env"
 require "modules.keybinds"
+
+local ok, noctalia =  pcall(require "noctalia")
+if ok then noctalia.apply_theme() end
