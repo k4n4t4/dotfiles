0e818bab149badf3a51d3c8f94d205618dd2eb25
kana <kana>
2026-07-22 20:10:39 +0900

    update

--- files/home/.config/nvim/init.lua
+++ files/home/.config/nvim/init.lua
@@ -1,8 +1,11 @@
 vim.loader.enable()
 
-if vim.g.vscode == 1 then
+if vim.g.vscode then
     require "vscode-nvim"
 else
+    if vim.g.neovide then
+        require "neovide"
+    end
     require "core"
     require "plugins"
 end
---
+++ files/home/.config/nvim/lua/neovide/init.lua
@@ -0,0 +1,4 @@
+vim.opt.guifont = "JetBrainsMono Nerd Font"
+
+vim.g.neovide_scale_factor = 1.1
+
