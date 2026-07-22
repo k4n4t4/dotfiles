0e3a3e45b4cc438eb9ccf488859ecf90c0f5ac5a
kana <kana>
2026-07-23 00:25:09 +0900

    update

--- files/home/.config/nvim/lua/neovide/init.lua
+++ files/home/.config/nvim/lua/neovide/init.lua
@@ -10,6 +10,8 @@ vim.g.neovide_scroll_animation_far_lines = 1
 
 vim.g.neovide_hide_mouse_when_typing = true
 
+vim.g.neovide_floating_corner_radius = 0.5
+
 vim.g.neovide_cursor_vfx_mode = {
     "torpedo",
     "ripple",
