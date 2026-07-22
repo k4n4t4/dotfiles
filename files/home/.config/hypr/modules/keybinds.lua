537eb8ce5ea8ea296c7d475d4a15af597c05f82c
kana <kana>
2026-07-23 03:29:14 +0900

    update

--- files/home/.config/hypr/modules/keybinds.lua
+++ files/home/.config/hypr/modules/keybinds.lua
@@ -2,30 +2,19 @@ local MOD = "SUPER"
 local LMB = "mouse:272"
 local RMB = "mouse:273"
 
-hl.bind(MOD .. " + SHIFT + Q", hl.dsp.exit())
+
+-- Hyprland control bindings
+hl.bind(MOD .. " + SHIFT + Q", hl.dsp.exec_cmd("uwsm stop"))
 hl.bind(MOD .. " + SHIFT + R", hl.dsp.exec_cmd("hyprctl reload"))
-
-
-local screenshots_dir = "~/pers/imgs/screenshots/"
-
-hl.bind(MOD .. " + O", hl.dsp.exec_cmd("hyprshot -m output -o " .. screenshots_dir))
-hl.bind(MOD .. " + CTRL + O", hl.dsp.exec_cmd("hyprshot -m region -o " .. screenshots_dir))
-hl.bind("Print", hl.dsp.exec_cmd("hyprshot -m region -o " .. screenshots_dir))
-hl.bind(MOD .. " + SHIFT + O", hl.dsp.exec_cmd("hyprshot -m window -o " .. screenshots_dir))
-hl.bind(MOD .. " + ALT + O", hl.dsp.exec_cmd("hyprshot -m output --clipboard-only"))
-hl.bind(MOD .. " + ALT + CTRL + O", hl.dsp.exec_cmd("hyprshot -m region --clipboard-only"))
-hl.bind(MOD .. " + ALT + SHIFT + O", hl.dsp.exec_cmd("hyprshot -m window --clipboard-only"))
-
-hl.bind(MOD .. " + Q", hl.dsp.window.kill())
-
-hl.bind(MOD .. " + F", hl.dsp.window.float({ action = "toggle" }))
-hl.bind(MOD .. " + D", hl.dsp.window.pseudo())
-hl.bind(MOD .. " + G", hl.dsp.layout("togglesplit"))
+hl.bind(MOD .. " + Q", hl.dsp.window.close())
+hl.bind(MOD .. " + ALT + Q", hl.dsp.window.kill())
+hl.bind(MOD .. " + F", hl.dsp.window.float())
 hl.bind(MOD .. " + C", hl.dsp.window.center())
-hl.bind("F11", hl.dsp.window.fullscreen({ action = "toggle" }))
+hl.bind(MOD .. " + M", hl.dsp.window.fullscreen())
+hl.bind("F11", hl.dsp.window.fullscreen())
 
-hl.bind(MOD .. " + " .. LMB, hl.dsp.window.drag(), { mouse = true })
-hl.bind(MOD .. " + " .. RMB, hl.dsp.window.resize(), { mouse = true })
+hl.bind(MOD .. " + " .. LMB, hl.dsp.window.drag())
+hl.bind(MOD .. " + " .. RMB, hl.dsp.window.resize())
 hl.bind(MOD .. " + H", hl.dsp.focus({ direction = "l" }))
 hl.bind(MOD .. " + L", hl.dsp.focus({ direction = "r" }))
 hl.bind(MOD .. " + K", hl.dsp.focus({ direction = "u" }))
@@ -51,35 +40,8 @@ hl.bind(MOD .. " + SHIFT + TAB", hl.dsp.window.cycle_next({ next = false }), { r
 hl.bind(MOD .. " + TAB", hl.dsp.window.alter_zorder({ mode = "top" }), { repeating = true })
 hl.bind(MOD .. " + SHIFT + TAB", hl.dsp.window.alter_zorder({ mode = "top" }), { repeating = true })
 
-hl.bind(MOD .. " + W", hl.dsp.submap("Window"))
-hl.define_submap("Window", function()
-    hl.bind("escape", hl.dsp.submap("reset"))
 
-    hl.bind("F", hl.dsp.window.float({ action = "toggle" }))
-    hl.bind("D", hl.dsp.window.pseudo())
-    hl.bind("G", hl.dsp.layout("togglesplit"))
-    hl.bind("C", hl.dsp.window.center())
-
-    hl.bind("H", hl.dsp.window.move({ x = -20, y = 0, relative = true }), { repeating = true })
-    hl.bind("L", hl.dsp.window.move({ x = 20, y = 0, relative = true }), { repeating = true })
-    hl.bind("K", hl.dsp.window.move({ x = 0, y = -20, relative = true }), { repeating = true })
-    hl.bind("J", hl.dsp.window.move({ x = 0, y = 20, relative = true }), { repeating = true })
-    hl.bind("CTRL + H", hl.dsp.window.swap({ direction = "l" }))
-    hl.bind("CTRL + L", hl.dsp.window.swap({ direction = "r" }))
-    hl.bind("CTRL + K", hl.dsp.window.swap({ direction = "u" }))
-    hl.bind("CTRL + J", hl.dsp.window.swap({ direction = "d" }))
-    hl.bind("SHIFT + H", hl.dsp.window.move({ direction = "l" }))
-    hl.bind("SHIFT + L", hl.dsp.window.move({ direction = "r" }))
-    hl.bind("SHIFT + K", hl.dsp.window.move({ direction = "u" }))
-    hl.bind("SHIFT + J", hl.dsp.window.move({ direction = "d" }))
-    hl.bind("ALT + H", hl.dsp.window.resize({ x = -10, y = 0, relative = true }), { repeating = true })
-    hl.bind("ALT + L", hl.dsp.window.resize({ x = 10, y = 0, relative = true }), { repeating = true })
-    hl.bind("ALT + K", hl.dsp.window.resize({ x = 0, y = -10, relative = true }), { repeating = true })
-    hl.bind("ALT + J", hl.dsp.window.resize({ x = 0, y = 10, relative = true }), { repeating = true })
-    hl.bind("TAB", hl.dsp.window.cycle_next({ next = true }), { repeating = true })
-    hl.bind("SHIFT + TAB", hl.dsp.window.cycle_next({ next = false }), { repeating = true })
-end)
-
+-- Media bindings
 local raise_volume    = hl.dsp.exec_cmd("noctalia msg volume-up")
 local lower_volume    = hl.dsp.exec_cmd("noctalia msg volume-down")
 local mute_volume     = hl.dsp.exec_cmd("noctalia msg volume-mute")
@@ -88,7 +50,11 @@ local lower_mic       = hl.dsp.exec_cmd("noctalia msg mic-volume-down")
 local mute_mic        = hl.dsp.exec_cmd("noctalia msg mic-mute")
 local brightness_up   = hl.dsp.exec_cmd("noctalia msg brightness-up")
 local brightness_down = hl.dsp.exec_cmd("noctalia msg brightness-down")
-
+local audio_play      = hl.dsp.exec_cmd("noctalia msg media toggle")
+local audio_pause     = hl.dsp.exec_cmd("noctalia msg media toggle")
+local audio_stop      = hl.dsp.exec_cmd("noctalia msg media stop")
+local audio_next      = hl.dsp.exec_cmd("noctalia msg media next")
+local audio_prev      = hl.dsp.exec_cmd("noctalia msg media previous")
 
 hl.bind("XF86AudioRaiseVolume", raise_volume, { repeating = true })
 hl.bind("XF86AudioLowerVolume", lower_volume, { repeating = true })
@@ -96,6 +62,11 @@ hl.bind("XF86AudioMute", mute_volume)
 hl.bind("XF86AudioMicMute", mute_mic)
 hl.bind("XF86MonBrightnessUp", brightness_up, { repeating = true })
 hl.bind("XF86MonBrightnessDown", brightness_down, { repeating = true })
+hl.bind("XF86AudioPlay", audio_play)
+hl.bind("XF86AudioPause", audio_pause)
+hl.bind("XF86AudioStop", audio_stop)
+hl.bind("XF86AudioNext", audio_next)
+hl.bind("XF86AudioPrev", audio_prev)
 
 hl.bind(MOD .. " + A", hl.dsp.submap("Media"))
 hl.define_submap("Media", function()
@@ -117,11 +88,15 @@ hl.define_submap("Media", function()
     hl.bind("CTRL + j", brightness_down, { repeating = true })
 end)
 
+
+-- Application bindings
 local terminal = "uwsm app -- kitty"
+local text_editor = "uwsm app -- neovide"
+local file_manager = "uwsm app -- nautilus"
 local browser = "uwsm app -- firefox"
 local private_browser = "uwsm app -- firefox --private-window"
 local launcher = "noctalia msg panel-toggle launcher"
-local clipboard_manager = "cliphist list | uwsm app -- wofi --dmenu | cliphist decode | wl-copy"
+local clipboard_manager = "noctalia msg panel-toggle clipboard"
 local system_menu = "noctalia msg panel-toggle session"
 local task_manager = "noctalia msg panel-toggle control-center system"
 local lock_screen = "noctalia msg session lock"
@@ -129,38 +104,38 @@ local lock_screen = "noctalia msg session lock"
 hl.bind(MOD .. " + ESCAPE", hl.dsp.exec_cmd(system_menu))
 hl.bind(MOD .. " + X", hl.dsp.exec_cmd(lock_screen))
 hl.bind(MOD .. " + T", hl.dsp.exec_cmd(terminal))
+hl.bind(MOD .. " + N", hl.dsp.exec_cmd(text_editor))
+hl.bind(MOD .. " + E", hl.dsp.exec_cmd(file_manager))
 hl.bind(MOD .. " + R", hl.dsp.exec_cmd(launcher))
 hl.bind(MOD .. " + V", hl.dsp.exec_cmd(clipboard_manager))
 hl.bind(MOD .. " + B", hl.dsp.exec_cmd(browser))
-hl.bind(MOD .. " + P", hl.dsp.exec_cmd(private_browser))
-hl.bind(MOD .. " + M", hl.dsp.exec_cmd(task_manager))
+hl.bind(MOD .. " + SHIFT + B", hl.dsp.exec_cmd(private_browser))
+hl.bind(MOD .. " + S", hl.dsp.exec_cmd(task_manager))
 
+
+-- Screenshot bindings
+local screenshots_dir = "~/pers/media/imgs/screenshots/"
+local screenshot_all = hl.dsp.exec_cmd("hyprshot -m output -o " .. screenshots_dir)
+local screenshot_region = hl.dsp.exec_cmd("hyprshot -m region -o " .. screenshots_dir)
+local screenshot_window = hl.dsp.exec_cmd("hyprshot -m window -o " .. screenshots_dir)
+
+hl.bind(MOD .. " + P", screenshot_all)
+hl.bind(MOD .. " + CTRL + P", screenshot_region)
+hl.bind(MOD .. " + SHIFT + P", screenshot_window)
+hl.bind("Print", screenshot_region)
+
+
+-- Workspace navigation bindings
 for i = 1, 10 do
     local key = tostring(i % 10)
     hl.bind(MOD .. " + " .. key, hl.dsp.focus({ workspace = i }))
     hl.bind(MOD .. " + SHIFT + " .. key, hl.dsp.window.move({ workspace = i }))
 end
 
-hl.bind(MOD .. " + grave", hl.dsp.focus({ workspace = 11 }))
-hl.bind(MOD .. " + SHIFT + grave", hl.dsp.window.move({ workspace = 11 }))
+hl.bind(MOD .. " + grave", hl.dsp.workspace.toggle_special("S"))
+hl.bind(MOD .. " + period", hl.dsp.focus({ workspace = "+1" }), { repeating = true })
+hl.bind(MOD .. " + comma", hl.dsp.focus({ workspace = "-1" }), { repeating = true })
 
-hl.bind(MOD .. " + S", hl.dsp.workspace.toggle_special("A"))
-hl.bind(MOD .. " + CTRL + S", hl.dsp.workspace.toggle_special("B"))
-hl.bind(MOD .. " + ALT + S", hl.dsp.workspace.toggle_special("C"))
-hl.bind(MOD .. " + CTRL + ALT + S", hl.dsp.workspace.toggle_special("D"))
-hl.bind(MOD .. " + left", hl.dsp.focus({ workspace = "-1" }), { repeating = true })
-hl.bind(MOD .. " + right", hl.dsp.focus({ workspace = "+1" }), { repeating = true })
-
-hl.bind(MOD .. " + SHIFT + S", hl.dsp.window.move({ workspace = "special:A" }))
-hl.bind(MOD .. " + CTRL + SHIFT + S", hl.dsp.window.move({ workspace = "special:B" }))
-hl.bind(MOD .. " + ALT + SHIFT + S", hl.dsp.window.move({ workspace = "special:C" }))
-hl.bind(MOD .. " + CTRL + ALT + SHIFT + S", hl.dsp.window.move({ workspace = "special:D" }))
-hl.bind(MOD .. " + SHIFT + left", hl.dsp.window.move({ workspace = "-1" }), { repeating = true })
-hl.bind(MOD .. " + SHIFT + right", hl.dsp.window.move({ workspace = "+1" }), { repeating = true })
-
-hl.bind(MOD .. " + period", hl.dsp.layout("move +col"))
-hl.bind(MOD .. " + comma", hl.dsp.layout("move -col"))
-hl.bind(MOD .. " + SHIFT + period", hl.dsp.layout("movewindowto r"))
-hl.bind(MOD .. " + SHIFT + comma", hl.dsp.layout("movewindowto l"))
-hl.bind(MOD .. " + SHIFT + up", hl.dsp.layout("movewindowto u"))
-hl.bind(MOD .. " + SHIFT + down", hl.dsp.layout("movewindowto d"))
+hl.bind(MOD .. " + SHIFT + grave", hl.dsp.window.move({ workspace = "special:S" }))
+hl.bind(MOD .. " + SHIFT + period", hl.dsp.window.move({ workspace = "+1" }), { repeating = true })
+hl.bind(MOD .. " + SHIFT + comma", hl.dsp.window.move({ workspace = "-1" }), { repeating = true })
