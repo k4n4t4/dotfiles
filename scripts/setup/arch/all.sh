0b15516f45a6019130f1e9a0a84c03683912d295
kana <kana>
2026-07-23 12:37:14 +0900

    update

--- scripts/setup/arch/all.sh
+++ scripts/setup/arch/all.sh
@@ -1,6 +1,9 @@
 script_source setup/arch/essential
 script_source setup/arch/yay
 script_source setup/arch/cli-tools
-script_source setup/arch/niri
+
+# script_source setup/arch/niri
+script_source setup/arch/hyprland
+
 script_source setup/arch/greeter
 script_source setup/arch/apps
