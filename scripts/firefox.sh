728d3595292f7116d3badca4841e77f087b63653
k4n4t4 <91060664+k4n4t4@users.noreply.github.com>
2026-07-28 23:59:20 +0900

    feat(config): add firefox

---
+++ scripts/firefox.sh
@@ -0,0 +1,26 @@
+firefox_folder=""
+
+if cmd_exists flatpak && flatpak info org.mozilla.firefox &>/dev/null; then
+    firefox_folder=~/.var/app/org.mozilla.firefox/config/mozilla/firefox
+    flatpak override --user --filesystem="$DOT_ORIGIN_PATH/home/.config/firefox":ro org.mozilla.firefox
+fi
+
+if cmd_exists firefox; then
+    firefox_folder=~/.config/mozilla/firefox
+fi
+
+if ! [ -f "$firefox_folder/profiles.ini" ]; then
+    firefox_folder=""
+fi
+
+if [ -n "$firefox_folder" ]; then
+    firefox_profile_name="$(awk -F= '/^\[Install/ {f=1} f && /^Default=/ {print $2; exit}' "$firefox_folder/profiles.ini")"
+    firefox_profile="$firefox_folder/$firefox_profile_name"
+
+    if [ -d "$firefox_profile" ]; then
+        dothome "config/firefox/user.js" "$firefox_profile/user.js"
+        dothome -r "config/firefox/chrome" "$firefox_profile/chrome"
+    else
+        echo "Firefox profile not found at $firefox_profile"
+    fi
+fi
