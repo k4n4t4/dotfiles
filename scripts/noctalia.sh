f33ed16e5f6f5a18b211caa02167d19e0fa207d5
k4n4t4 <91060664+k4n4t4@users.noreply.github.com>
2026-07-28 22:16:19 +0900

    refactor: change xdg user-dirs.dirs

--- scripts/noctalia.sh
+++ scripts/noctalia.sh
@@ -1,9 +1,9 @@
 if cmd_exists noctalia; then
     dotconf "noctalia" -r
-    if ! [ -d "$TARGET_PATH/pers/media/imgs/wallpapers" ]; then
-        msg_run mkdir -p "$TARGET_PATH/pers/media/imgs/wallpapers"
+    if ! [ -d "$TARGET_PATH/pers/media/images/wallpapers" ]; then
+        msg_run mkdir -p "$TARGET_PATH/pers/media/images/wallpapers"
     fi
-    if ! [ -f "$TARGET_PATH/pers/media/imgs/wallpapers/wallpaper.png" ]; then
-        msg_run cp "$DOTFILES_PATH/assets/wallpaper.png" "$TARGET_PATH/pers/media/imgs/wallpapers"
+    if ! [ -f "$TARGET_PATH/pers/media/images/wallpapers/wallpaper.png" ]; then
+        msg_run cp "$DOTFILES_PATH/assets/wallpaper.png" "$TARGET_PATH/pers/media/images/wallpapers"
     fi
 fi
