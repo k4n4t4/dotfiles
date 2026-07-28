b424799322dd3f3239c5243831d3a79c6822dd1d
k4n4t4 <kana>
2026-07-28 19:21:50 +0900

    feat(config/nvim): time

--- files/home/config/nvim/init.lua
+++ files/home/config/nvim/init.lua
@@ -1,5 +1,14 @@
 vim.loader.enable()
 
+-- local time = vim.loop.hrtime()
+-- vim.api.nvim_create_autocmd("SafeState", {
+--     once = true,
+--     callback = function()
+--         local elapsed_ms = (vim.loop.hrtime() - time) / 1e6
+--         vim.notify(string.format("Neovim loaded in %.2fms", elapsed_ms))
+--     end,
+-- })
+
 if vim.g.vscode then
     require "vscode-nvim"
 else
