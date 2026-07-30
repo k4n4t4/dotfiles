837e55ec693aa5249a3a5fabf55d9e356afcac5c
k4n4t4 <91060664+k4n4t4@users.noreply.github.com>
2026-07-30 19:18:38 +0900

    chore: icon theme

--- scripts/setup/arch/theme.sh
+++ scripts/setup/arch/theme.sh
@@ -9,3 +9,7 @@ mkdir -p ~/.config/environment.d
 touch ~/.config/environment.d/cursor.conf
 grep -q '^XCURSOR_THEME=' ~/.config/environment.d/cursor.conf && sed -i 's|^XCURSOR_THEME=.*|XCURSOR_THEME=capitaine-cursors|' ~/.config/environment.d/cursor.conf || echo 'XCURSOR_THEME=capitaine-cursors' >> ~/.config/environment.d/cursor.conf
 grep -q '^XCURSOR_SIZE=' ~/.config/environment.d/cursor.conf && sed -i 's|^XCURSOR_SIZE=.*|XCURSOR_SIZE=36|' ~/.config/environment.d/cursor.conf || echo 'XCURSOR_SIZE=36' >> ~/.config/environment.d/cursor.conf
+
+sudo pacman --needed --noconfirm -S tela-circle-icon-theme-all
+
+gsettings set org.gnome.desktop.interface icon-theme 'Tela-circle-grey-dark'
