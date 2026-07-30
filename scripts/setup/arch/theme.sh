b6b8097ee6f35da466eacb28f2c362c4b52ff07e
k4n4t4 <91060664+k4n4t4@users.noreply.github.com>
2026-07-30 19:00:20 +0900

    chore: theme

--- scripts/setup/arch/theme.sh
+++ scripts/setup/arch/theme.sh
@@ -2,7 +2,10 @@ sudo pacman --needed --noconfirm -S nwg-look adw-gtk-theme
 
 gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3'
 
+
 sudo pacman --needed --noconfirm -S capitaine-cursors
 
-gsettings set org.gnome.desktop.interface cursor-theme 'capitaine-cursors'
-gsettings set org.gnome.desktop.interface cursor-size 96
+mkdir -p ~/.config/environment.d
+touch ~/.config/environment.d/cursor.conf
+grep -q '^XCURSOR_THEME=' ~/.config/environment.d/cursor.conf && sed -i 's|^XCURSOR_THEME=.*|XCURSOR_THEME=capitaine-cursors|' ~/.config/environment.d/cursor.conf || echo 'XCURSOR_THEME=capitaine-cursors' >> ~/.config/environment.d/cursor.conf
+grep -q '^XCURSOR_SIZE=' ~/.config/environment.d/cursor.conf && sed -i 's|^XCURSOR_SIZE=.*|XCURSOR_SIZE=36|' ~/.config/environment.d/cursor.conf || echo 'XCURSOR_SIZE=36' >> ~/.config/environment.d/cursor.conf
