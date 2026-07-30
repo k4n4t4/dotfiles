7f2c08d23d7e8819391b95ef78c063774a134ed1
k4n4t4 <91060664+k4n4t4@users.noreply.github.com>
2026-07-30 09:50:26 +0900

    feat(config/firefox): Add default page

--- files/home/config/firefox/user.js
+++ files/home/config/firefox/user.js
@@ -1,7 +1,7 @@
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("browser.aboutConfig.showWarning", false);
 user_pref("browser.warnOnQuitShortcut", false);
-user_pref("browser.startup.homepage", "about:home");
+user_pref("browser.startup.homepage", "~/.dotfiles/files/home/config/firefox/default-page/index.html");
 user_pref("browser.download.dir", "~/downloads");
 user_pref("browser.theme.dark-private-windows", true);
 user_pref("browser.tabs.hoverPreview.enabled", true);
