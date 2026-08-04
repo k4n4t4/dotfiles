961a37ee55643366c21bc666b71af36197737f96
kana <kana>
2026-08-04 21:42:57 +0900

    feat(config/nvim): lualine visual info

--- files/home/config/nvim/lua/plugins/specs/misc.lua
+++ files/home/config/nvim/lua/plugins/specs/misc.lua
@@ -105,6 +105,34 @@ return {
                 end
                 return " " .. table.concat(names, ", ")
             end
+            local function visual_info()
+                local mode = vim.fn.mode()
+                if mode ~= 'v' and mode ~= 'V' and mode ~= '\22' then
+                    return ''
+                end
+
+                local l1, l2 = vim.fn.line('v'), vim.fn.line('.')
+                local lines = math.abs(l2 - l1) + 1
+
+                if mode == 'V' then
+                    return string.format('%d lines', lines)
+
+                elseif mode == '\22' then
+                    local c1, c2 = vim.fn.virtcol('v'), vim.fn.virtcol('.')
+                    local cols = math.abs(c2 - c1) + 1
+                    return string.format('%dx%d', lines, cols)
+
+                else
+                    local wc = vim.fn.wordcount()
+                    local chars = wc.visual_chars or 0
+                    if lines > 1 then
+                        return string.format('%d lines %d chars', lines, chars)
+                    else
+                        return string.format('%d chars', chars)
+                    end
+                end
+            end
+
             require("lualine").setup {
                 options = {
                     icons_enabled = true,
@@ -135,7 +163,7 @@ return {
                     },
                     lualine_x = { lsp_status, "encoding", "fileformat", "filetype" },
                     lualine_y = { "progress", "searchcount" },
-                    lualine_z = { "location" },
+                    lualine_z = { visual_info, "location" },
                 },
                 inactive_sections = {
                     lualine_a = {},
