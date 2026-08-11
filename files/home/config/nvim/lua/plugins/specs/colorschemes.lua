ede17362bd495e6ed94830c454edca8eee51946e
kana <kana>
2026-08-11 22:32:29 +0900

    feat(config/nvim): random colorscheme on startup

--- files/home/config/nvim/lua/plugins/specs/colorschemes.lua
+++ files/home/config/nvim/lua/plugins/specs/colorschemes.lua
@@ -1,3 +1,28 @@
+local schemes = {
+    { name = "tokyonight-moon", plugin = "tokyonight.nvim" },
+    { name = "kanagawa-wave",   plugin = "kanagawa.nvim" },
+    { name = "gruvbox",         plugin = "gruvbox.nvim" },
+    { name = "onedark",         plugin = "onedark.nvim" },
+    { name = "colorful",        plugin = "mini.base16" }
+}
+
+vim.api.nvim_create_autocmd("UIEnter", {
+    callback = function()
+        math.randomseed(os.time())
+        local selected = schemes[math.random(1, #schemes)]
+
+        local ok_lazy, lazy = pcall(require, "lazy")
+        if ok_lazy then
+            lazy.load({ plugins = { selected.plugin } })
+        end
+
+        local status_ok, _ = pcall(vim.cmd.colorscheme, selected.name)
+        if not status_ok then
+            vim.notify("Failed to load colorscheme: " .. selected.name, vim.log.levels.ERROR)
+        end
+    end
+})
+
 return {
     { -- tokyonight
         "folke/tokyonight.nvim",
@@ -9,10 +34,10 @@ return {
         "ellisonleao/gruvbox.nvim",
     },
     { -- onedark
-        "navarasu/onedark.nvim"
+        "navarasu/onedark.nvim",
     },
     { -- solarized osaka
-        "craftzdog/solarized-osaka.nvim"
+        "craftzdog/solarized-osaka.nvim",
     },
     { -- mini base16
         "nvim-mini/mini.base16",
