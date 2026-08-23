22967350287ac34ad5e556e26881ea1e54af34ba
kana <kana>
2026-08-23 17:19:27 +0900

    feat(config/fish): greeting message

--- files/home/config/fish/functions/fish_greeting.fish
+++ files/home/config/fish/functions/fish_greeting.fish
@@ -1,2 +1,9 @@
 function fish_greeting
+    if type -q fastfetch
+        if test $COLUMNS -le 80
+            fastfetch -c mini
+        else
+            fastfetch -c full
+        end
+    end
 end
